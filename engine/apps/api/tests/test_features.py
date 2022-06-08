2f23cd4d546d1a93c3f1b29436b7cfefc7d3c23e
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2022-06-08 18:55:44 +0400

    Fix tests

--- engine/apps/api/tests/test_features.py
+++ engine/apps/api/tests/test_features.py
@@ -3,7 +3,12 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from apps.api.views.features import FEATURE_LIVE_SETTINGS, FEATURE_SLACK, FEATURE_TELEGRAM
+from apps.api.views.features import (
+    FEATURE_GRAFANA_CLOUD_CONNECTION,
+    FEATURE_LIVE_SETTINGS,
+    FEATURE_SLACK,
+    FEATURE_TELEGRAM,
+)
 
 
 @pytest.mark.django_db
@@ -30,15 +35,17 @@ def test_select_features_all_enabled(
     make_user_auth_headers,
 ):
     organization, user, token = make_organization_and_user_with_plugin_token()
+    settings.OSS = True
     settings.FEATURE_SLACK_INTEGRATION_ENABLED = True
     settings.FEATURE_TELEGRAM_INTEGRATION_ENABLED = True
     settings.FEATURE_LIVE_SETTINGS_ENABLED = True
+    settings.FEATURE_GRAFANA_CLOUD_CONNECTION = True
     client = APIClient()
     url = reverse("api-internal:features")
     response = client.get(url, format="json", **make_user_auth_headers(user, token))
 
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == [FEATURE_SLACK, FEATURE_TELEGRAM, FEATURE_LIVE_SETTINGS]
+    assert response.json() == [FEATURE_SLACK, FEATURE_TELEGRAM, FEATURE_GRAFANA_CLOUD_CONNECTION, FEATURE_LIVE_SETTINGS]
 
 
 @pytest.mark.django_db
@@ -48,6 +55,7 @@ def test_select_features_all_disabled(
     make_user_auth_headers,
 ):
     organization, user, token = make_organization_and_user_with_plugin_token()
+    settings.OSS = False
     settings.FEATURE_SLACK_INTEGRATION_ENABLED = False
     settings.FEATURE_TELEGRAM_INTEGRATION_ENABLED = False
     settings.FEATURE_LIVE_SETTINGS_ENABLED = False
