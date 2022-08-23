26b5671234c248602313c2cff46aa0673cda81a9
Gilberto <gp_sj@hotmail.com>
2022-08-23 17:29:27 -0300

    Script to insert Pagerduty users into Grafana via API

---
+++ tools/pagerduty-migrator/scripts/README.md
@@ -0,0 +1,9 @@
+# Grafana Oncall Scripts
+
+When we run MODE="plan" we can notice that there is escalation, integration in pagerduty that needs to be linked to a user.
+
+To solve this problem, we can run the  add_users_pagerduty_to_grafana.py script
+
+```bash
+docker run -it --rm -e PAGERDUTY_API_TOKEN="mytoken" -e URL_GRAFANA="http://localhost:3000" -e USERNAME_GRAFANA="admin" -e PASSWORD_GRAFANA="admin" pd-oncall-migrator python /app/scripts/add_users_pagerduty_to_grafana.py
+```
---
+++ tools/pagerduty-migrator/scripts/add_users_pagerduty_to_grafana.py
@@ -0,0 +1,44 @@
+import os
+import secrets
+import sys
+import requests
+
+from urllib.parse import urljoin
+from pdpyras import APISession
+
+PAGERDUTY_API_TOKEN = os.environ["PAGERDUTY_API_TOKEN"]
+PATH_USERS_GRAFANA = "/api/admin/users"
+URL_GRAFANA = os.environ["URL_GRAFANA"] # Example: http://localhost:3000
+USERNAME_GRAFANA = os.environ["USERNAME_GRAFANA"]
+PASSWORD_GRAFANA = os.environ["PASSWORD_GRAFANA"]
+SUCCESS_SIGN = "✅"
+ERROR_SIGN = "❌"
+
+def pd_list_users():
+
+    session = APISession(PAGERDUTY_API_TOKEN)
+
+    users = session.list_all("users", params={"include[]": "notification_rules"})
+
+    for user in users:
+        password = secrets.token_urlsafe(15)
+        username = user["email"].split("@")
+        json = {"name": user["name"], "email": user["email"], "login": username[0], "password": password}
+        post_grafana(json)
+
+def post_grafana(data):
+    url = urljoin(URL_GRAFANA, PATH_USERS_GRAFANA)
+    response = requests.request("POST", url, auth=(USERNAME_GRAFANA, PASSWORD_GRAFANA), json=data)
+
+    if response.status_code == 200:
+        print(SUCCESS_SIGN + " User created " + data["login"])
+    elif response.status_code == 401:
+        sys.exit(ERROR_SIGN + " Invalid username or password.")
+    elif response.status_code == 412:
+        print(ERROR_SIGN + " User " + data["login"] + " already exists." )
+    else: 
+        print(response.text)
+        sys.exit()
+
+if __name__ == "__main__":
+    pd_list_users()
