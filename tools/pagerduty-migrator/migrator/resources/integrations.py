7b206037479dc3a1aa3d8b943eb5a6d9acb49be5
Gilberto <gp_sj@hotmail.com>
2022-08-23 17:39:30 -0300

    If there are no vendors, do not break / If there is no integration apply webhook as default

--- tools/pagerduty-migrator/migrator/resources/integrations.py
+++ tools/pagerduty-migrator/migrator/resources/integrations.py
@@ -15,19 +15,20 @@ def match_integration(integration: dict, oncall_integrations: list[dict]) -> Non
 
 
 def match_integration_type(integration: dict, vendors: list[dict]) -> None:
-    vendors_map = {vendor["id"]: vendor for vendor in vendors}
-
-    if integration["type"] not in [
-        "generic_events_api_inbound_integration",
-        "events_api_v2_inbound_integration",
-    ]:
-        integration["oncall_type"] = None
-        return
-
-    vendor_id = integration["vendor"]["id"]
-    vendor_name = vendors_map[vendor_id]["name"]
-
-    integration["oncall_type"] = PAGERDUTY_TO_ONCALL_VENDOR_MAP.get(vendor_name)
+    if integration["vendor"] != None:
+        vendors_map = {vendor["id"]: vendor for vendor in vendors}
+    
+        if integration["type"] not in [
+            "generic_events_api_inbound_integration",
+            "events_api_v2_inbound_integration",
+        ]:
+            integration["name"] = None
+            return
+    
+        vendor_id = integration["vendor"]["id"]
+        vendor_name = vendors_map[vendor_id]["name"]
+    
+        integration["name"] = PAGERDUTY_TO_ONCALL_VENDOR_MAP.get(vendor_name)
 
 
 def migrate_integration(integration: dict, escalation_policies: list[dict]) -> None:
@@ -43,9 +44,14 @@ def migrate_integration(integration: dict, escalation_policies: list[dict]) -> N
 
     oncall_name = "{} - {}".format(integration["service"]["name"], integration["name"])
 
+    if "oncall_type" in integration.keys():
+        oncall_type = integration["oncall_type"]
+    else:
+        oncall_type = "webhook"
+
     create_integration(
         oncall_name,
-        integration["oncall_type"],
+        oncall_type,
         oncall_escalation_chain["id"],
     )
 
@@ -57,9 +63,11 @@ def create_integration(
 
     integration = oncall_api_client.create("integrations", payload)
 
-    routes = oncall_api_client.list_all("routes/?integration_id={}".format(integration["id"]))
-    default_route_id = routes[0]["id"]
+    if "id" in integration.keys():
 
-    oncall_api_client.update(
-        f"routes/{default_route_id}", {"escalation_chain_id": escalation_chain_id}
-    )
+        routes = oncall_api_client.list_all("routes/?integration_id={}".format(integration["id"]))
+        default_route_id = routes[0]["id"]
+
+        oncall_api_client.update(
+            f"routes/{default_route_id}", {"escalation_chain_id": escalation_chain_id}
+        )
