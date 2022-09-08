a920062c8faea39f95348d131b5b016a8dff62ab
Gilberto <gp_sj@hotmail.com>
2022-09-08 15:30:39 -0300

    Adjusted script to follow project pattern

--- tools/pagerduty-migrator/scripts/add_users_pagerduty_to_grafana.py
+++ tools/pagerduty-migrator/scripts/add_users_pagerduty_to_grafana.py
@@ -8,27 +8,26 @@ from pdpyras import APISession
 
 PAGERDUTY_API_TOKEN = os.environ["PAGERDUTY_API_TOKEN"]
 PATH_USERS_GRAFANA = "/api/admin/users"
-URL_GRAFANA = os.environ["URL_GRAFANA"] # Example: http://localhost:3000
-USERNAME_GRAFANA = os.environ["USERNAME_GRAFANA"]
-PASSWORD_GRAFANA = os.environ["PASSWORD_GRAFANA"]
+GRAFANA_URL = os.environ["GRAFANA_URL"] # Example: http://localhost:3000
+GRAFANA_USERNAME = os.environ["GRAFANA_USERNAME"]
+GRAFANA_PASSWORD = os.environ["GRAFANA_PASSWORD"]
 SUCCESS_SIGN = "✅"
 ERROR_SIGN = "❌"
 
-def pd_list_users():
-
+def list_pagerduty_users():
     session = APISession(PAGERDUTY_API_TOKEN)
 
     users = session.list_all("users")
 
     for user in users:
         password = secrets.token_urlsafe(15)
-        username = user["email"].split("@")
+        username = user["email"].split("@")[0]
         json = {"name": user["name"], "email": user["email"], "login": username, "password": password}
-        post_grafana(json)
+        create_grafana_user(json)
 
-def post_grafana(data):
-    url = urljoin(URL_GRAFANA, PATH_USERS_GRAFANA)
-    response = requests.request("POST", url, auth=(USERNAME_GRAFANA, PASSWORD_GRAFANA), json=data)
+def create_grafana_user(data):
+    url = urljoin(GRAFANA_URL, PATH_USERS_GRAFANA)
+    response = requests.request("POST", url, auth=(GRAFANA_USERNAME, GRAFANA_PASSWORD), json=data)
 
     if response.status_code == 200:
         print(SUCCESS_SIGN + " User created: " + data["login"])
@@ -40,4 +39,4 @@ def post_grafana(data):
         print("{} {}".format(ERROR_SIGN, response.text))
 
 if __name__ == "__main__":
-    pd_list_users()
+    list_pagerduty_users()
