efd32683466a9fc705a8f5c7a53e9c3bf7d4e18d
Vadim Stepanov <vadimkerr@gmail.com>
2022-09-15 16:59:32 +0100

    add sqlite and redis config to hobby settings

--- engine/settings/hobby.py
+++ engine/settings/hobby.py
@@ -1,35 +1,47 @@
 # flake8: noqa: F405
+from .prod_without_db import *
 
-from random import randrange
+DATABASE = os.getenv("DATABASE", default="mysql")
+assert DATABASE in ["mysql", "sqlite"]
 
-# Workaround to use pymysql instead of mysqlclient
-import pymysql
+BROKER = os.getenv("BROKER", default="rabbitmq")
+assert BROKER in ["rabbitmq", "redis"]
 
-from .prod_without_db import *  # noqa
+if DATABASE == "mysql":
+    import pymysql
 
-pymysql.install_as_MySQLdb()
+    pymysql.install_as_MySQLdb()
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("MYSQL_DB_NAME"),
-        "USER": os.environ.get("MYSQL_USER"),
-        "PASSWORD": os.environ["MYSQL_PASSWORD"],
-        "HOST": os.environ.get("MYSQL_HOST"),
-        "PORT": os.environ.get("MYSQL_PORT"),
-        "OPTIONS": {
-            "charset": "utf8mb4",
-            "connect_timeout": 1,
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.environ["MYSQL_DB_NAME"],
+            "USER": os.environ["MYSQL_USER"],
+            "PASSWORD": os.environ["MYSQL_PASSWORD"],
+            "HOST": os.environ["MYSQL_HOST"],
+            "PORT": os.environ["MYSQL_PORT"],
+            "OPTIONS": {
+                "charset": "utf8mb4",
+                "connect_timeout": 1,
+            },
         },
-    },
-}
+    }
+elif DATABASE == "sqlite":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": "/var/lib/oncall/oncall.db",
+        },
+    }
 
-RABBITMQ_USERNAME = os.environ.get("RABBITMQ_USERNAME")
-RABBITMQ_PASSWORD = os.environ.get("RABBITMQ_PASSWORD")
-RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST")
-RABBITMQ_PORT = os.environ.get("RABBITMQ_PORT")
-
-CELERY_BROKER_URL = f"amqp://{RABBITMQ_USERNAME}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}"
+if BROKER == "rabbitmq":
+    RABBITMQ_USERNAME = os.environ["RABBITMQ_USERNAME"]
+    RABBITMQ_PASSWORD = os.environ["RABBITMQ_PASSWORD"]
+    RABBITMQ_HOST = os.environ["RABBITMQ_HOST"]
+    RABBITMQ_PORT = os.environ["RABBITMQ_PORT"]
+    CELERY_BROKER_URL = f"amqp://{RABBITMQ_USERNAME}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}"
+elif BROKER == "redis":
+    CELERY_BROKER_URL = os.environ["REDIS_URI"]
 
 MIRAGE_SECRET_KEY = SECRET_KEY
 MIRAGE_CIPHER_IV = "1234567890abcdef"  # use default
