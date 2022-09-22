b86a3f06561e859fe88a5e926932b9c69c2e8ee7
Michael Derynck <michael.derynck@grafana.com>
2022-09-22 15:32:25 -0600

    More concise override

--- engine/common/api_helpers/paginators.py
+++ engine/common/api_helpers/paginators.py
@@ -1,25 +1,19 @@
 from rest_framework.pagination import CursorPagination, PageNumberPagination
-from rest_framework.utils.urls import remove_query_param, replace_query_param
 
 from common.api_helpers.utils import create_engine_url
 
 
 class PathPrefixedPagination(PageNumberPagination):
-    def get_next_link(self):
-        if not self.page.has_next():
-            return None
-        url = create_engine_url(self.request.get_full_path())
-        page_number = self.page.next_page_number()
-        return replace_query_param(url, self.page_query_param, page_number)
+    def paginate_queryset(self, queryset, request, view=None):
+        request.build_absolute_uri = lambda: create_engine_url(request.get_full_path())
+        return super().paginate_queryset(queryset, request, view)
 
-    def get_previous_link(self):
-        if not self.page.has_previous():
-            return None
-        url = create_engine_url(self.request.get_full_path())
-        page_number = self.page.previous_page_number()
-        if page_number == 1:
-            return remove_query_param(url, self.page_query_param)
-        return replace_query_param(url, self.page_query_param, page_number)
+
+class PathPrefixedCursorPagination(CursorPagination):
+    def paginate_queryset(self, queryset, request, view=None):
+        pg = super().paginate_queryset(queryset, request, view)
+        self.base_url = create_engine_url(request.get_full_path())
+        return pg
 
 
 class HundredPageSizePaginator(PathPrefixedPagination):
@@ -34,7 +28,7 @@ class TwentyFivePageSizePaginator(PathPrefixedPagination):
     page_size = 25
 
 
-class TwentyFiveCursorPaginator(CursorPagination):
+class TwentyFiveCursorPaginator(PathPrefixedCursorPagination):
     page_size = 25
     max_page_size = 100
     page_size_query_param = "perpage"
