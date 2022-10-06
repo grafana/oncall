0ee23680e3fe4b6b67b5909e7a772dfded4d86d9
Vadim Stepanov <vadimkerr@gmail.com>
2022-10-06 14:43:17 +0100

    add email templater

--- engine/apps/email/backend.py
+++ engine/apps/email/backend.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import get_connection, send_mail
 from django.utils.html import strip_tags
 
 from apps.base.messaging import BaseMessagingBackend
@@ -11,6 +11,7 @@ class EmailBackend(BaseMessagingBackend):
     label = "Email"
     short_label = "Email"
     available_for_use = True
+    templater = "apps.email.alert_rendering.AlertEmailTemplater"
 
     def serialize_user(self, user):
         return {"email": user.email}
@@ -22,5 +23,15 @@ class EmailBackend(BaseMessagingBackend):
         email_from = settings.EMAIL_HOST_USER
         recipient_list = [user.email]
 
+        # todo: add to live settings
+        connection = get_connection(
+            host=settings.EMAIL_HOST,
+            port=settings.EMAIL_PORT,
+            username=settings.EMAIL_HOST_USER,
+            password=settings.EMAIL_HOST_PASSWORD,
+            use_tls=settings.EMAIL_USE_TLS,
+            fail_silently=False,
+        )
+
         # send email through Django setup
-        send_mail(subject, message, email_from, recipient_list, html_message=html_message)
+        send_mail(subject, message, email_from, recipient_list, html_message=html_message, connection=connection)
