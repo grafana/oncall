70f7c5034182a24e3026e1958c8cd1040c19086f
Vadim Stepanov <vadimkerr@gmail.com>
2022-10-13 12:05:53 +0100

    fix tests

--- engine/apps/public_api/tests/test_integrations.py
+++ engine/apps/public_api/tests/test_integrations.py
@@ -59,6 +59,11 @@ def test_get_list_integrations(
                         "message": None,
                         "image_url": None,
                     },
+                    TEST_MESSAGING_BACKEND_FIELD: {
+                        "title": None,
+                        "message": None,
+                        "image_url": None,
+                    },
                 },
                 "maintenance_mode": None,
                 "maintenance_started_at": None,
@@ -184,6 +189,71 @@ def test_update_integration_template(
                 "message": None,
                 "image_url": None,
             },
+            TEST_MESSAGING_BACKEND_FIELD: {
+                "title": None,
+                "message": None,
+                "image_url": None,
+            },
+        },
+        "maintenance_mode": None,
+        "maintenance_started_at": None,
+        "maintenance_end_at": None,
+    }
+    url = reverse("api-public:integrations-detail", args=[integration.public_primary_key])
+    response = client.put(url, data=data_for_update, format="json", HTTP_AUTHORIZATION=f"{token}")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == expected_response
+
+
+@pytest.mark.django_db
+def test_update_integration_template_messaging_backend(
+    make_organization_and_user_with_token, make_alert_receive_channel, make_channel_filter, make_integration_heartbeat
+):
+    organization, user, token = make_organization_and_user_with_token()
+    integration = make_alert_receive_channel(organization, verbal_name="grafana")
+    default_channel_filter = make_channel_filter(integration, is_default=True)
+    make_integration_heartbeat(integration)
+
+    client = APIClient()
+    data_for_update = {"templates": {"grouping_key": "ip_addr", TEST_MESSAGING_BACKEND_FIELD: {"title": "Incident"}}}
+    expected_response = {
+        "id": integration.public_primary_key,
+        "team_id": None,
+        "name": "grafana",
+        "link": integration.integration_url,
+        "type": "grafana",
+        "default_route": {
+            "escalation_chain_id": None,
+            "id": default_channel_filter.public_primary_key,
+            "slack": {"channel_id": None, "enabled": True},
+            "telegram": {"id": None, "enabled": False},
+            TEST_MESSAGING_BACKEND_FIELD: {"id": None, "enabled": False},
+        },
+        "heartbeat": {
+            "link": f"{integration.integration_url}heartbeat/",
+        },
+        "templates": {
+            "grouping_key": "ip_addr",
+            "resolve_signal": None,
+            "acknowledge_signal": None,
+            "slack": {"title": None, "message": None, "image_url": None},
+            "web": {"title": None, "message": None, "image_url": None},
+            "sms": {
+                "title": None,
+            },
+            "phone_call": {
+                "title": None,
+            },
+            "telegram": {
+                "title": None,
+                "message": None,
+                "image_url": None,
+            },
+            TEST_MESSAGING_BACKEND_FIELD: {
+                "title": "Incident",
+                "message": None,
+                "image_url": None,
+            },
         },
         "maintenance_mode": None,
         "maintenance_started_at": None,
@@ -251,6 +321,11 @@ def test_update_resolve_signal_template(
                 "title": None,
             },
             "telegram": {
+                "title": None,
+                "message": None,
+                "image_url": None,
+            },
+            TEST_MESSAGING_BACKEND_FIELD: {
                 "title": None,
                 "message": None,
                 "image_url": None,
@@ -358,6 +433,11 @@ def test_update_sms_template_with_empty_dict(
                 "message": None,
                 "image_url": None,
             },
+            TEST_MESSAGING_BACKEND_FIELD: {
+                "title": None,
+                "message": None,
+                "image_url": None,
+            },
         },
         "maintenance_mode": None,
         "maintenance_started_at": None,
@@ -409,6 +489,11 @@ def test_update_integration_name(
                 "title": None,
             },
             "telegram": {
+                "title": None,
+                "message": None,
+                "image_url": None,
+            },
+            TEST_MESSAGING_BACKEND_FIELD: {
                 "title": None,
                 "message": None,
                 "image_url": None,
@@ -467,6 +552,11 @@ def test_set_default_template(
                 "title": None,
             },
             "telegram": {
+                "title": None,
+                "message": None,
+                "image_url": None,
+            },
+            TEST_MESSAGING_BACKEND_FIELD: {
                 "title": None,
                 "message": None,
                 "image_url": None,
