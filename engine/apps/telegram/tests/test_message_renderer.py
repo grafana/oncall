dd61f12a7c4667e755f81fe09495003f46a8be32
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2022-10-20 20:29:20 +0800

    Fix test_personal_message

--- engine/apps/telegram/tests/test_message_renderer.py
+++ engine/apps/telegram/tests/test_message_renderer.py
@@ -156,7 +156,7 @@ def test_personal_message(
     text = renderer.render_personal_message()
 
     assert text == (
-        f"🟠 #{alert_group.inside_organization_number}, {alert_receive_channel.config.tests['telegram']['title']}\n"
+        f"<a href='{organization.web_link_with_id}'>&#8205;</a>🟠 #{alert_group.inside_organization_number}, {alert_receive_channel.config.tests['telegram']['title']}\n"
         f"Acknowledged by {user_name}, alerts: 1\n"
         "Source: Test integration - Grafana\n"
         f"{alert_group.web_link}\n\n"
