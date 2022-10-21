0e67e8940c03312c564a33d31a0f7dbd0a139877
Joey Orlando <joseph.t.orlando@gmail.com>
2022-10-21 14:32:45 +0200

    fix more new prettier errors

--- grafana-plugin/src/components/SourceCode/SourceCode.tsx
+++ grafana-plugin/src/components/SourceCode/SourceCode.tsx
@@ -33,7 +33,13 @@ const SourceCode: FC<SourceCodeProps> = (props) => {
           {showClipboardIconOnly ? (
             <IconButton className={cx('copyIcon')} size={'lg'} name="copy" data-testid="test__copyIcon" />
           ) : (
-            <Button className={cx('copyButton')} variant="primary" size="xs" icon="copy" data-testid="test__copyIconWithText">
+            <Button
+              className={cx('copyButton')}
+              variant="primary"
+              size="xs"
+              icon="copy"
+              data-testid="test__copyIconWithText"
+            >
               Copy
             </Button>
           )}
