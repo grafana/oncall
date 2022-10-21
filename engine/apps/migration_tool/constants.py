9f44df151203f9d6cd86bdec93c3a3959db0ca7e
Joey Orlando <joey.orlando@grafana.com>
2022-10-21 14:11:47 +0200

    remove migration_tool app (#687)

--- engine/apps/migration_tool/constants.py
+++
@@ -1,7 +0,0 @@
-# amixr api url
-REQUEST_URL = "https://amixr.io/api/v1"
-
-# migration status
-NOT_STARTED = "not_started"
-IN_PROGRESS = "in_progress"
-FINISHED = "finished"
--- engine/apps/migration_tool/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .amixr_migration_task_status import AmixrMigrationTaskStatus  # noqa: F401
-from .locked_alert import LockedAlert  # noqa: F401
