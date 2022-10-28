f98945eb7c22e61086b30041fd25b338da21a1ac
Joey Orlando <joseph.t.orlando@gmail.com>
2022-10-28 14:49:36 +0200

    finish Unauthorized component

--- grafana-plugin/src/components/Unauthorized/index.tsx
+++ grafana-plugin/src/components/Unauthorized/index.tsx
@@ -1,11 +1,40 @@
 import React, { FC } from 'react';
 
-// import cn from 'classnames/bind';
+import { OrgRole } from '@grafana/data';
+import { VerticalGroup } from '@grafana/ui';
+import cn from 'classnames/bind';
+import { contextSrv } from 'grafana/app/core/core';
 
-// import styles from './Unauthorized.module.css';
+import Text from 'components/Text/Text';
+import { UserAction } from 'utils/authorization';
 
-// const cx = cn.bind(styles);
+import styles from './Unauthorized.module.css';
 
-const Unauthorized: FC = (_props) => <p>You don't have access!</p>;
+const cx = cn.bind(styles);
+
+type Props = {
+  requiredUserAction: UserAction;
+};
+
+const Unauthorized: FC<Props> = ({ requiredUserAction: { permission, fallbackMinimumRoleRequired } }) => (
+  <div className={cx('not-found')}>
+    <VerticalGroup spacing="lg" align="center">
+      <Text.Title level={1} className={cx('error-code')}>
+        403
+      </Text.Title>
+      <Text.Title level={4}>
+        You do not have access to view this page.{' '}
+        {contextSrv.accessControlEnabled()
+          ? `You are missing the ${permission} permission.`
+          : `You must be at least a${
+              fallbackMinimumRoleRequired === OrgRole.Viewer ? '' : 'n'
+            } ${fallbackMinimumRoleRequired}.`}
+        <br />
+        <br />
+        Please contact your organization administrator to request access.
+      </Text.Title>
+    </VerticalGroup>
+  </div>
+);
 
 export default Unauthorized;
