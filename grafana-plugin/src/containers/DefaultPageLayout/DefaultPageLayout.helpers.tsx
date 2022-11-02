c01e7d202b8cae167afab12ba9a764ecfeb3b412
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2022-11-02 12:47:52 +0100

    Changes after review

--- grafana-plugin/src/containers/DefaultPageLayout/DefaultPageLayout.helpers.tsx
+++ grafana-plugin/src/containers/DefaultPageLayout/DefaultPageLayout.helpers.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 import PluginLink from 'components/PluginLink/PluginLink';
 import { Team } from 'models/team/team.types';
-import { RootStore } from 'state';
-import { AppFeature } from 'state/features';
 
 import { SlackError } from './DefaultPageLayout.types';
 
-export function getSlackMessage(slackError: SlackError, team: Team, store: RootStore) {
+export function getSlackMessage(slackError: SlackError, team: Team, hasLiveSettingsFeature: boolean) {
   if (slackError === SlackError.WRONG_WORKSPACE) {
     return (
       <>
@@ -32,7 +30,7 @@ export function getSlackMessage(slackError: SlackError, team: Team, store: RootS
     return (
       <>
         An error has occurred with Slack authentication.{' '}
-        {store.hasFeature(AppFeature.LiveSettings) && (
+        {hasLiveSettingsFeature && (
           <>
             <PluginLink query={{ page: 'live-settings' }}>Check ENV variables</PluginLink> related to Slack and try
             again please.
