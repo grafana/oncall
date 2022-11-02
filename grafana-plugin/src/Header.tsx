2af990098ba4a3db613f955fff9d5f008c9da4e5
Rares Mardare <rares.mardare@grafana.com>
2022-11-02 14:03:40 +0200

    header

--- grafana-plugin/src/Header.tsx
+++ grafana-plugin/src/Header.tsx
@@ -1,6 +1,28 @@
-import GrafanaTeamSelect from 'containers/GrafanaTeamSelect/GrafanaTeamSelect';
 import React from 'react';
 
+import GrafanaTeamSelect from 'containers/GrafanaTeamSelect/GrafanaTeamSelect';
+
+import { APP_SUBTITLE } from 'utils/consts';
+
+import logo from 'img/logo.svg';
+
 export default function Header({ page }: { page: string }) {
-  return  <GrafanaTeamSelect currentPage={page} />
+  return (
+    <div className="page-container">
+      <div className="page-header">
+        <div className="page-header__inner">
+          <span className="page-header__logo">
+            <img className="page-header__img" src={logo} alt="Grafana OnCall" />
+          </span>
+
+          <div className="page-header__info-block">
+            <h1 className="page-header__title">Grafana OnCall</h1>
+            <div className="page-header__sub-title">{APP_SUBTITLE}</div>
+          </div>
+
+          <GrafanaTeamSelect currentPage={page} />
+        </div>
+      </div>
+    </div>
+  );
 }
