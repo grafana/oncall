0a98758d623a9a522526b32b7076678b074eff00
Vadim Stepanov <vadimkerr@gmail.com>
2022-11-03 15:28:29 +0000

    add test for get_from_email

--- engine/apps/email/tests/test_notify_user.py
+++ engine/apps/email/tests/test_notify_user.py
@@ -6,7 +6,7 @@ from django.core import mail
 from django.core.mail.backends.locmem import EmailBackend
 
 from apps.base.models import UserNotificationPolicy, UserNotificationPolicyLogRecord
-from apps.email.tasks import notify_user_async
+from apps.email.tasks import get_from_email, notify_user_async
 from apps.user_management.subscription_strategy.free_public_beta_subscription_strategy import (
     FreePublicBetaSubscriptionStrategy,
 )
@@ -224,3 +224,38 @@ def test_notify_user_from_email_cloud(
 
     notify_user_async(user.pk, alert_group.pk, notification_policy.pk)
     assert mail.outbox[0].from_email == "oncall@slug.grafana.net"
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "license_name,email_host_user,email_from_address,expected",
+    [
+        ("Cloud", "user", "from_address", "from_address"),
+        ("OpenSource", "user", "from_address", "from_address"),
+        ("Cloud", "user", None, "oncall@slug.grafana.net"),
+        ("Cloud", None, None, "oncall@slug.grafana.net"),
+        ("OpenSource", "user", None, "user"),
+        ("OpenSource", None, None, None),
+    ],
+)
+def test_get_from_email(
+    settings,
+    make_organization,
+    make_user_for_organization,
+    license_name,
+    email_host_user,
+    email_from_address,
+    expected,
+):
+    settings.EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+    settings.EMAIL_HOST = "test"
+    settings.EMAIL_HOST_PASSWORD = "password"
+
+    settings.LICENSE = license_name
+    settings.EMAIL_HOST_USER = email_host_user
+    settings.EMAIL_FROM_ADDRESS = email_from_address
+
+    organization = make_organization(stack_slug="slug")
+    user = make_user_for_organization(organization)
+
+    assert get_from_email(user) == expected
