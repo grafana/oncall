dd249c0cae8947d11ddf7f28ab77fd9419f81750
Vadim Stepanov <vadimkerr@gmail.com>
2022-11-21 16:15:18 +0000

    move mobile auth related code to the mobile_app Django app

--- engine/apps/mobile_app/models.py
+++ engine/apps/mobile_app/models.py
@@ -4,9 +4,33 @@ from django.db import models
 from django.utils import timezone
 
 from apps.auth_token import constants, crypto
-from apps.auth_token.constants import MOBILE_APP_AUTH_VERIFICATION_TOKEN_TIMEOUT_SECONDS
 from apps.auth_token.models import BaseAuthToken
 from apps.user_management.models import Organization, User
+
+
+class MobileAppAuthToken(BaseAuthToken):
+    user = models.ForeignKey(
+        to=User, null=False, blank=False, related_name="mobile_app_auth_tokens", on_delete=models.CASCADE
+    )
+    organization = models.ForeignKey(
+        to=Organization, null=False, blank=False, related_name="mobile_app_auth_tokens", on_delete=models.CASCADE
+    )
+
+    @classmethod
+    def create_auth_token(cls, user: User, organization: Organization) -> Tuple["MobileAppAuthToken", str]:
+        token_string = crypto.generate_token_string()
+        digest = crypto.hash_token_string(token_string)
+
+        instance = cls.objects.create(
+            token_key=token_string[: constants.TOKEN_KEY_LENGTH],
+            digest=digest,
+            user=user,
+            organization=organization,
+        )
+        return instance, token_string
+
+
+MOBILE_APP_AUTH_VERIFICATION_TOKEN_TIMEOUT_SECONDS = 60
 
 
 def get_expire_date():
