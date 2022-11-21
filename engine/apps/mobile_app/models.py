b31ad37b48d039f7ca06cd9d1a77009dcc510a60
Vadim Stepanov <vadimkerr@gmail.com>
2022-11-21 16:17:30 +0000

    move mobile auth related code to the mobile_app Django app

--- engine/apps/mobile_app/models.py
+++ engine/apps/mobile_app/models.py
@@ -7,29 +7,6 @@ from apps.auth_token import constants, crypto
 from apps.auth_token.models import BaseAuthToken
 from apps.user_management.models import Organization, User
 
-
-class MobileAppAuthToken(BaseAuthToken):
-    user = models.ForeignKey(
-        to=User, null=False, blank=False, related_name="mobile_app_auth_tokens", on_delete=models.CASCADE
-    )
-    organization = models.ForeignKey(
-        to=Organization, null=False, blank=False, related_name="mobile_app_auth_tokens", on_delete=models.CASCADE
-    )
-
-    @classmethod
-    def create_auth_token(cls, user: User, organization: Organization) -> Tuple["MobileAppAuthToken", str]:
-        token_string = crypto.generate_token_string()
-        digest = crypto.hash_token_string(token_string)
-
-        instance = cls.objects.create(
-            token_key=token_string[: constants.TOKEN_KEY_LENGTH],
-            digest=digest,
-            user=user,
-            organization=organization,
-        )
-        return instance, token_string
-
-
 MOBILE_APP_AUTH_VERIFICATION_TOKEN_TIMEOUT_SECONDS = 60
 
 
@@ -61,6 +38,28 @@ class MobileAppVerificationToken(BaseAuthToken):
     @classmethod
     def create_auth_token(cls, user: User, organization: Organization) -> Tuple["MobileAppVerificationToken", str]:
         token_string = crypto.generate_short_token_string()
+        digest = crypto.hash_token_string(token_string)
+
+        instance = cls.objects.create(
+            token_key=token_string[: constants.TOKEN_KEY_LENGTH],
+            digest=digest,
+            user=user,
+            organization=organization,
+        )
+        return instance, token_string
+
+
+class MobileAppAuthToken(BaseAuthToken):
+    user = models.ForeignKey(
+        to=User, null=False, blank=False, related_name="mobile_app_auth_tokens", on_delete=models.CASCADE
+    )
+    organization = models.ForeignKey(
+        to=Organization, null=False, blank=False, related_name="mobile_app_auth_tokens", on_delete=models.CASCADE
+    )
+
+    @classmethod
+    def create_auth_token(cls, user: User, organization: Organization) -> Tuple["MobileAppAuthToken", str]:
+        token_string = crypto.generate_token_string()
         digest = crypto.hash_token_string(token_string)
 
         instance = cls.objects.create(
