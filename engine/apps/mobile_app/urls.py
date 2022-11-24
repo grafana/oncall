b72f52582cb0c271a48a2d2bc3dcb3402fe5c1c4
Vadim Stepanov <vadimkerr@gmail.com>
2022-11-24 13:53:50 +0000

    move MobileAppAuthToken view to mobile_app

--- engine/apps/mobile_app/urls.py
+++ engine/apps/mobile_app/urls.py
@@ -1,9 +1,13 @@
-from apps.mobile_app.views import APNSDeviceAuthorizedViewSet, GCMDeviceAuthorizedViewSet
-from common.api_helpers.optional_slash_router import OptionalSlashRouter
+from apps.mobile_app.views import APNSDeviceAuthorizedViewSet, GCMDeviceAuthorizedViewSet, MobileAppAuthTokenAPIView
+from common.api_helpers.optional_slash_router import OptionalSlashRouter, optional_slash_path
 
+app_name = "mobile_app"
 router = OptionalSlashRouter()
 
-router.register("apns", APNSDeviceAuthorizedViewSet)
-router.register("gcm", GCMDeviceAuthorizedViewSet)
+router.register("apns", APNSDeviceAuthorizedViewSet, basename="apns")
+router.register("gcm", GCMDeviceAuthorizedViewSet, basename="gcm")
 
-urlpatterns = router.urls
+urlpatterns = [
+    *router.urls,
+    optional_slash_path("auth_token", MobileAppAuthTokenAPIView.as_view(), name="auth_token"),
+]
