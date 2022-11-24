7d7cfb533a72cc1374f80c46adea1fef36ef5fa4
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2022-11-24 15:01:00 +0800

    Fix tests

--- engine/apps/api/tests/test_alert_receive_channel_template.py
+++ engine/apps/api/tests/test_alert_receive_channel_template.py
@@ -295,9 +295,12 @@ def test_update_alert_receive_channel_templates(
 
     assert response.status_code == status.HTTP_200_OK
     existing_templates_data = response.json()
+
     # leave only templates-related fields
     del existing_templates_data["id"]
     del existing_templates_data["verbal_name"]
+    del existing_templates_data["payload_example"]
+
     new_templates_data = {}
     for template_name, template_value in existing_templates_data.items():
         new_templates_data[template_name] = template_update_func(template_value)
