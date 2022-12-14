5b8bedc8901ca72c70b19902526eebcbc568a683
Joey Orlando <joseph.t.orlando@gmail.com>
2022-12-14 14:57:37 +0100

    swap out django-push-notifications for fcm-django

--- dev/README.md
+++ dev/README.md
@@ -13,6 +13,7 @@
   - [Could not build wheels for cryptography which use PEP 517 and cannot be installed directly](#could-not-build-wheels-for-cryptography-which-use-pep-517-and-cannot-be-installed-directly)
   - [django.db.utils.OperationalError: (1366, "Incorrect string value")](#djangodbutilsoperationalerror-1366-incorrect-string-value)
   - [/bin/sh: line 0: cd: grafana-plugin: No such file or directory](#binsh-line-0-cd-grafana-plugin-no-such-file-or-directory)
+  - [Encountered error while trying to install package - grpcio](#encountered-error-while-trying-to-install-package---grpcio)
 - [IDE Specific Instructions](#ide-specific-instructions)
   - [PyCharm](#pycharm)
 
@@ -278,6 +279,34 @@ clear everything in docker by resetting or:
 
 ```bash
 make cleanup
+```
+
+### Encountered error while trying to install package - grpcio
+
+**Problem:**
+
+We are currently using a library, `fcm-django`, which has a dependency on `grpcio`. Google does not provide `grpcio`
+wheels built for Apple Silicon Macs. The best solution so far has been to use a `conda` virtualenv. There's apparently
+a lot of community work put into making packages play well with M1/arm64 architecture.
+
+```bash
+pip install -r requirements.txt
+...
+   note: This error originates from a subprocess, and is likely not a problem with pip.
+error: legacy-install-failure
+
+× Encountered error while trying to install package.
+╰─> grpcio
+...
+```
+
+**Solution:**
+
+Use a `conda` virtualenv, and then run the following when installing the engine dependencies/
+[See here for more details](https://stackoverflow.com/a/74307636/3902555)
+
+```bash
+GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1 GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1 pip install -r requirements.txt
 ```
 
 ## IDE Specific Instructions
--- engine/apps/mobile_app/tasks.py
+++ engine/apps/mobile_app/tasks.py
@@ -1,6 +1,17 @@
 from celery.utils.log import get_task_logger
 from django.conf import settings
-from push_notifications.models import GCMDevice
+from fcm_django.models import FCMDevice
+from firebase_admin.messaging import (
+    AndroidConfig,
+    AndroidNotification,
+    APNSConfig,
+    APNSPayload,
+    Aps,
+    ApsAlert,
+    CriticalSound,
+    Message,
+    Notification,
+)
 
 from apps.alerts.models import AlertGroup
 from apps.mobile_app.alert_rendering import get_push_notification_message
@@ -34,10 +45,10 @@ def notify_user_async(user_pk, alert_group_pk, notification_policy_pk, critical)
         logger.warning(f"User notification policy {notification_policy_pk} does not exist")
         return
 
-    gcm_devices_to_notify = GCMDevice.objects.filter(user=user)
+    device_to_notify = FCMDevice.objects.filter(user=user).first()
 
     # create an error log in case user has no devices set up
-    if not gcm_devices_to_notify.exists():
+    if not device_to_notify:
         UserNotificationPolicyLogRecord.objects.create(
             author=user,
             type=UserNotificationPolicyLogRecord.TYPE_PERSONAL_NOTIFICATION_FAILED,
@@ -47,66 +58,76 @@ def notify_user_async(user_pk, alert_group_pk, notification_policy_pk, critical)
             notification_step=notification_policy.step,
             notification_channel=notification_policy.notify_by,
         )
-        logger.info(f"Error while sending a mobile push notification: user {user_pk} has no devices set up")
+        logger.info(f"Error while sending a mobile push notification: user {user_pk} has no device set up")
         return
 
     message = get_push_notification_message(alert_group)
     thread_id = f"{alert_group.channel.organization.public_primary_key}:{alert_group.public_primary_key}"
     alert_title = f"Critical page: {message}" if critical else message
-    # TODO: this is what will be shown in the app badge (little red bubble w/ number)
-    # is this the proper thing we want to show? or is there a more valuable metric to use?
-    number_of_alert_groups_firing = alert_group.alerts.count()
+    number_of_alerts = alert_group.alerts.count()
 
-    extra = {
-        "orgId": alert_group.channel.organization.public_primary_key,
-        "orgName": alert_group.channel.organization.stack_slug,
-        "alertGroupId": alert_group.public_primary_key,
-        "status": alert_group.status,
-        "apns": {
-            # for possible iOS values, see here
-            # https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/generating_a_remote_notification#2943365
-            "aps": {
-                "badge": number_of_alert_groups_firing,
-                "sound": {
-                    "critical": 1 if critical else 0,
-                    "name": "ambulance.aiff" if critical else "bingbong.aiff",
-                    "volume": 1,
-                },
-                "interruption-level": "critical" if critical else "time-sensitive",
-                "alert": {
-                    "title": alert_title,
-                    # TODO: do we want these attributes?
-                    # "subtitle": "",
-                    # "body": "",
-                },
-            },
+    # TODO: we should update this to check if FCM_RELAY is set and conditionally make a call here..
+
+    message = Message(
+        token=device_to_notify.registration_id,
+        notification=Notification(
+            title=alert_title,
+            body="this is the alert body",
+        ),
+        data={
+            # from the docs..
+            # A dictionary of data fields (optional). All keys and values in the dictionary must be strings
+            #
+            # alert_group.status is an int so it must be casted...
+            "orgId": alert_group.channel.organization.public_primary_key,
+            "orgName": alert_group.channel.organization.stack_slug,
+            "alertGroupId": alert_group.public_primary_key,
+            "status": str(alert_group.status),
         },
-        "android": {
-            # for possible Android settings, see here
-            # https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages#androidconfig
-            "priority": "HIGH",
-            "notification": {
-                "title": alert_title,
-                # TODO: do we want the body attribute?
-                "body": "",
-                "ticker": alert_title,
-                "sound": "ambulance.mp3" if critical else "bingbong.mp3",
-                "click_action": "CRITICAL_NOTIFICATION" if critical else "DEFAULT",
-                "visibility": "PUBLIC",
-                "notification_priority": "PRIORITY_MAX" if critical else "PRIORITY_HIGH",
-                "notification_count": number_of_alert_groups_firing,
-                # NOTE: we'll ignore vibrate_timings for now, but we could use it to make a weird vibration
-                # pattern for critical notifications
-                "vibrate_timings": [],
-            },
-        },
-    }
-
-    logger.info(f"Sending push notification with message: {message}; thread-id: {thread_id}; extra: {extra}")
-
-    fcm_response = gcm_devices_to_notify.send_message(
-        message, thread_id=thread_id, category="USER_NEW_ALERT_GROUP", extra=extra
+        android=AndroidConfig(
+            priority="high",
+            notification=AndroidNotification(
+                ticker=alert_title,
+                tag=thread_id,
+                sound="ambulance.mp3" if critical else "bingbong.mp3",
+                default_sound=False,
+                click_action="CRITICAL_NOTIFICATION" if critical else "DEFAULT",
+                visibility="public",
+                priority="max" if critical else "high",
+                notification_count=number_of_alerts,
+                # NOTE: we'll ignore light_settings and vibrate_timings_millis for now
+                # but we could use it to make a weird vibration/light
+                # patterns for critical notifications
+                # light_settings=LightSettings(),
+                # vibrate_timings_millis=[],
+            ),
+        ),
+        apns=APNSConfig(
+            payload=APNSPayload(
+                aps=Aps(
+                    thread_id=thread_id,
+                    badge=number_of_alerts,
+                    alert=ApsAlert(
+                        title=alert_title,
+                        subtitle="yooo this is a subtitle",
+                        body="hello this is the body",
+                    ),
+                    sound=CriticalSound(
+                        critical=1 if critical else 0,
+                        name="ambulance.aiff" if critical else "bingbong.aiff",
+                        volume=1,
+                    ),
+                    custom_data={
+                        "interruption-level": "critical" if critical else "time-sensitive",
+                    },
+                ),
+            ),
+        ),
     )
+
+    logger.info(f"Sending push notification with message: {message}; thread-id: {thread_id};")
+
+    fcm_response = device_to_notify.send_message(message)
 
     # NOTE: we may want to further handle the response from FCM, but for now lets simply log it out
     # https://firebase.google.com/docs/cloud-messaging/http-server-ref#interpret-downstream
