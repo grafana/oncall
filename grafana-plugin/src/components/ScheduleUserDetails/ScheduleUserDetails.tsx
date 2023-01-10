0ca1e3ad5c2fa46cc9022c0f11fa4b755fc023e8
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2023-01-10 13:25:22 +0100

    slack link has been added

--- grafana-plugin/src/components/ScheduleUserDetails/ScheduleUserDetails.tsx
+++ grafana-plugin/src/components/ScheduleUserDetails/ScheduleUserDetails.tsx
@@ -9,6 +9,7 @@ import Text from 'components/Text/Text';
 import { isInWorkingHours } from 'components/WorkingHours/WorkingHours.helpers';
 import { getTzOffsetString } from 'models/timezone/timezone.helpers';
 import { User } from 'models/user/user.types';
+import { useStore } from 'state/useStore';
 
 import styles from './ScheduleUserDetails.module.css';
 
@@ -25,8 +26,11 @@ const ScheduleUserDetails: FC<ScheduleUserDetailsProps> = (props) => {
   const userMoment = currentMoment.tz(user.timezone);
   const userOffsetHoursStr = getTzOffsetString(userMoment);
   const isInWH = isInWorkingHours(userMoment, user.working_hours);
+  const store = useStore();
 
-  console.log('USER', user);
+  const { teamStore } = store;
+  let slackWorkspaceNameOrigin = teamStore.currentTeam.slack_team_identity.cached_name;
+  const slackWorkspaceName = slackWorkspaceNameOrigin.replace(/[^0-9a-z]/gi, '');
   return (
     <div className={cx('root')}>
       <VerticalGroup spacing="md">
@@ -77,16 +81,21 @@ const ScheduleUserDetails: FC<ScheduleUserDetailsProps> = (props) => {
             </a>{' '}
           </Text>
           {user.slack_user_identity && (
-            <Text>
-              <Icon name="slack" /> <Text type="link">{user.slack_user_identity.slack_login}</Text>
+            <Text type="secondary">
+              <Icon name="slack" />
+              <a href={`https://${slackWorkspaceName}.slack.com/team/${user.slack_user_identity.slack_id}`}>
+                <Text type="link">{user.slack_user_identity.slack_login}</Text>
+              </a>{' '}
             </Text>
           )}
           {user.telegram_configuration && (
-            <Text>
+            <Text type="secondary">
               <Icon name="message" />{' '}
-              <a href={`https://t.me/${user.telegram_configuration.telegram_nick_name}`}>
-                <Text type="link">{user.telegram_configuration.telegram_nick_name}</Text>
-              </a>{' '}
+              <Text type="link">
+                <a href={`https://t.me/${user.telegram_configuration.telegram_nick_name}`}>
+                  <Text>{user.telegram_configuration.telegram_nick_name}</Text>
+                </a>{' '}
+              </Text>
             </Text>
           )}
           {!user.hide_phone_number && user.verified_phone_number && <Text>Phone: {user.verified_phone_number}</Text>}
