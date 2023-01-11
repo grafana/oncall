231c0f45a30112137d75009c22f422db6b630873
Vadim Stepanov <vadimkerr@gmail.com>
2023-01-11 11:42:01 +0000

    Re-implement FCM relay after introducing firebase (#1121)
    This PR changes how `FCMRelayView` handles push notifications from OSS
    instances, also changing how the mobile app backend sends push
    notifications.

--- engine/apps/mobile_app/fcm_relay.py
+++ engine/apps/mobile_app/fcm_relay.py
@@ -1,29 +1,72 @@
-# from firebase_admin.messaging import Message
-# from fcm_django.models import FCMDevice
+import logging
+
+from fcm_django.models import FCMDevice
+from firebase_admin.messaging import APNSConfig, APNSPayload, Aps, ApsAlert, CriticalSound, Message
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-REQUIRED_FIELDS = {"registration_ids", "notification", "data"}
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
-# TODO: update thie
 class FCMRelayView(APIView):
+    # TODO: use public API authentication (then it would be required to connect to a cloud instance to use the app)
+    authentication_classes = []
+    permission_classes = []
+
     def post(self, request):
         """
-        This view accepts requests from OSS instances of Grafana OnCall and forwards these requests to FCM.
-        Requests will be sent with the FCM_API_KEY configured in server settings
-        (see PUSH_NOTIFICATIONS_SETTINGS in settings/base.py)
+        This view accepts push notifications from OSS instances and forwards these requests to FCM.
+        Requests to this endpoint come from OSS instances: apps.mobile_app.tasks.send_push_notification_to_fcm_relay
         """
 
-        if not REQUIRED_FIELDS.issubset(request.data.keys()):
+        try:
+            token = request.data["token"]
+            data = request.data["data"]
+        except KeyError:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        # registration_ids = request.data["registration_ids"]
-        # data = {
-        #     **request.data["data"],
-        #     **request.data["notification"],
-        # }
+        message = Message(token=token, data=data, apns=get_apns(request.data))
 
-        # return FCMDevice.objects.send_message(Message(), False, ["registration_ids"])
-        return "TODO:"
+        logger.debug(f"Sending message to FCM: {message}")
+        result = FCMDevice(registration_id=token).send_message(message)
+        logger.debug(f"FCM response: {result}")
+
+        return Response(status=status.HTTP_200_OK)
+
+
+def get_apns(data):
+    """
+    Create APNSConfig object from JSON payload from OSS instance.
+    """
+    aps = data.get("apns", {}).get("payload", {}).get("aps", {})
+    if not aps:
+        return None
+
+    thread_id = aps.get("thread-id")
+    badge = aps.get("badge")
+
+    alert = aps.get("alert")
+    if isinstance(alert, dict):
+        alert = ApsAlert(**alert)
+
+    sound = aps.get("sound")
+    if isinstance(sound, dict):
+        sound = CriticalSound(**sound)
+
+    # remove all keys from "aps" so it can be used for custom_data
+    for key in ["thread-id", "badge", "alert", "sound"]:
+        aps.pop(key, None)
+
+    return APNSConfig(
+        payload=APNSPayload(
+            aps=Aps(
+                thread_id=thread_id,
+                badge=badge,
+                alert=alert,
+                sound=sound,
+                custom_data=aps,
+            )
+        )
+    )
