90c97b0abbfc932c5cf2fb04060bc9bdcf2d6c7b
Joey Orlando <joey.orlando@grafana.com>
2023-01-11 13:05:32 +0100

    Update grafana-plugin/src/containers/UserSettings/parts/tabs/SlackTab/SlackTab.tsx

--- grafana-plugin/src/containers/UserSettings/parts/tabs/SlackTab/SlackTab.tsx
+++ grafana-plugin/src/containers/UserSettings/parts/tabs/SlackTab/SlackTab.tsx
@@ -25,7 +25,7 @@ export const SlackTab = () => {
         <VerticalGroup align="center" spacing="lg">
           <SlackNewIcon />
           <Text>
-            Personal Slack connection will allow you to manage alert groups in your connected team Internal Slack
+            Personal Slack connection will allow you to manage alert groups in your connected team's Internal Slack
             workspace.
           </Text>
           <Text>To setup personal Slack click the button below, choose workspace and click Allow.</Text>
