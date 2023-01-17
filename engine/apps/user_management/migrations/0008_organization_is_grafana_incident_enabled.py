f83cff85d7655033102efd22ad9ac6831bf0e1a3
Julia <ferril.darkdiver@gmail.com>
2023-01-17 10:05:12 +0100

    Fix migrations

--- engine/apps/user_management/migrations/0008_organization_is_grafana_incident_enabled.py
+++ engine/apps/user_management/migrations/0008_organization_is_grafana_incident_enabled.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user_management', '0006_organization_uuid'),
+        ('user_management', '0007_organization_deleted_at'),
     ]
 
     operations = [
