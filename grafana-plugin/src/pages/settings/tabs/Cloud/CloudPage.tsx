6b87ad74e9695fc77ae3c55fe0ea7ce9dfab2aec
Vadim Stepanov <vadimkerr@gmail.com>
2023-01-19 11:15:56 +0000

    Enforce cloud connection to send push notifications on OSS (#1132)
    This PR modifies how OSS instances send mobile app push notifications.
    It also adds frontend warnings when user is trying to use the mobile app
    without connecting to cloud.
    
    - [x] Add public API authentication to `FCMRelayView` and throttle the
    view to 300 push notifications per instance per minute. This is similar
    to how SMS and phone call notifications work on OSS instances.
    - [x] Add frontend warnings based on cloud connectivity
    - [x] Fix/add frontend tests
    - [x] Add tests for FCMRelayView and mobile app backend
    
    ## Screenshots
    
    When a user tries to connect the mobile app in his settings and cloud is
    not connected (clicking "Connect Cloud OnCall" redirects to the "Cloud"
    tab):
    
    <img width="1088" alt="Screenshot 2023-01-12 at 18 48 58"
    src="https://user-images.githubusercontent.com/20116910/212156591-86906020-eddf-43f1-9402-7ebb7547c7e6.png">
    
    When a user tries to use mobile push notifications as a personal
    notification step and cloud is not connected:
    
    <img width="764" alt="Screenshot 2023-01-12 at 19 01 10"
    src="https://user-images.githubusercontent.com/20116910/212157580-9abb0758-79ad-4316-b8cd-15b4fff01502.png">
    
    Now on the "Cloud" tab there's some info about the mobile app (the last
    section at the bottom of the page):
    
    <img width="1245" alt="Screenshot 2023-01-12 at 18 49 10"
    src="https://user-images.githubusercontent.com/20116910/212156997-c8b70dd5-bf15-4bc7-8eb8-9decdb8ecc80.png">
    
    After connecting to the cloud instance, everything goes back to active
    and it's now possible to connect the mobile app:
    
    <img width="1091" alt="Screenshot 2023-01-12 at 19 08 27"
    src="https://user-images.githubusercontent.com/20116910/212158811-60d49888-4714-4c0e-850f-3ff6a11a117a.png">
    
    After connecting the app the warning is gone:
    
    <img width="764" alt="Screenshot 2023-01-12 at 19 07 00"
    src="https://user-images.githubusercontent.com/20116910/212158614-677ab889-127f-4d64-bacc-0c26887f3097.png">

--- grafana-plugin/src/pages/settings/tabs/Cloud/CloudPage.tsx
+++ grafana-plugin/src/pages/settings/tabs/Cloud/CloudPage.tsx
@@ -11,6 +11,7 @@ import Text from 'components/Text/Text';
 import WithConfirm from 'components/WithConfirm/WithConfirm';
 import { CrossCircleIcon, HeartIcon } from 'icons';
 import { Cloud } from 'models/cloud/cloud.types';
+import { AppFeature } from 'state/features';
 import { WithStoreProps } from 'state/types';
 import { useStore } from 'state/useStore';
 import { withMobXProviderContext } from 'state/withStore';
@@ -60,9 +61,10 @@ const CloudPage = observer((props: CloudPageProps) => {
     setApiKeyError(false);
   }, []);
 
-  const disconnectCloudOncall = () => {
+  const disconnectCloudOncall = async () => {
     setCloudIsConnected(false);
-    store.cloudStore.disconnectToCloud();
+    await store.cloudStore.disconnectToCloud();
+    await store.cloudStore.loadCloudConnectionStatus();
   };
 
   const connectToCloud = async () => {
@@ -81,6 +83,7 @@ const CloudPage = observer((props: CloudPageProps) => {
           const heartbeatData: { link: string } = await store.cloudStore.getCloudHeartbeat();
           setheartbeatLink(heartbeatData?.link);
         }
+        await store.cloudStore.loadCloudConnectionStatus();
       });
   };
 
@@ -312,6 +315,19 @@ const CloudPage = observer((props: CloudPageProps) => {
           </VerticalGroup>
         )}
       </Block>
+      {store.hasFeature(AppFeature.MobileApp) && (
+        <Block bordered withBackground className={cx('info-block')}>
+          <VerticalGroup>
+            <Text.Title level={4}>
+              <Icon name="mobile-android" className={cx('block-icon')} size="lg" /> Mobile app push notifications
+            </Text.Title>
+            <Text type="secondary">
+              Connecting to Cloud OnCall enables sending push notifications on mobile devices using the Grafana OnCall
+              mobile app.
+            </Text>
+          </VerticalGroup>
+        </Block>
+      )}
     </VerticalGroup>
   );
 
@@ -358,6 +374,19 @@ const CloudPage = observer((props: CloudPageProps) => {
           <Text type="secondary">Users matched between OSS and Cloud OnCall currently unavailable.</Text>
         </VerticalGroup>
       </Block>
+      {store.hasFeature(AppFeature.MobileApp) && (
+        <Block bordered withBackground className={cx('info-block')}>
+          <VerticalGroup>
+            <Text.Title level={4}>
+              <Icon name="mobile-android" className={cx('block-icon')} size="lg" /> Mobile app push notifications
+            </Text.Title>
+            <Text type="secondary">
+              Connecting to Cloud OnCall enables sending push notifications on mobile devices using the Grafana OnCall
+              mobile app.
+            </Text>
+          </VerticalGroup>
+        </Block>
+      )}
     </VerticalGroup>
   );
 
