d0dca4005b38b069abc52be46fce4b2675a97042
Ildar Iskhakov <ildar.iskhakov@grafana.com>
2023-01-24 14:52:31 +0800

    Fix is_installed

--- engine/apps/grafana_plugin/views/sync.py
+++ engine/apps/grafana_plugin/views/sync.py
@@ -23,25 +23,26 @@ class PluginSyncView(GrafanaHeadersMixin, APIView):
         stack_id = self.instance_context["stack_id"]
         org_id = self.instance_context["org_id"]
         is_installed = False
+        allow_signup = True
         try:
             organization = Organization.objects.get(stack_id=stack_id, org_id=org_id)
+            if organization.api_token_status == Organization.API_TOKEN_STATUS_OK:
+                is_installed = True
+
             is_user = PluginAuthentication.is_user_from_request_present_in_organization(request, organization)
-            if is_user:
-                if organization.api_token_status == Organization.API_TOKEN_STATUS_OK:
-                    is_installed = True
-            else:
+            if not is_user:
                 organization.api_token_status = Organization.API_TOKEN_STATUS_PENDING
                 organization.save(update_fields=["api_token_status"])
+
+            if not organization:
+                DynamicSetting = apps.get_model("base", "DynamicSetting")
+                allow_signup = DynamicSetting.objects.get_or_create(
+                    name="allow_plugin_organization_signup", defaults={"boolean_value": True}
+                )[0].boolean_value
+
             plugin_sync_organization_async.apply_async((organization.pk,))
         except Organization.DoesNotExist:
             logger.info(f"Organization for stack {stack_id} org {org_id} was not found")
-
-        allow_signup = True
-        if not organization:
-            DynamicSetting = apps.get_model("base", "DynamicSetting")
-            allow_signup = DynamicSetting.objects.get_or_create(
-                name="allow_plugin_organization_signup", defaults={"boolean_value": True}
-            )[0].boolean_value
 
         return Response(
             status=status.HTTP_202_ACCEPTED,
