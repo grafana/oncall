6bc57fe11ad8221cef2c20bd23296b3ba0136600
Ildar Iskhakov <ildar.iskhakov@grafana.com>
2023-02-01 00:22:21 +0800

    Cache render_for_web and render_for_classic_markdown for aler and alertgroup

--- engine/apps/api/serializers/alert.py
+++ engine/apps/api/serializers/alert.py
@@ -1,10 +1,37 @@
+from django.core.cache import cache
+from django.utils import timezone
 from rest_framework import serializers
 
 from apps.alerts.incident_appearance.renderers.web_renderer import AlertWebRenderer
 from apps.alerts.models import Alert
 
 
-class AlertSerializer(serializers.ModelSerializer):
+class AlertFieldsCacheSerializerMixin:
+    @classmethod
+    def get_or_set_cached_web_template_field(
+        cls,
+        obj,
+        field_name,
+        renderer_class,
+        cache_lifetime=60 * 60 * 24,
+    ):
+        CACHE_KEY = f"{field_name}_alert_{obj.id}"
+        cached_field = cache.get(CACHE_KEY, None)
+
+        web_templates_modified_at = obj.channel.web_templates_modified_at
+
+        if cached_field is not None and (
+            web_templates_modified_at is None or cached_field.get("cache_created_at") > web_templates_modified_at
+        ):
+            field = cached_field.get(field_name)
+        else:
+            field = renderer_class(obj, obj.last_alert).render()
+            cache.set(CACHE_KEY, {"cache_created_at": timezone.now(), field_name: field}, cache_lifetime)
+
+        return field
+
+
+class AlertSerializer(AlertFieldsCacheSerializerMixin, serializers.ModelSerializer):
     id = serializers.CharField(read_only=True, source="public_primary_key")
     render_for_web = serializers.SerializerMethodField()
 
@@ -18,7 +45,13 @@ class AlertSerializer(serializers.ModelSerializer):
         ]
 
     def get_render_for_web(self, obj):
-        return AlertWebRenderer(obj).render()
+        if not obj.last_alert:
+            return {}
+        return AlertFieldsCacheSerializerMixin.get_or_set_web_template_field(
+            obj,
+            "render_for_web",
+            AlertWebRenderer,
+        )
 
 
 class AlertRawSerializer(serializers.ModelSerializer):
