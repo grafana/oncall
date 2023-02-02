bfae605785bab50cea9c135751121f57384457ec
Ildar Iskhakov <ildar.iskhakov@grafana.com>
2023-02-02 19:59:16 +0800

    Optmize alert and alert group public api endpoints, add filter by id

--- engine/apps/public_api/serializers/incidents.py
+++ engine/apps/public_api/serializers/incidents.py
@@ -13,12 +13,11 @@ class IncidentSerializer(EagerLoadingMixin, serializers.ModelSerializer):
     route_id = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(source="started_at")
     alerts_count = serializers.SerializerMethodField()
-    title = serializers.SerializerMethodField()
+    title = serializers.CharField(source="web_title_cache")
     state = serializers.SerializerMethodField()
 
-    SELECT_RELATED = ["channel", "channel_filter", "slack_message"]
+    SELECT_RELATED = ["channel", "channel_filter", "slack_message", "channel__organization"]
     PREFETCH_RELATED = [
-        "alerts",
         Prefetch(
             "telegram_messages",
             TelegramMessage.objects.filter(chat_id__startswith="-", message_type=TelegramMessage.ALERT_GROUP_MESSAGE),
@@ -42,10 +41,7 @@ class IncidentSerializer(EagerLoadingMixin, serializers.ModelSerializer):
         ]
 
     def get_alerts_count(self, obj):
-        return len(obj.alerts.all())
-
-    def get_title(self, obj):
-        return obj.alerts.all()[0].title
+        return obj.alerts_count
 
     def get_state(self, obj):
         return obj.state
