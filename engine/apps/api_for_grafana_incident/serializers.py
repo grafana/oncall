8c0595830d976a37728c4b96eb40b8dd180e997a
Ben Sully <ben.sully@grafana.com>
2023-02-03 10:11:16 +0000

    Include alert details in Grafana Incident alert-group endpoint
    This will be used by Grafana Incident to infer details about the
    incident if alert groups are present.

--- engine/apps/api_for_grafana_incident/serializers.py
+++ engine/apps/api_for_grafana_incident/serializers.py
@@ -2,17 +2,31 @@ import logging
 
 from rest_framework import serializers
 
-from apps.alerts.models import AlertGroup
+from apps.alerts.models import Alert, AlertGroup
 from common.api_helpers.mixins import EagerLoadingMixin
 
 logger = logging.getLogger(__name__)
 
 
-class AlertGroupSerializer(EagerLoadingMixin, serializers.ModelSerializer):
+class AlertSerializer(EagerLoadingMixin, serializers.ModelSerializer):
 
     id_oncall = serializers.CharField(read_only=True, source="public_primary_key")
+    payload = serializers.JSONField(read_only=True, source="raw_request_data")
+
+    class Meta:
+        model = Alert
+        fields = [
+            "id_oncall",
+            "payload",
+        ]
+
+
+class AlertGroupSerializer(EagerLoadingMixin, serializers.ModelSerializer):
+
+    id = serializers.CharField(read_only=True, source="public_primary_key")
     status = serializers.SerializerMethodField(source="get_status")
     link = serializers.CharField(read_only=True, source="web_link")
+    alerts = AlertSerializer(many=True, read_only=True)
 
     def get_status(self, obj):
         return next(filter(lambda status: status[0] == obj.status, AlertGroup.STATUS_CHOICES))[1].lower()
@@ -20,7 +34,8 @@ class AlertGroupSerializer(EagerLoadingMixin, serializers.ModelSerializer):
     class Meta:
         model = AlertGroup
         fields = [
-            "id_oncall",
+            "id",
             "link",
             "status",
+            "alerts",
         ]
