52de1c15b8af80f16e8aad370d743322107fa873
Ben Sully <ben.sully@grafana.com>
2023-02-03 10:55:19 +0000

    Remove EagerLoadingMixin from Grafana Incident serializers

--- engine/apps/api_for_grafana_incident/serializers.py
+++ engine/apps/api_for_grafana_incident/serializers.py
@@ -3,12 +3,11 @@ import logging
 from rest_framework import serializers
 
 from apps.alerts.models import Alert, AlertGroup
-from common.api_helpers.mixins import EagerLoadingMixin
 
 logger = logging.getLogger(__name__)
 
 
-class AlertSerializer(EagerLoadingMixin, serializers.ModelSerializer):
+class AlertSerializer(serializers.ModelSerializer):
 
     id_oncall = serializers.CharField(read_only=True, source="public_primary_key")
     payload = serializers.JSONField(read_only=True, source="raw_request_data")
@@ -21,7 +20,7 @@ class AlertSerializer(EagerLoadingMixin, serializers.ModelSerializer):
         ]
 
 
-class AlertGroupSerializer(EagerLoadingMixin, serializers.ModelSerializer):
+class AlertGroupSerializer(serializers.ModelSerializer):
 
     id = serializers.CharField(read_only=True, source="public_primary_key")
     status = serializers.SerializerMethodField(source="get_status")
