0a896fcff0251a30eac9746a43b6e82aee822ef0
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2023-02-22 14:29:27 +0800

    fix excess api call

--- engine/common/api_helpers/utils.py
+++ engine/common/api_helpers/utils.py
@@ -51,9 +51,6 @@ def validate_ical_url(url):
             raise serializers.ValidationError("Potential self-reference")
         try:
             ical_file = fetch_ical_file(url)
-            # without user-agent header google calendar sometimes returns text/html instead of text/icalendar
-            headers = {"User-Agent": "Grafana OnCall"}
-            ical_file = requests.get(url, headers=headers, timeout=10).text
             Calendar.from_ical(ical_file)
         except requests.exceptions.RequestException:
             raise serializers.ValidationError("Ical download failed")
