f23e6ac58c116b3892c345038a13e6c8644a88fa
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2023-03-07 14:50:44 +0800

    Add logging

--- engine/common/insight_log/insight_logs_enabled_check.py
+++ engine/common/insight_log/insight_logs_enabled_check.py
@@ -1,7 +1,11 @@
+import logging
+
 from django.conf import settings
 from django.core.cache import cache
 
 from apps.grafana_plugin.helpers import GcomAPIClient
+
+logger = logging.getLogger(__name__)
 
 
 def is_insight_logs_enabled(organization):
@@ -11,8 +15,16 @@ def is_insight_logs_enabled(organization):
     CACHE_KEY = f"insight_logs_{organization.id}"
     insight_logs_enabled = cache.get(CACHE_KEY, None)
     if insight_logs_enabled is None:
+        logger.info(f"is_insight_logs_enabled: check if logs enabled for org_id={organization.id}")
         client = GcomAPIClient(organization.gcom_token)
         cluster_slug = client.get_cluster_slug(organization.stack_id, timeout=5)
+        logger.info(
+            "is_insight_logs_enabled: "
+            f"IS_OPEN_SOURCE={settings.IS_OPEN_SOURCE} "
+            f"ONCALL_BACKEND_REGION={settings.ONCALL_BACKEND_REGION} "
+            f"cluster_slug={cluster_slug}"
+        )
         insight_logs_enabled = settings.IS_OPEN_SOURCE or settings.ONCALL_BACKEND_REGION == cluster_slug
+        logger.info(f"is_insight_logs_enabled: insight_logs {insight_logs_enabled}")
         cache.set(CACHE_KEY, insight_logs_enabled, 60 * 60 * 24)
     return insight_logs_enabled
