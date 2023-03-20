6bc86e03c162bf07c2cb9162614d4277d323ee39
Matias Bordese <mbordese@gmail.com>
2023-03-20 14:59:35 -0300

    Ignore lint migration when dropping unused table

--- engine/apps/webhooks/migrations/0002_auto_20230320_1604.py
+++ engine/apps/webhooks/migrations/0002_auto_20230320_1604.py
@@ -3,6 +3,8 @@
 from django.db import migrations, models
 import django.db.models.deletion
 
+import django_migration_linter as linter
+
 
 class Migration(migrations.Migration):
 
@@ -12,6 +14,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        linter.IgnoreMigration(),
         migrations.CreateModel(
             name='WebhookResponse',
             fields=[
