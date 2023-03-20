44531a342c43bd501a8814fbac255ac7b98edce8
Hamed Karbasi <30399989+hoptical@users.noreply.github.com>
2023-03-20 17:16:45 +0330

    add test_urlvalidator_without_tld as unit test

---
+++ engine/common/tests/test_urlvalidator_without_tld.py
@@ -0,0 +1,19 @@
+from common.api_helpers.utils import URLValidatorWithoutTLD
+import pytest
+from django.core.validators import ValidationError
+
+valid_urls = ["https://www.google.com", "https://www.google", "http://conatainer1"]
+invalid_urls = ["https:/www.google.com", "htt://www.google.com/"]
+
+
+@pytest.mark.parametrize("url", valid_urls)
+def test_urlvalidator_without_tld_valid_urls(url):
+    # Test valid URLs
+    URLValidatorWithoutTLD()(url)
+
+
+@pytest.mark.parametrize("url", invalid_urls)
+def test_urlvalidator_without_tld_invalid_urls(url):
+    # Test an invalid URL
+    with pytest.raises(ValidationError):
+        URLValidatorWithoutTLD()(url)
