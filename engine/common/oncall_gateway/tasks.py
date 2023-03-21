daacc6a0a3e798ad4664c424e49564699ac747fc
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2023-03-21 12:54:18 +0800

    Add max retries for oncall_gateway tasks

--- engine/common/oncall_gateway/tasks.py
+++ engine/common/oncall_gateway/tasks.py
@@ -12,16 +12,16 @@ task_logger = get_task_logger(__name__)
 @shared_dedicated_queue_retry_task(
     autoretry_for=(Exception,),
     retry_backoff=True,
-    max_retries=None,
+    max_retries=1000,
 )
 def create_oncall_connector_async(oncall_org_id, backend):
     client = OnCallGatewayAPIClient(settings.ONCALL_GATEWAY_URL, settings.ONCALL_GATEWAY_API_TOKEN)
     try:
         client.post_oncall_connector(oncall_org_id, backend)
     except requests.exceptions.HTTPError as http_exc:
-        # TODO: decide which http codes to retry
-        print(http_exc.response)
         if http_exc.response.status_code == 409:
+            # 409 Indicates that it's impossible to create such connector.
+            # More likely because it already exists.
             task_logger.error(
                 f"Failed to create OnCallConnector oncall_org_id={oncall_org_id} backend={backend} exc={http_exc}"
             )
@@ -35,7 +35,7 @@ def create_oncall_connector_async(oncall_org_id, backend):
 @shared_dedicated_queue_retry_task(
     autoretry_for=(Exception,),
     retry_backoff=True,
-    max_retries=None,
+    max_retries=1000,
 )
 def delete_oncall_connector_async(oncall_org_id):
     client = OnCallGatewayAPIClient(settings.ONCALL_GATEWAY_URL, settings.ONCALL_GATEWAY_API_TOKEN)
@@ -43,7 +43,7 @@ def delete_oncall_connector_async(oncall_org_id):
         client.delete_slack_connector(oncall_org_id)
     except requests.exceptions.HTTPError as http_exc:
         if http_exc.response.status_code == 404:
-            # 404 indicates than resourse was deleted already
+            # 404 indicates that connector was deleted already
             return
         else:
             task_logger.error(f"Failed to delete OnCallConnector oncall_org_id={oncall_org_id} exc={http_exc}")
@@ -53,53 +53,30 @@ def delete_oncall_connector_async(oncall_org_id):
         raise e
 
 
+# deprecated
 @shared_dedicated_queue_retry_task(
     autoretry_for=(Exception,),
     retry_backoff=True,
     max_retries=None,
 )
 def create_slack_connector_async(slack_id, backend):
-    client = OnCallGatewayAPIClient(settings.ONCALL_GATEWAY_URL, settings.ONCALL_GATEWAY_API_TOKEN)
-    try:
-        client.post_slack_connector(slack_id, backend)
-    except requests.exceptions.HTTPError as http_exc:
-        # TODO: decide which http codes to retry
-        if http_exc.response.status_code == 409:
-            task_logger.error(
-                f"Failed to create SlackConnector oncall_org_id={slack_id} backend={backend} exc={http_exc}"
-            )
-        else:
-            raise http_exc
-    except Exception as e:
-        task_logger.error(f"Failed to create SlackConnector slack_id={slack_id} backend={backend} exc={e}")
-        raise e
+    pass
 
 
+# deprecated
 @shared_dedicated_queue_retry_task(
     autoretry_for=(Exception,),
     retry_backoff=True,
     max_retries=None,
 )
 def delete_slack_connector_async(slack_id):
-    client = OnCallGatewayAPIClient(settings.ONCALL_GATEWAY_URL, settings.ONCALL_GATEWAY_API_TOKEN)
-    try:
-        client.delete_slack_connector(slack_id)
-    except requests.exceptions.HTTPError as http_exc:
-        if http_exc.response.status_code == 404:
-            # 404 indicates than resourse was deleted already
-            return
-        else:
-            task_logger.error(f"Failed to delete OnCallConnector slack_id={slack_id} exc={http_exc}")
-            raise http_exc
-    except Exception as e:
-        task_logger.error(f"Failed to delete OnCallConnector slack_id={slack_id} exc={e}")
-        raise e
+    pass
 
 
 @shared_dedicated_queue_retry_task(
     autoretry_for=(Exception,),
     retry_backoff=True,
-    max_retries=None,
+    max_retries=1000,
 )
 def create_slack_connector_async_v2(**kwargs):
     oncall_org_id = kwargs.get("oncall_org_id")
@@ -109,8 +86,9 @@ def create_slack_connector_async_v2(**kwargs):
     try:
         client.post_slack_connector(oncall_org_id, slack_team_id, backend)
     except requests.exceptions.HTTPError as http_exc:
-        # TODO: decide which http codes to retry
         if http_exc.response.status_code == 409:
+            # 409 Indicates that it's impossible to create such connector.
+            # More likely because it already exists.
             task_logger.error(
                 f"Failed to create SlackConnector oncall_org_id={oncall_org_id} backend={backend} exc={http_exc}"
             )
@@ -124,7 +102,7 @@ def create_slack_connector_async_v2(**kwargs):
 @shared_dedicated_queue_retry_task(
     autoretry_for=(Exception,),
     retry_backoff=True,
-    max_retries=None,
+    max_retries=1000,
 )
 def delete_slack_connector_async_v2(**kwargs):
     oncall_org_id = kwargs.get("oncall_org_id")
@@ -133,10 +111,9 @@ def delete_slack_connector_async_v2(**kwargs):
         client.delete_slack_connector(oncall_org_id)
     except requests.exceptions.HTTPError as http_exc:
         if http_exc.response.status_code == 404:
-            # 404 indicates than resourse was deleted already
+            # 404 indicates that connector was deleted already
             return
         else:
-            task_logger.error(f"Failed to delete SlackConnectorV2 oncall_org_id={oncall_org_id} exc={http_exc}")
             raise http_exc
     except Exception as e:
         task_logger.error(f"Failed to delete SlackConnectorV2 oncall_org_id={oncall_org_id} exc={e}")
