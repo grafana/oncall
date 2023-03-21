d63a36a473cbb3b88ef4fc4a04c2caf0ddd4fd48
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2023-03-21 12:40:15 +0800

    Slack connectors v2

--- engine/common/oncall_gateway/tasks.py
+++ engine/common/oncall_gateway/tasks.py
@@ -94,3 +94,50 @@ def delete_slack_connector_async(slack_id):
     except Exception as e:
         task_logger.error(f"Failed to delete OnCallConnector slack_id={slack_id} exc={e}")
         raise e
+
+
+@shared_dedicated_queue_retry_task(
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    max_retries=None,
+)
+def create_slack_connector_async_v2(**kwargs):
+    oncall_org_id = kwargs.get("oncall_org_id")
+    slack_team_id = kwargs.get("slack_team_id")
+    backend = kwargs.get("backend")
+    client = OnCallGatewayAPIClient(settings.ONCALL_GATEWAY_URL, settings.ONCALL_GATEWAY_API_TOKEN)
+    try:
+        client.post_slack_connector(oncall_org_id, slack_team_id, backend)
+    except requests.exceptions.HTTPError as http_exc:
+        # TODO: decide which http codes to retry
+        if http_exc.response.status_code == 409:
+            task_logger.error(
+                f"Failed to create SlackConnector oncall_org_id={oncall_org_id} backend={backend} exc={http_exc}"
+            )
+        else:
+            raise http_exc
+    except Exception as e:
+        task_logger.error(f"Failed to create SlackConnector slack_id={oncall_org_id} backend={backend} exc={e}")
+        raise e
+
+
+@shared_dedicated_queue_retry_task(
+    autoretry_for=(Exception,),
+    retry_backoff=True,
+    max_retries=None,
+)
+def delete_slack_connector_async_v2(**kwargs):
+    oncall_org_id = kwargs.get("oncall_org_id")
+    client = OnCallGatewayAPIClient(settings.ONCALL_GATEWAY_URL, settings.ONCALL_GATEWAY_API_TOKEN)
+    try:
+        client.delete_slack_connector(oncall_org_id)
+    except requests.exceptions.HTTPError as http_exc:
+        if http_exc.response.status_code == 404:
+            # 404 indicates than resourse was deleted already
+            return
+        else:
+            task_logger.error(f"Failed to delete SlackConnectorV2 oncall_org_id={oncall_org_id} exc={http_exc}")
+            raise http_exc
+    except Exception as e:
+        task_logger.error(f"Failed to delete SlackConnectorV2 oncall_org_id={oncall_org_id} exc={e}")
+        raise e
