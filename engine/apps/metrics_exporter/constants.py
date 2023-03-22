b9a090960450bd36f6dea5640582716ff805796c
Julia <ferril.darkdiver@gmail.com>
2023-03-22 15:32:36 +0100

    Add metrics exporter app
    - add endpoint for prometheus
    - add caching system for metrics
    - add metrics calculation

---
+++ engine/apps/metrics_exporter/constants.py
@@ -0,0 +1,8 @@
+ALERT_GROUPS_TOTAL = "oncall_alert_groups_total"
+ALERT_GROUPS_RESPONSE_TIME = "oncall_alert_groups_response_time_7d_seconds"
+
+METRICS_CACHE_TIMER = "metrics_cache_timer"
+METRICS_RECALCULATE_CACHE_TIMEOUT = 86400  # 24 hours
+METRICS_RECALCULATE_CACHE_TIMEOUT_DISPERSE = (0, 3600)  # 1 hour
+
+METRICS_CACHE_LIFETIME = 86400  # todo:metrics
