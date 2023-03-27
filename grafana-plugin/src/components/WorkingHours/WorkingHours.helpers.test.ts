9113c53e5ef48223226249fd6683531cd98e3976
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2023-03-27 21:20:11 +0200

    fixing the tests

--- grafana-plugin/src/components/WorkingHours/WorkingHours.helpers.test.ts
+++ grafana-plugin/src/components/WorkingHours/WorkingHours.helpers.test.ts
@@ -33,10 +33,10 @@ describe('Is in working hours', () => {
   
     test('Returns false when it is complex Working hours schedule', () => {
         const workingHours =  
-        {monday: [{start: "09:00:00", end: "14:00:00"}, {start: "15:00:00", end: "18:00:00"}], tuesday: [{start: "09:00:00", end: "14:00:00"}, {start: "15:00:00", end: "18:00:00"}],
-        wednesday: [{start: "09:00:00", end: "14:00:00"}, {start: "15:00:00", end: "18:00:00"}], thursday: [{start: "09:00:00", end: "14:00:00"}, {start: "15:00:00", end: "18:00:00"}],
-        friday: [{start: "09:00:00", end: "14:00:00"}, {start: "15:00:00", end: "18:00:00"}], saturday:[{start: "09:00:00", end: "14:00:00"}, {start: "15:00:00", end: "18:00:00"}],
-        sunday:[{start: "09:00:00", end: "14:00:00"}, {start: "15:00:00", end: "18:00:00"}]};
+        {monday: [{start: "09:00:00", end: "11:00:00"}, {start: "15:00:00", end: "18:00:00"}], tuesday: [{start: "09:00:00", end: "11:00:00"}, {start: "15:00:00", end: "18:00:00"}],
+        wednesday: [{start: "09:00:00", end: "11:00:00"}, {start: "15:00:00", end: "18:00:00"}], thursday: [{start: "09:00:00", end: "11:00:00"}, {start: "15:00:00", end: "18:00:00"}],
+        friday: [{start: "09:00:00", end: "11:00:00"}, {start: "15:00:00", end: "18:00:00"}], saturday:[{start: "09:00:00", end: "11:00:00"}, {start: "15:00:00", end: "18:00:00"}],
+        sunday:[{start: "09:00:00", end: "11:00:00"}, {start: "15:00:00", end: "18:00:00"}]};
 
         expect(isInWorkingHours(currentMoment, workingHours, 'Europe/Amsterdam')).toBeFalsy();
     });
