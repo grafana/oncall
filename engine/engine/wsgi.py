e0dd4bd1287bd6d88314dfb1dd644009935ac15a
Ildar Iskhakov <Ildar.iskhakov@grafana.com>
2023-03-28 13:11:22 +0800

    Update wsgi.py

--- engine/engine/wsgi.py
+++ engine/engine/wsgi.py
@@ -52,6 +52,7 @@ if settings.PYROSCOPE_PROFILER_ENABLED:
                 application_name=settings.PYROSCOPE_APPLICATION_NAME,
                 server_address=settings.PYROSCOPE_SERVER_ADDRESS,
                 auth_token=settings.PYROSCOPE_AUTH_TOKEN,
+                detect_subprocesses=True,
                 tags={"type": "uwsgi"},
             )
 
