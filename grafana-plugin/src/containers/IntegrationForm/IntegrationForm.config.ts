8555143149e14406ea6a0ec1a9e939c82724361b
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2023-03-30 21:31:42 +0200

    WIP: first version of Integrations table and Integration Form Drawer

---
+++ grafana-plugin/src/containers/IntegrationForm/IntegrationForm.config.ts
@@ -0,0 +1,26 @@
+import { FormItem, FormItemType } from 'components/GForm/GForm.types';
+
+export const form: { name: string; fields: FormItem[] } = {
+  name: 'Integration',
+  fields: [
+    {
+      name: 'verbal_name',
+      type: FormItemType.Input,
+      validation: { required: true },
+    },
+    {
+      name: 'team',
+      label: 'Assign to team',
+      description:
+        'Assigning to the teams allows you to filter Integrations and configure their visibility. Go to OnCall -> Settings -> Team and Access Settings for more details',
+      type: FormItemType.GSelect,
+      extra: {
+        modelName: 'grafanaTeamStore',
+        displayField: 'name',
+        valueField: 'id',
+        showSearch: true,
+        allowClear: true,
+      },
+    },
+  ],
+};
