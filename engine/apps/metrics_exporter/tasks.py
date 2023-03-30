faa2f06e9fb17d6158c25814e93765ee074bf9b1
Julia <ferril.darkdiver@gmail.com>
2023-03-30 10:02:22 +0200

    Add default structure for metrics

--- engine/apps/metrics_exporter/tasks.py
+++ engine/apps/metrics_exporter/tasks.py
@@ -1,3 +1,5 @@
+import typing
+
 from django.apps import apps
 from django.conf import settings
 from django.core.cache import cache
@@ -5,7 +7,13 @@ from django.db.models import Q
 from django.utils import timezone
 
 from apps.alerts.constants import STATE_ACKNOWLEDGED, STATE_NEW, STATE_RESOLVED, STATE_SILENCED
-from apps.metrics_exporter.constants import ALERT_GROUPS_RESPONSE_TIME, ALERT_GROUPS_TOTAL, METRICS_CACHE_TIMER
+from apps.metrics_exporter.constants import (
+    ALERT_GROUPS_RESPONSE_TIME,
+    ALERT_GROUPS_TOTAL,
+    METRICS_CACHE_TIMER,
+    AlertGroupsResponseTimeMetricsDict,
+    AlertGroupsTotalMetricsDict,
+)
 from apps.metrics_exporter.helpers import get_metrics_recalculate_timeout, get_response_time_period
 from common.custom_celery_tasks import shared_dedicated_queue_retry_task
 
@@ -28,8 +36,8 @@ def calculate_and_cache_metrics():  # todo:metrics org
     integrations = AlertReceiveChannel.objects.filter(~Q(integration=AlertReceiveChannel.INTEGRATION_MAINTENANCE))
     response_time_period = get_response_time_period(timezone.now())
 
-    metric_alert_group_total = {}
-    metric_alert_group_response_time = {}
+    metric_alert_group_total: typing.Dict[int, AlertGroupsTotalMetricsDict] = {}
+    metric_alert_group_response_time: typing.Dict[int, AlertGroupsResponseTimeMetricsDict] = {}
 
     states = {
         STATE_NEW: AlertGroup.get_new_state_filter(),
@@ -41,18 +49,6 @@ def calculate_and_cache_metrics():  # todo:metrics org
     alert_groups_to_update = []
     for integration in integrations:
         # calculate states
-        # Result example:
-        # {
-        #     123 <- integration_id: {
-        #         integration_name: test,
-        #         team_name: oncall,
-        #         team_id: 456,
-        #         new: 3,
-        #         acknowledged: 4,
-        #         silenced: 0,
-        #         resolved: 0,
-        #     }, ...
-        # }
         for state, alert_group_filter in states.items():
             metric_alert_group_total.setdefault(
                 integration.id,
@@ -64,16 +60,6 @@ def calculate_and_cache_metrics():  # todo:metrics org
             )[state] = integration.alert_groups.filter(alert_group_filter).count()
 
         # calculate response time
-        # Result example:
-        # {
-        #     123 <- integration_id: {
-        #         integration_name: test,
-        #         team_name: oncall,
-        #         team_id: 456,
-        #         response_time: [35, 569, 82, 1065], <- list of response time seconds
-        #     }, ...
-        # }
-
         all_response_time = []
         alert_groups = integration.alert_groups.filter(started_at__gte=response_time_period)
         for alert_group in alert_groups:
