d8da3238b931b3c45e18362bbe0e119d1e801f38
Rares Mardare <rares.mardare@grafana.com>
2023-04-05 17:08:15 +0300

    added tree view

--- grafana-plugin/src/components/IntegrationCollapsibleTreeView/IntegrationCollapsibleTreeView.tsx
+++ grafana-plugin/src/components/IntegrationCollapsibleTreeView/IntegrationCollapsibleTreeView.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import cn from 'classnames/bind';
 
 import styles from './IntegrationCollapsibleTreeView.module.scss';
+import { Icon } from '@grafana/ui';
 
 const cx = cn.bind(styles);
 
 interface IntegrationCollapsibleTreeViewProps {
-  children: React.ReactNode[]
+  children: React.ReactNode[];
 }
 
-const IntegrationCollapsibleTreeView: React.FC<IntegrationCollapsibleTreeViewProps> = () => {
-  return <div className={cx('integrationTree__container')}></div>;
+const IntegrationCollapsibleTreeView: React.FC<IntegrationCollapsibleTreeViewProps> = (props) => {
+  const { children } = props;
+  return (
+    <div className={cx('integrationTree__container')}>
+      {children.map((itemNode: React.ReactNode) => {
+        return (
+          <div className={cx('integrationTree__group')}>
+            <div className={cx('integrationTree__icon')}>
+              <Icon name="arrow-down" />
+            </div>
+            {itemNode}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default IntegrationCollapsibleTreeView;
