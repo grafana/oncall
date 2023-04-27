9f11b287addeb528de07455977942faee3f8e213
Ildar Iskhakov <ildar.iskhakov@grafana.com>
2023-04-27 10:49:41 +0800

    Filter by last 1000 alert groups

--- engine/apps/api/views/alert_group.py
+++ engine/apps/api/views/alert_group.py
@@ -186,18 +186,24 @@ class AlertGroupFilter(DateRangeFilterMixin, ByTeamModelFieldFilterMixin, ModelF
         return queryset
 
     def filter_by_involved_users(self, queryset, name, value):
+        NOTIFICATION_HISTORY_CUTOFF = 1000
         users = value
 
         if not users:
             return queryset
 
-        alert_groups_by_users_qs = (
-            UserNotificationPolicyLogRecord.objects.filter(author__in=users).distinct().only("id").all()
+        # This is expensive to filter all alert groups with involved users,
+        # so we limit the number of alert groups to filter by the last 1000 for the given user(s)
+        alert_group_notified_users_ids = list(
+            UserNotificationPolicyLogRecord.objects.filter(author__in=users)
+            .order_by("-alert_group_id")
+            .values_list("alert_group_id", flat=True)
+            .distinct()[:NOTIFICATION_HISTORY_CUTOFF]
         )
 
         queryset = queryset.filter(
             # user was notified
-            Q(id__in=alert_groups_by_users_qs)
+            Q(id__in=alert_group_notified_users_ids)
             |
             # or interacted with the alert group
             Q(acknowledged_by_user__in=users)
