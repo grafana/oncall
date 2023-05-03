0d6a55eed70ecd3fc9d1cecaf798d368fc8c3140
Rares Mardare <rares.mardare@grafana.com>
2023-05-03 17:39:16 +0300

    disabled a few tests

--- grafana-plugin/jest.config.js
+++ grafana-plugin/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
 
-  transformIgnorePatterns: [`/node_modules/(?!${esModules})`, `/node_modules/`],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
 
   moduleNameMapper: {
     'grafana/app/(.*)': '<rootDir>/src/jest/grafanaMock.ts',
