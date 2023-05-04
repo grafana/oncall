b314164ca9eb7f8a8ade887026947ee020261387
Rares Mardare <rares.mardare@grafana.com>
2023-05-04 09:14:44 +0300

    linter

--- grafana-plugin/src/components/GBlock/Block.tsx
+++ grafana-plugin/src/components/GBlock/Block.tsx
@@ -29,17 +29,13 @@ const Block: FC<BlockProps> = (props) => {
 
   return (
     <div
-      className={cx(
-        'root',
-        className,
-        {
-          root_bordered: bordered,
-          root_shadowed: shadowed,
-          'root--fullWidth': fullWidth,
-          'root--withBackGround': withBackground,
-          'root--hover': hover,
-        },
-      )}
+      className={cx('root', className, {
+        root_bordered: bordered,
+        root_shadowed: shadowed,
+        'root--fullWidth': fullWidth,
+        'root--withBackGround': withBackground,
+        'root--hover': hover,
+      })}
       style={style}
       {...rest}
     >
