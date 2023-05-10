4ddc12848553490b9b6e7a21416e572314655901
Vadim Stepanov <vadimkerr@gmail.com>
2023-05-10 15:51:36 +0100

    Add docs & logo for AppDynamics integration

--- grafana-plugin/src/components/IntegrationLogo/IntegrationLogo.config.ts
+++ grafana-plugin/src/components/IntegrationLogo/IntegrationLogo.config.ts
@@ -21,4 +21,5 @@ export const logoCoors: { [key: string]: { x: number; y: number } } = {
   prtg: { x: 12, y: 5 },
   jira: { x: 8, y: 9 },
   zendesk: { x: 9, y: 14 },
+  appdynamics: { x: 3, y: 3 },
 };
