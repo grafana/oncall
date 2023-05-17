d86f02f2ba188770ad981a5da0b2b7896fdccedd
Julia <ferril.darkdiver@gmail.com>
2023-05-17 07:08:48 +0200

    Fix migration

--- engine/apps/alerts/migrations/0016_alertgroup_response_time.py
+++ engine/apps/alerts/migrations/0016_alertgroup_response_time.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('alerts', '0014_alertreceivechannel_restricted_at'),
+        ('alerts', '0015_auto_20230508_1641'),
     ]
 
     operations = [
