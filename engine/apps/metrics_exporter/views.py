977ad6a8d0160343e6332a3f712e11812d74cb18
Matias Bordese <mbordese@gmail.com>
2023-05-18 10:31:04 -0300

    Remove internal ip check

--- engine/apps/metrics_exporter/views.py
+++ engine/apps/metrics_exporter/views.py
@@ -1,20 +1,13 @@
 from django.conf import settings
-from django.http import Http404, HttpResponse
+from django.http import HttpResponse
 from prometheus_client import generate_latest
 from rest_framework.views import APIView
 
 from .metrics_collectors import application_metrics_registry
 
 
-def is_internal(request):
-    return request.META.get("REMOTE_ADDR") in settings.INTERNAL_IPS
-
-
 class MetricsExporterView(APIView):
-    def get(self, request):
-        # todo:metrics
-        if not is_internal(request):
-            raise Http404
 
+    def get(self, request):
         result = generate_latest(application_metrics_registry).decode("utf-8")
         return HttpResponse(result, content_type="text/plain; version=0.0.4; charset=utf-8")
