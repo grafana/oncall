6c633c7e4f3a0397a8d4e2abb3f36cac32dfd5ef
Rares Mardare <rares.mardare@grafana.com>
2023-05-23 10:50:35 +0300

    linter

--- grafana-plugin/src/containers/IntegrationContainers/IntegrationTemplatesList.tsx
+++ grafana-plugin/src/containers/IntegrationContainers/IntegrationTemplatesList.tsx
@@ -7,6 +7,7 @@ import IntegrationBlockItem from 'components/Integrations/IntegrationBlockItem';
 import IntegrationTemplateBlock from 'components/Integrations/IntegrationTemplateBlock';
 import MonacoEditor from 'components/MonacoEditor/MonacoEditor';
 import Text from 'components/Text/Text';
+import { templatesToRender } from 'containers/IntegrationContainers/IntegrationTemplatesList.config';
 import { AlertReceiveChannel } from 'models/alert_receive_channel/alert_receive_channel.types';
 import { AlertTemplatesDTO } from 'models/alert_templates';
 import { MONACO_INPUT_HEIGHT_TALL, MONACO_OPTIONS } from 'pages/integration_2/Integration2.config';
@@ -14,8 +15,6 @@ import IntegrationHelper from 'pages/integration_2/Integration2.helper';
 import styles from 'pages/integration_2/Integration2.module.scss';
 import { useStore } from 'state/useStore';
 import { openErrorNotification, openNotification } from 'utils';
-
-import { templatesToRender } from 'containers/IntegrationContainers/IntegrationTemplatesList.config';
 
 const cx = cn.bind(styles);
 
