d588f50fea6e3ec34d29ce44611e321e824dbbf9
Yulia Shanyrova <yulia.shanyrova@grafana.com>
2023-06-06 13:35:32 +0200

    UID has been added to Integrations table, Integration page and routes

---
+++ grafana-plugin/src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -0,0 +1,39 @@
+import React, { useRef } from 'react';
+
+import { Icon } from '@grafana/ui';
+import cn from 'classnames/bind';
+
+import styles from './HamburgerMenu.module.css';
+
+interface HamburgerMenuProps {
+  openMenu: React.MouseEventHandler<HTMLElement>;
+  listWidth: number;
+  listBorder: number;
+  withBackground?: boolean;
+  className?: string;
+}
+
+const cx = cn.bind(styles);
+
+const HamburgerMenu: React.FC<HamburgerMenuProps> = (props) => {
+  const ref = useRef<HTMLDivElement>();
+  const { openMenu, listBorder, listWidth, withBackground, className } = props;
+  return (
+    <div
+      ref={ref}
+      className={withBackground ? cx('hamburger-menu-withBackground') : cx('hamburger-menu', className)}
+      onClick={() => {
+        const boundingRect = ref.current.getBoundingClientRect();
+
+        openMenu({
+          pageX: boundingRect.right - listWidth + listBorder * 2,
+          pageY: boundingRect.top + boundingRect.height,
+        } as any);
+      }}
+    >
+      <Icon size="sm" name="ellipsis-v" />
+    </div>
+  );
+};
+
+export default HamburgerMenu;
