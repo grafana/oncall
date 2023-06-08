c7f8ac355951de8ff881f8427f2443d51add6bb2
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2023-06-08 13:55:41 +0800

    Add BaseFailed exceptions for phone_notificator (#2074)
    # What this PR does
    Introduces BaseFailed exception for phone_notificator.
    
    # Why
    We need to somehow distinguish errors we want to be notified - like
    network errors or invalid twilio credentials (I will call them "real"
    errors) and errors we want to share with user, but don't want to be
    paged ( I will call them "fake" errors).
    To do that I added "graceful_msg" to all Failed... exceptions. If
    details field is present - it mean we can return 400 code with the
    message, if not - 500 code. So, "real" errors will raise Failed...
    exception, while "fake" will add "graceful_msg".
    
    # TODO
    handle exceptions handled here
    https://github.com/grafana/oncall/pull/2065
    ## Checklist
    
    - [ ] Unit, integration, and e2e (if applicable) tests updated
    - [x] Documentation added (or `pr:no public docs` PR label added if not
    required)
    - [x] `CHANGELOG.md` updated (or `pr:no changelog` PR label added if not
    required)
    
    ---------
    
    Co-authored-by: Michael Derynck <michael.derynck@grafana.com>

--- engine/apps/phone_notifications/exceptions.py
+++ engine/apps/phone_notifications/exceptions.py
@@ -1,8 +1,32 @@
-class FailedToMakeCall(Exception):
+class BaseFailed(Exception):
+    """
+    Failed is base exception for all Failed... exceptions.
+    This exception is indicates error while performing some phone notification operation.
+    Optionally can contain graceful_msg attribute. When graceful_msg is provided it mean that error on provider side is
+    not our fault, but some provider error (number is blocked, fraud guard, ...).
+    By default, graceful_msg is None - it means that error is our fault (network problems, invalid configuration,...).
+
+    Attributes:
+       graceful_msg: string with some details about exception which can be exposed to caller.
+    """
+
+    def __init__(self, graceful_msg=None):
+        self.graceful_msg = graceful_msg
+
+
+class FailedToMakeCall(BaseFailed):
     pass
 
 
-class FailedToSendSMS(Exception):
+class FailedToSendSMS(BaseFailed):
+    pass
+
+
+class FailedToStartVerification(BaseFailed):
+    pass
+
+
+class FailedToFinishVerification(BaseFailed):
     pass
 
 
@@ -11,14 +35,6 @@ class NumberNotVerified(Exception):
 
 
 class NumberAlreadyVerified(Exception):
-    pass
-
-
-class FailedToStartVerification(Exception):
-    pass
-
-
-class FailedToFinishVerification(Exception):
     pass
 
 
--- engine/apps/twilioapp/tests/test_twilio_provider.py
+++ engine/apps/twilioapp/tests/test_twilio_provider.py
@@ -1,11 +1,18 @@
 from unittest import mock
 
 import pytest
+from twilio.base.exceptions import TwilioRestException
 
+from apps.phone_notifications.exceptions import FailedToFinishVerification, FailedToMakeCall, FailedToSendSMS
 from apps.twilioapp.phone_provider import TwilioPhoneProvider
 
 
 class MockTwilioCallInstance:
+    status = "mock_status"
+    sid = "mock_sid"
+
+
+class MockTwilioMessageInstance:
     status = "mock_status"
     sid = "mock_sid"
 
@@ -63,3 +70,87 @@ def test_send_sms(mock_messages_create):
     provider_sms = provider.send_sms(number, message)
     assert provider_sms is None  # test that provider_sms is not returned from send_sms
     mock_messages_create.assert_called_once_with(number, message, with_callback=False)
+
+
+TEST_NUMBER = "+1234567890"
+TEST_MESSAGE = "Hello"
+TEST_CODE = "12345"
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "twilio_code,expected_exception,graceful_msg,provider_method,mock_method",
+    [
+        (60200, FailedToMakeCall, True, lambda p: p.make_call(TEST_NUMBER, TEST_MESSAGE), "_call_create"),
+        (30808, FailedToMakeCall, False, lambda p: p.make_call(TEST_NUMBER, TEST_MESSAGE), "_call_create"),
+        (None, FailedToMakeCall, False, lambda p: p.make_call(TEST_NUMBER, TEST_MESSAGE), "_call_create"),
+        (30410, FailedToMakeCall, True, lambda p: p.make_notification_call(TEST_NUMBER, TEST_MESSAGE), "_call_create"),
+        (30808, FailedToMakeCall, False, lambda p: p.make_notification_call(TEST_NUMBER, TEST_MESSAGE), "_call_create"),
+        (None, FailedToMakeCall, False, lambda p: p.make_notification_call(TEST_NUMBER, TEST_MESSAGE), "_call_create"),
+        (30004, FailedToSendSMS, True, lambda p: p.send_sms(TEST_NUMBER, TEST_MESSAGE), "_messages_create"),
+        (30808, FailedToSendSMS, False, lambda p: p.send_sms(TEST_NUMBER, TEST_MESSAGE), "_messages_create"),
+        (None, FailedToSendSMS, False, lambda p: p.send_sms(TEST_NUMBER, TEST_MESSAGE), "_messages_create"),
+        (
+            30006,
+            FailedToSendSMS,
+            True,
+            lambda p: p.send_notification_sms(TEST_NUMBER, TEST_MESSAGE),
+            "_messages_create",
+        ),
+        (
+            30808,
+            FailedToSendSMS,
+            False,
+            lambda p: p.send_notification_sms(TEST_NUMBER, TEST_MESSAGE),
+            "_messages_create",
+        ),
+        (
+            None,
+            FailedToSendSMS,
+            False,
+            lambda p: p.send_notification_sms(TEST_NUMBER, TEST_MESSAGE),
+            "_messages_create",
+        ),
+        (
+            60203,
+            FailedToFinishVerification,
+            True,
+            lambda p: p.finish_verification(TEST_NUMBER, TEST_CODE),
+            "_verify_sender",
+        ),
+        (
+            30808,
+            FailedToFinishVerification,
+            False,
+            lambda p: p.finish_verification(TEST_NUMBER, TEST_CODE),
+            "_verify_sender",
+        ),
+        (
+            None,
+            FailedToFinishVerification,
+            False,
+            lambda p: p.finish_verification(TEST_NUMBER, TEST_CODE),
+            "_verify_sender",
+        ),
+    ],
+)
+@mock.patch("apps.twilioapp.phone_provider.TwilioPhoneProvider._normalize_phone_number", return_value=(TEST_NUMBER, 1))
+def test_twilio_provider_exceptions(
+    mocked_normalize, twilio_code, expected_exception, graceful_msg, provider_method, mock_method
+):
+    provider = TwilioPhoneProvider()
+
+    with mock.patch(f"apps.twilioapp.phone_provider.TwilioPhoneProvider.{mock_method}") as twilio_mock:
+        twilio_mock.side_effect = TwilioRestException(500, "", code=twilio_code)
+        with pytest.raises(expected_exception) as exc:
+            provider_method(provider)
+            if graceful_msg:
+                assert len(exc.value.graceful_msg) > 0
+            else:
+                assert exc.value.graceful_msg is None
+        twilio_mock.assert_called_once()
+
+
+class MockTwilioSMSInstance:
+    status = "mock_status"
+    sid = "mock_sid"
