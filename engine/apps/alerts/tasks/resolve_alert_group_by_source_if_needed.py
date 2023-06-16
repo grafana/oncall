6bc1509ede74b1c7a2adea68e3ce4c6b88343271
Ildar Iskhakov <ildar.iskhakov@grafana.com>
2023-06-16 16:56:51 +0800

    Polish

--- engine/apps/alerts/tasks/resolve_alert_group_by_source_if_needed.py
+++ engine/apps/alerts/tasks/resolve_alert_group_by_source_if_needed.py
@@ -22,11 +22,9 @@ def resolve_alert_group_by_source_if_needed(alert_group_pk):
             alert_group.active_resolve_calculation_id
         )
     else:
-        # is_more_than_max_alerts_in_group = alert_group.alerts.count() > AlertGroupForAlertManager.MAX_ALERTS_IN_GROUP_FOR_AUTO_RESOLVE
-        is_more_than_max_alerts_in_group = (
-            len(alert_group.alerts[: AlertGroupForAlertManager.MAX_ALERTS_IN_GROUP_FOR_AUTO_RESOLVE])
-            >= AlertGroupForAlertManager.MAX_ALERTS_IN_GROUP_FOR_AUTO_RESOLVE
-        )
+        # alert_group.alerts.count() is too expensive, we only need to know if there are more than N alerts in group
+        max_alert_number = AlertGroupForAlertManager.MAX_ALERTS_IN_GROUP_FOR_AUTO_RESOLVE + 1
+        is_more_than_max_alerts_in_group = len(alert_group.alerts.values("pk")[:max_alert_number]) >= max_alert_number
         if alert_group.resolved_by == alert_group.NOT_YET_STOP_AUTORESOLVE:
             return "alert_group is too big to auto-resolve"
         if is_more_than_max_alerts_in_group:
