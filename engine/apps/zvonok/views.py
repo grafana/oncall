d3b8ceda9fcd381bd5937e2d933e021662e2cf14
Andrey Oleynik <a.y.oleynik@gmail.com>
2023-07-01 16:22:31 +0300

    add _handle_call_status method for post/get requests

--- engine/apps/zvonok/views.py
+++ engine/apps/zvonok/views.py
@@ -36,8 +36,15 @@ class CallStatusCallback(APIView):
     permission_classes = [AllowOnlyZvonok]
 
     def get(self, request):
+        self._handle_call_status(request)
+        return Response(data="", status=status.HTTP_204_NO_CONTENT)
+
+    def post(self, request):
+        self._handle_call_status(request)
+        return Response(data="", status=status.HTTP_204_NO_CONTENT)
+
+    def _handle_call_status(self, request):
         call_id = request.GET.get(live_settings.ZVONOK_POSTBACK_CALL_ID)
         call_status = request.GET.get(live_settings.ZVONOK_POSTBACK_STATUS)
         user_choice = request.GET.get(live_settings.ZVONOK_POSTBACK_USER_CHOICE)
         update_zvonok_call_status(call_id=call_id, call_status=call_status, user_choice=user_choice)
-        return Response(data="", status=status.HTTP_204_NO_CONTENT)
