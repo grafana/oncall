d6f3cd30a332ed772355b73fe21b76eb97e0d7d2
Andrey Oleynik <a.y.oleynik@gmail.com>
2023-07-01 14:30:05 +0300

    remove unused url namespace

--- engine/apps/zvonok/urls.py
+++ engine/apps/zvonok/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import CallStatusCallback
 
-app_name = "zvonok"
 
 urlpatterns = [
     path("call_status_events", CallStatusCallback.as_view(), name="call_status_events"),
