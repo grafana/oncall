f6e0ec83d866f245a92a76aa4ef0f7ae29d7214a
Joey Orlando <joey.orlando@grafana.com>
2023-07-05 11:15:21 +0200

    remove networkIdle wait state when switching pages (#2428)
    # Which issue(s) this PR fixes
    
    speed up e2e tests + improve stability
    
    ## Checklist
    
    - [ ] Unit, integration, and e2e (if applicable) tests updated
    - [ ] Documentation added (or `pr:no public docs` PR label added if not
    required)
    - [ ] `CHANGELOG.md` updated (or `pr:no changelog` PR label added if not
    required)

--- grafana-plugin/integration-tests/utils/navigation.ts
+++ grafana-plugin/integration-tests/utils/navigation.ts
@@ -4,8 +4,7 @@ import { BASE_URL } from './constants';
 type GrafanaPage = '/plugins/grafana-oncall-app';
 type OnCallPage = 'incidents' | 'integrations' | 'escalations' | 'schedules' | 'users';
 
-const _goToPage = (page: Page, url = ''): Promise<Response> =>
-  page.goto(`${BASE_URL}${url}`, { waitUntil: 'networkidle' });
+const _goToPage = (page: Page, url = ''): Promise<Response> => page.goto(`${BASE_URL}${url}`);
 
 export const goToGrafanaPage = (page: Page, url: GrafanaPage): Promise<Response> => _goToPage(page, url);
 
