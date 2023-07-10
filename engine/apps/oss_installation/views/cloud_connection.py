711c8dd975574df752efd0f585c9f8097e33ef30
Joey Orlando <joseph.t.orlando@gmail.com>
2023-07-10 09:02:27 +0200

    fix typo

--- engine/apps/oss_installation/views/cloud_connection.py
+++ engine/apps/oss_installation/views/cloud_connection.py
@@ -22,14 +22,14 @@ class CloudConnectionView(APIView):
     def get(self, request):
         connector = CloudConnector.objects.first()
         heartbeat = CloudHeartbeat.objects.first()
-        return Response(
-            {
-                "cloud_notifications_enabled": live_settings.GRAFANA_CLOUD_NOTIFICATIONS_ENABLED,
-                "cloud_heartbeat_enabled": live_settings.GRAFANA_CLOUD_ONCALL_HEARTBEAT_ENABLED,
-                "cloud_heartbeat_link": get_heartbeat_link(connector, heartbeat),
-                "cloud_heartbeat_status": heartbeat is not None and heartbeat.success,
-            }
-        )
+        response = {
+            "cloud_connection_status": connector is not None,
+            "cloud_notifications_enabled": live_settings.GRAFANA_CLOUD_NOTIFICATIONS_ENABLED,
+            "cloud_heartbeat_enabled": live_settings.GRAFANA_CLOUD_ONCALL_HEARTBEAT_ENABLED,
+            "cloud_heartbeat_link": get_heartbeat_link(connector, heartbeat),
+            "cloud_heartbeat_status": heartbeat is not None and heartbeat.success,
+        }
+        return Response(response)
 
     def delete(self, request):
         s = LiveSetting.objects.filter(name="GRAFANA_CLOUD_ONCALL_TOKEN").first()
