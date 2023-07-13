39638aa3da51682cca3f477477554e50aabdcf50
Joey Orlando <joseph.t.orlando@gmail.com>
2023-07-13 23:23:40 +0200

    update migration

--- engine/apps/alerts/models/alert_group.py
+++ engine/apps/alerts/models/alert_group.py
@@ -408,7 +408,10 @@ class AlertGroup(AlertGroupSlackRenderingMixin, EscalationSnapshotMixin, models.
             "is_open_for_grouping",
         ]
         indexes = [
-            models.Index(fields=["channel_id", "resolved", "acknowledged", "silenced", "root_alert_group_id"]),
+            # TODO: remove is_archived from this index when we drop the is_archived column
+            models.Index(
+                fields=["channel_id", "resolved", "acknowledged", "silenced", "root_alert_group_id", "is_archived"]
+            ),
         ]
 
     def __str__(self):
