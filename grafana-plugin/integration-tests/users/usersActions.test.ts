0b6412adf8ce31d6b672ecd5ee63a0a18681b768
Maxim <maxim.mordasov@grafana.com>
2023-07-24 11:12:06 +0300

    review fixes

--- grafana-plugin/integration-tests/users/usersActions.test.ts
+++ grafana-plugin/integration-tests/users/usersActions.test.ts
@@ -1,36 +1,25 @@
 import { test, expect, Page } from '../fixtures';
 import { goToOnCallPage } from '../utils/navigation';
 
-test.describe('view list of users', () => {
-  test.slow(); // this test is doing a good amount of work, give it time
+test.describe("edit other user's profiles", () => {
+  test.slow();
 
-  test('admin is allowed to edit other profiles', async ({ adminRolePage }) => {
-    const { page } = adminRolePage;
-
+  const _testButtons = async (page: Page, selector: string) => {
     await goToOnCallPage(page, 'users');
 
     const usersTableElement = page.getByTestId('users-table');
     await usersTableElement.waitFor({ state: 'visible' });
 
-    const editOtherUserProfileDisabledButtonList = await page.locator('button.edit-other-profile-button[disabled]');
+    const buttonsList = await page.locator(selector);
 
-    expect(editOtherUserProfileDisabledButtonList).toHaveCount(0);
+    expect(buttonsList).toHaveCount(0);
+  };
+
+  test('admin is allowed', async ({ adminRolePage }) => {
+    await _testButtons(adminRolePage.page, 'button.edit-other-profile-button[disabled]');
   });
 
-  test('editor is not allowed to edit other profiles', async ({ editorRolePage }) => {
-    const { page } = editorRolePage;
-
-    await goToOnCallPage(page, 'users');
-
-    const usersTableElement = page.getByTestId('users-table');
-    await usersTableElement.waitFor({ state: 'visible' });
-
-    const editOtherUserProfileEnabledButtonList = await page.locator(
-      'button.edit-other-profile-button:not([disabled])'
-    );
-
-    expect(editOtherUserProfileEnabledButtonList).toHaveCount(0);
+  test('editor is not allowed', async ({ editorRolePage }) => {
+    await _testButtons(editorRolePage.page, 'button.edit-other-profile-button:not([disabled])');
   });
-
-  test.skip('viewer is not allowed to', async ({ viewerRolePage }) => {});
 });
