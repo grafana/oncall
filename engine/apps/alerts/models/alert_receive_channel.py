ad0ed6714ac9993582e78b50d855aaf69df17619
Joey Orlando <joseph.t.orlando@gmail.com>
2023-07-25 14:35:47 +0200

    tweak restricted_at database migration

--- engine/apps/alerts/models/alert_receive_channel.py
+++ engine/apps/alerts/models/alert_receive_channel.py
@@ -198,6 +198,9 @@ class AlertReceiveChannel(IntegrationOptionsMixin, MaintainableObject):
     rate_limited_in_slack_at = models.DateTimeField(null=True, default=None)
     rate_limit_message_task_id = models.CharField(max_length=100, null=True, default=None)
 
+    # TODO: drop this field in a subsequent release
+    restricted_at = models.DateTimeField(null=True, default=None)
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
