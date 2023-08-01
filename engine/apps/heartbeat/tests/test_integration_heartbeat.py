8db1ea52350ea827cdc8224561567414f58a8158
Joey Orlando <joey.orlando@grafana.com>
2023-08-01 20:22:42 +0200

    remove some references to amixr (#2698)
    # What this PR does
    
    Update references to amixr in various spots in the docs/code + some
    `.md` IDE autoformatter changes
    
    ## Checklist
    
    - [ ] Unit, integration, and e2e (if applicable) tests updated (N/A)
    - [x] Documentation added (or `pr:no public docs` PR label added if not
    required)
    - [x] `CHANGELOG.md` updated (or `pr:no changelog` PR label added if not
    required)

--- engine/apps/heartbeat/tests/test_integration_heartbeat.py
+++ engine/apps/heartbeat/tests/test_integration_heartbeat.py
@@ -12,11 +12,11 @@ from apps.alerts.models import AlertReceiveChannel
 def test_integration_heartbeat_expired(
     mocked_handler, make_organization_and_user, make_alert_receive_channel, make_integration_heartbeat, integration
 ):
-    amixr_team, _ = make_organization_and_user()
+    team, _ = make_organization_and_user()
     # Some short timeout and last_heartbeat_time to make sure that heartbeat is expired
     timeout = 1
     last_heartbeat_time = timezone.now() - timezone.timedelta(seconds=timeout * 10)
-    alert_receive_channel = make_alert_receive_channel(amixr_team, integration=integration)
+    alert_receive_channel = make_alert_receive_channel(team, integration=integration)
     integration_heartbeat = make_integration_heartbeat(
         alert_receive_channel, timeout, last_heartbeat_time=last_heartbeat_time
     )
@@ -30,11 +30,11 @@ def test_integration_heartbeat_expired(
 def test_integration_heartbeat_already_expired(
     mocked_handler, make_organization_and_user, make_alert_receive_channel, make_integration_heartbeat, integration
 ):
-    amixr_team, _ = make_organization_and_user()
+    team, _ = make_organization_and_user()
     # Some short timeout and last_heartbeat_time to make sure that heartbeat is expired
     timeout = 1
     last_heartbeat_time = timezone.now() - timezone.timedelta(seconds=timeout * 10)
-    alert_receive_channel = make_alert_receive_channel(amixr_team, integration=integration)
+    alert_receive_channel = make_alert_receive_channel(team, integration=integration)
     integration_heartbeat = make_integration_heartbeat(
         alert_receive_channel,
         timeout,
@@ -51,11 +51,11 @@ def test_integration_heartbeat_already_expired(
 def test_integration_heartbeat_restored(
     mocked_handler, make_organization_and_user, make_alert_receive_channel, make_integration_heartbeat, integration
 ):
-    amixr_team, _ = make_organization_and_user()
+    team, _ = make_organization_and_user()
     # Some long timeout and last_heartbeat_time to make sure that heartbeat is not expired
     timeout = 1000
     last_heartbeat_time = timezone.now()
-    alert_receive_channel = make_alert_receive_channel(amixr_team, integration=integration)
+    alert_receive_channel = make_alert_receive_channel(team, integration=integration)
     integration_heartbeat = make_integration_heartbeat(
         alert_receive_channel,
         timeout,
@@ -72,11 +72,11 @@ def test_integration_heartbeat_restored(
 def test_integration_heartbeat_restored_and_alert_was_not_sent(
     mocked_handler, make_organization_and_user, make_alert_receive_channel, make_integration_heartbeat, integration
 ):
-    amixr_team, _ = make_organization_and_user()
+    team, _ = make_organization_and_user()
     # Some long timeout and last_heartbeat_time to make sure that heartbeat is not expired
     timeout = 1000
     last_heartbeat_time = timezone.now()
-    alert_receive_channel = make_alert_receive_channel(amixr_team, integration=integration)
+    alert_receive_channel = make_alert_receive_channel(team, integration=integration)
     integration_heartbeat = make_integration_heartbeat(
         alert_receive_channel,
         timeout,
