cd37e53870a71a79f65ed46b6d053815943fb44f
Rares Mardare <rares.mardare@grafana.com>
2023-08-15 00:26:14 +0300

    import fix

--- grafana-plugin/src/components/IntegrationSendDemoAlertModal/IntegrationSendDemoAlertModal.tsx
+++ grafana-plugin/src/components/IntegrationSendDemoAlertModal/IntegrationSendDemoAlertModal.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import { Button, HorizontalGroup, Icon, Modal, Tooltip, VerticalGroup } from '@grafana/ui';
 import cn from 'classnames/bind';
