057d87f521f88ce47eea3554b7df78d822d70245
Michael Derynck <michael.derynck@grafana.com>
2023-08-16 07:42:52 -0600

    Remove deprecated endpoints

--- engine/apps/grafana_plugin/views/status.py
+++ engine/apps/grafana_plugin/views/status.py
@@ -5,7 +5,6 @@ from rest_framework.views import APIView
 
 from apps.auth_token.auth import BasePluginAuthentication
 from apps.base.models import DynamicSetting
-from apps.grafana_plugin.helpers import GrafanaAPIClient
 from apps.grafana_plugin.tasks.sync import plugin_sync_organization_async
 from apps.mobile_app.auth import MobileAppAuthTokenAuthentication
 from apps.user_management.models import Organization
@@ -71,35 +70,5 @@ class StatusView(GrafanaHeadersMixin, APIView):
                 "recaptcha_site_key": settings.RECAPTCHA_V3_SITE_KEY,
                 "currently_undergoing_maintenance_message": settings.CURRENTLY_UNDERGOING_MAINTENANCE_MESSAGE,
                 "api_url": api_url,
-            }
-        )
-
-    def get(self, _request: Request) -> Response:
-        """Deprecated. May be used for the plugins with versions < 1.3.17"""
-        stack_id = self.instance_context["stack_id"]
-        org_id = self.instance_context["org_id"]
-        is_installed = False
-        token_ok = False
-        allow_signup = True
-
-        if organization := Organization.objects.filter(stack_id=stack_id, org_id=org_id).first():
-            is_installed = True
-            _, resp = GrafanaAPIClient(api_url=organization.grafana_url, api_token=organization.api_token).check_token()
-            token_ok = resp["connected"]
-        else:
-            from apps.base.models import DynamicSetting
-
-            allow_signup = DynamicSetting.objects.get_or_create(
-                name="allow_plugin_organization_signup", defaults={"boolean_value": True}
-            )[0].boolean_value
-
-        return Response(
-            data={
-                "is_installed": is_installed,
-                "token_ok": token_ok,
-                "allow_signup": allow_signup,
-                "is_user_anonymous": self.grafana_context.get("IsAnonymous", _request.user is None),
-                "license": settings.LICENSE,
-                "version": settings.VERSION,
             }
         )
