56c0c023ad28553cf77b7f4587ffe2f6e1820642
Michael Derynck <michael.derynck@grafana.com>
2023-08-31 12:42:08 -0600

    Test celery tasks have queue assignment (#2922)
    # What this PR does
    
    - Add a test to ensure celery tasks are assigned to a queue
    - Move CELERY_TASK_ROUTES out of settings into its own file for easier
    reuse and reference.
    
    ## Which issue(s) this PR fixes
    
    ## Checklist
    
    - [x] Unit, integration, and e2e (if applicable) tests updated
    - [x] Documentation added (or `pr:no public docs` PR label added if not
    required)
    - [x] `CHANGELOG.md` updated (or `pr:no changelog` PR label added if not
    required)

---
+++ engine/common/tests/test_task_queue_assignment.py
@@ -0,0 +1,35 @@
+from celery import current_app
+
+from settings.celery_task_routes import CELERY_TASK_ROUTES
+
+"""
+If a task has a legitimate reason to not have a queue assignment it can
+be added here (In development, in process of deprecation, etc.) if possible
+we should avoid @shared_dedicated_queue_retry_task or @shared_task and
+remove entirely if it is not needed.
+"""
+COMMON_IGNORED_TASKS = {
+    "apps.mobile_app.tasks.new_alert_group.notify_user_async",
+    "apps.alerts.tasks.create_contact_points_for_datasource.schedule_create_contact_points_for_datasource",
+    "common.oncall_gateway.tasks.create_slack_connector_async_v2",
+}
+
+
+def check_celery_task_route_mapping(task_ids, ignored_prefixes, additional_ignored_tasks=None):
+    tasks = set(k for k in current_app.tasks.keys() if not k.startswith(ignored_prefixes))
+    tasks -= set(COMMON_IGNORED_TASKS)
+    if additional_ignored_tasks:
+        tasks -= set(additional_ignored_tasks)
+    tasks -= set(task_ids)
+    if tasks:
+        print(f"Unassigned queue for celery task {tasks}")
+    assert len(tasks) == 0
+
+
+def test_celery_task_route_mapping():
+    """
+    If this test does not pass make sure you have added any newly added
+    @shared_dedicated_queue_retry_task or @shared_task to CELERY_TASK_ROUTES
+    in engine/settings/celery_task_routes.py
+    """
+    check_celery_task_route_mapping(CELERY_TASK_ROUTES.keys(), ("extensions", "celery"))
