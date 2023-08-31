046464ed38a0385ea59218bf75e1a4f1405bbb93
Michael Derynck <michael.derynck@grafana.com>
2023-08-31 11:57:09 -0600

    Fix ignored tasks for extensions

--- engine/common/tests/test_task_queue_assignment.py
+++ engine/common/tests/test_task_queue_assignment.py
@@ -8,17 +8,18 @@ be added here (In development, in process of deprecation, etc.) if possible
 we should avoid @shared_dedicated_queue_retry_task or @shared_task and
 remove entirely if it is not needed.
 """
-IGNORED_TASKS = [
+COMMON_IGNORED_TASKS = {
     "apps.mobile_app.tasks.new_alert_group.notify_user_async",
     "apps.alerts.tasks.create_contact_points_for_datasource.schedule_create_contact_points_for_datasource",
     "common.oncall_gateway.tasks.create_slack_connector_async_v2",
-]
+}
 
 
-def check_celery_task_route_mapping(task_ids, ignored_prefixes, ignored_tasks=None):
+def check_celery_task_route_mapping(task_ids, ignored_prefixes, additional_ignored_tasks=None):
     tasks = set(k for k in current_app.tasks.keys() if not k.startswith(ignored_prefixes))
-    if ignored_tasks:
-        tasks -= set(ignored_tasks)
+    tasks -= set(COMMON_IGNORED_TASKS)
+    if additional_ignored_tasks:
+        tasks -= set(additional_ignored_tasks)
     tasks -= set(task_ids)
     if tasks:
         print(f"Unassigned queue for celery task {tasks}")
@@ -31,4 +32,4 @@ def test_celery_task_route_mapping():
     @shared_dedicated_queue_retry_task or @shared_task to CELERY_TASK_ROUTES
     in engine/settings/celery_task_routes.py
     """
-    check_celery_task_route_mapping(CELERY_TASK_ROUTES.keys(), ("extensions", "celery"), IGNORED_TASKS)
+    check_celery_task_route_mapping(CELERY_TASK_ROUTES.keys(), ("extensions", "celery"))
