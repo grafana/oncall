d90cf0f59325770dc452d1e618a5a45ed4354aba
Vadim Stepanov <vadimkerr@gmail.com>
2023-09-07 09:42:30 +0100

    Fix Slack integration leftovers after disconnecting (#2986)
    # What this PR does
    
    Improve Slack disconnect cleanup:
    - Reset Slack user group for schedules
    - Reset Slack user identity for deleted users
    
    ## Which issue(s) this PR fixes
    
    https://github.com/grafana/oncall-private/issues/1962
    
    ## Checklist
    
    - [x] Unit, integration, and e2e (if applicable) tests updated
    - [x] Documentation added (or `pr:no public docs` PR label added if not
    required)
    - [x] `CHANGELOG.md` updated (or `pr:no changelog` PR label added if not
    required)

--- engine/apps/slack/tasks.py
+++ engine/apps/slack/tasks.py
@@ -162,17 +162,13 @@ def unpopulate_slack_user_identities(organization_pk, force=False, ts=None):
 
     organization = Organization.objects.get(pk=organization_pk)
 
-    users_to_update = []
-    for user in organization.users.filter(slack_user_identity__isnull=False):
-        user.slack_user_identity = None
-        users_to_update.append(user)
-
-    User.objects.bulk_update(users_to_update, ["slack_user_identity"], batch_size=5000)
+    # Reset slack_user_identity for organization users (make sure to include deleted users in the queryset)
+    User.objects.filter_with_deleted(organization=organization).update(slack_user_identity=None)
 
     if force:
         organization.slack_team_identity = None
         organization.general_log_channel_id = None
-        organization.save()
+        organization.save(update_fields=["slack_team_identity", "general_log_channel_id"])
 
 
 @shared_dedicated_queue_retry_task(autoretry_for=(Exception,), retry_backoff=True, max_retries=0)
@@ -626,12 +622,9 @@ def clean_slack_integration_leftovers(organization_id, *args, **kwargs):
     from apps.alerts.models import ChannelFilter
     from apps.schedules.models import OnCallSchedule
 
-    logger.info(f"Start clean slack leftovers for organization {organization_id}")
+    logger.info(f"Cleaning up for organization {organization_id}")
     ChannelFilter.objects.filter(alert_receive_channel__organization_id=organization_id).update(slack_channel_id=None)
-    logger.info(f"Cleaned ChannelFilters slack_channel_id for organization {organization_id}")
-    OnCallSchedule.objects.filter(organization_id=organization_id).update(channel=None)
-    logger.info(f"Cleaned OnCallSchedule slack_channel_id for organization {organization_id}")
-    logger.info(f"Finish clean slack leftovers for organization {organization_id}")
+    OnCallSchedule.objects.filter(organization_id=organization_id).update(channel=None, user_group=None)
 
 
 @shared_dedicated_queue_retry_task(autoretry_for=(Exception,), retry_backoff=True, max_retries=10)
