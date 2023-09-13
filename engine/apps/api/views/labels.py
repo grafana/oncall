d15ce74c0f98d8dd7a29bace8ccaedd52b518fe4
Julia <ferril.darkdiver@gmail.com>
2023-09-13 12:29:18 +0200

    Update labels api views

--- engine/apps/api/views/labels.py
+++ engine/apps/api/views/labels.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
-from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.exceptions import NotFound
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -16,63 +15,59 @@ class LabelsCRUDView(APIView):
     authentication_classes = (PluginAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    def dispatch(self, request, *args, **kwargs):
+    def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
         if not is_labels_enabled(self.request.auth.organization):
-            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-        return super().dispatch(request, *args, **kwargs)
+            raise NotFound
 
     def get(self, request):  # todo
-        # organization = self.request.auth.organization
+        organization = self.request.auth.organization
         key_id = self.request.query_params.get("keyID")
         if key_id:
-            result = LabelsAPIClient().get_labels_keys()
+            result = LabelsAPIClient(organization).get_labels_keys()
         else:
-            result = LabelsAPIClient().get_label_key_values(key_id)
+            result = LabelsAPIClient(organization).get_label_key_values(key_id)
         return Response(result)
 
     def post(self, request):  # todo
-        # organization = self.request.auth.organization
+        organization = self.request.auth.organization
         # {
         #     "key": {"repr": "severity"},
-        #     "values": [{"repr": "critical"}]
+        #     "values": [{"repr": "critical"}]  # []
         # }
         label_data = self.request.data
-        LabelsAPIClient().create_label(label_data)
-        return Response(status=status.HTTP_201_CREATED)
+        result = LabelsAPIClient(organization).create_label(label_data)
+        return Response(status=result.status_code)
 
     def put(self, request):  # todo
-        # organization = self.request.auth.organization
+        organization = self.request.auth.organization
         key_id = self.request.query_params.get("keyID")
-        if not key_id:
-            raise BadRequest()
         label_action = self.request.query_params.get("action")
-        # todo: update key name, update value name, add new value
-        # {
-        #     "key_id"
-        #     "key_name"
-        #     "value_id"
-        #     "value_name"
-        # }
+        label_data = self.request.data
+        if not key_id or not label_action or not label_data:
+            raise BadRequest()
         if label_action == "key_update":
-            key_name = self.request.data.get("key_name")
-            if key_name:
-                result = LabelsAPIClient().update_label_key(key_id, {"key_name": key_name})
-                return Response(result)
-            raise BadRequest
+            result = LabelsAPIClient(organization).update_label_key(key_id, label_data)
         elif label_action == "value_update":
-            # value_id = self.request.query_params.get("valueID")
-            # value_name = self.request.data.get("value_name")
-            pass
+            value_id = self.request.query_params.get("valueID")
+            if not value_id:
+                raise BadRequest()
+            result = LabelsAPIClient(organization).update_label_value(key_id, value_id, label_data)
         elif label_action == "value_add":
             # {"repr": "warning"}
-            # value_name = self.request.data.get("value_name")
-            pass
-
-        return Response()
+            result = LabelsAPIClient(organization).add_value(key_id, label_data)
+        else:
+            raise BadRequest()
+        # result.raise_for_status()
+        return Response(status=result.status_code)
 
 
 class LabelsAssociatingMixin:  # use for labelable objects views (ex. AlertReceiveChannelView)
+    def check_if_label_feature_enabled(self):
+        """Call this method to block if labels feature is not enabled"""
+        if not is_labels_enabled(self.request.auth.organization):
+            raise NotFound
+
     def filter_by_labels(self, queryset):
         """Call this method in `get_queryset()` to add filtering by labels"""
         labels = self.request.query_params.getlist("label")  # ["key1,value1", "key2,value2"]
@@ -88,6 +83,7 @@ class LabelsAssociatingMixin:  # use for labelable objects views (ex. AlertRecei
 
     @action(methods=["get"], detail=False)
     def labels(self, request):
+        self.check_if_label_feature_enabled()
         # associating_labels_class = self.model.associating_labels_class  # todo
         associating_labels_class = AlertReceiveChannelAssociatedLabel
         associated_labels_pk = (
@@ -101,6 +97,7 @@ class LabelsAssociatingMixin:  # use for labelable objects views (ex. AlertRecei
 
     @action(methods=["post"], detail=True)
     def associate_label(self, request, pk):
+        self.check_if_label_feature_enabled()
         organization = self.request.auth.organization
         key_id = request.data.get("key_id")
         value_id = request.data.get("value_id")
@@ -110,6 +107,7 @@ class LabelsAssociatingMixin:  # use for labelable objects views (ex. AlertRecei
 
     @action(methods=["post"], detail=True)
     def remove_label(self, request, pk):
+        self.check_if_label_feature_enabled()
         organization = self.request.auth.organization
         key_id = request.data.get("key_id")
         value_id = request.data.get("value_id")
