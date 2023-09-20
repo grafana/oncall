32fd15cee874552af673d7b8bba9d53519a5c1a4
Julia <ferril.darkdiver@gmail.com>
2023-09-20 11:53:08 +0200

    Fix labels CRUD

--- engine/apps/api/views/labels.py
+++ engine/apps/api/views/labels.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 
 from apps.auth_token.auth import PluginAuthentication
 from apps.labels.client import LabelsAPIClient
-from apps.labels.models import AlertReceiveChannelAssociatedLabel, Label
+from apps.labels.models import Label, get_associating_label_model
 from apps.labels.utils import is_labels_enabled
 from common.api_helpers.exceptions import BadRequest
 
@@ -14,22 +14,23 @@ from common.api_helpers.exceptions import BadRequest
 class LabelsCRUDView(APIView):
     authentication_classes = (PluginAuthentication,)
     permission_classes = (IsAuthenticated,)
+    # todo: permissions on create/update labels
 
-    def initial(self, request, *args, **kwargs):
-        super().initial(request, *args, **kwargs)
-        if not is_labels_enabled(self.request.auth.organization):
-            raise NotFound
+    # def initial(self, request, *args, **kwargs):
+    #     super().initial(request, *args, **kwargs)
+    #     if not is_labels_enabled(self.request.auth.organization):
+    #         raise NotFound
 
     def get(self, request):  # todo
         organization = self.request.auth.organization
         key_id = self.request.query_params.get("keyID")
         if key_id:
-            result = LabelsAPIClient(organization).get_label_key_values(key_id)
+            result, _ = LabelsAPIClient(organization.grafana_url, organization.api_token).get_label_key_values(key_id)
             # todo: update cache
         else:
-            result = LabelsAPIClient(organization).get_labels_keys()
+            result, _ = LabelsAPIClient(organization.grafana_url, organization.api_token).get_labels_keys()
             # todo: update cache
-        return Response(result.data, status=result.status_code)
+        return Response(result)
 
     def post(self, request):  # todo
         organization = self.request.auth.organization
@@ -38,30 +39,33 @@ class LabelsCRUDView(APIView):
         #     "values": [{"repr": "critical"}]  # []
         # }
         label_data = self.request.data
-        result = LabelsAPIClient(organization).create_label(label_data)
-        return Response(status=result.status_code)
+        if not label_data:
+            raise BadRequest()
+        key_id = self.request.query_params.get("keyID")
+        if key_id:
+            # {"repr": "warning"}
+            result, _ = LabelsAPIClient(organization.grafana_url, organization.api_token).add_value(key_id, label_data)
+        else:
+            result, _ = LabelsAPIClient(organization.grafana_url, organization.api_token).create_label(label_data)
+        return Response()
 
     def put(self, request):  # todo
         organization = self.request.auth.organization
         key_id = self.request.query_params.get("keyID")
-        label_action = self.request.query_params.get("action")
+        value_id = self.request.query_params.get("valueID")
         label_data = self.request.data
-        if not key_id or not label_action or not label_data:
+        if not key_id or not label_data:
             raise BadRequest()
-        if label_action == "key_update":
-            result = LabelsAPIClient(organization).update_label_key(key_id, label_data)
-        elif label_action == "value_update":
-            value_id = self.request.query_params.get("valueID")
-            if not value_id:
-                raise BadRequest()
-            result = LabelsAPIClient(organization).update_label_value(key_id, value_id, label_data)
-        elif label_action == "value_add":
-            # {"repr": "warning"}
-            result = LabelsAPIClient(organization).add_value(key_id, label_data)
+        if value_id:
+            result, _ = LabelsAPIClient(organization.grafana_url, organization.api_token).update_label_value(
+                key_id, value_id, label_data
+            )
         else:
-            raise BadRequest()
+            result, _ = LabelsAPIClient(organization.grafana_url, organization.api_token).update_label_key(
+                key_id, label_data
+            )
         # result.raise_for_status()
-        return Response(status=result.status_code)
+        return Response()
 
 
 class LabelsAssociatingMixin:  # use for labelable objects views (ex. AlertReceiveChannelView)
@@ -78,16 +82,15 @@ class LabelsAssociatingMixin:  # use for labelable objects views (ex. AlertRecei
         keys = []
         values = []
         for label in labels:
-            key_id, value_id = label.split(",")
+            key_id, value_id = label.split(":")
             keys.append(key_id)
             values.append(value_id)
         return queryset.filter(labels__key_id__in=keys, labels__value_id__in=values)
 
     @action(methods=["get"], detail=False)
-    def labels(self, request):
+    def labels_filter(self, request):  # todo
         self.check_if_label_feature_enabled()
-        # associating_labels_class = self.model.associating_labels_class  # todo
-        associating_labels_class = AlertReceiveChannelAssociatedLabel
+        associating_labels_class = get_associating_label_model(self.model)  # todo
         associated_labels_pk = (
             associating_labels_class.objects.filter(alert_receive_channel__organization=self.request.auth.organization)
             .values_list("label_id", flat=True)
