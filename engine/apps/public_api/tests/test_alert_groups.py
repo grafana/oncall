33ef807d3801a70621479037223f8290458ba3f0
Vadim Stepanov <vadimkerr@gmail.com>
2023-10-05 10:43:45 +0100

    API alert group deletion

--- engine/apps/public_api/tests/test_alert_groups.py
+++ engine/apps/public_api/tests/test_alert_groups.py
@@ -1,4 +1,3 @@
-from unittest import mock
 from unittest.mock import patch
 
 import pytest
@@ -8,34 +7,35 @@ from rest_framework.test import APIClient
 
 from apps.alerts.constants import ActionSource
 from apps.alerts.models import AlertGroup, AlertReceiveChannel
+from apps.alerts.tasks import delete_alert_group, wipe
 
 
-def construct_expected_response_from_incidents(incidents):
+def construct_expected_response_from_alert_groups(alert_groups):
     results = []
-    for incident in incidents:
+    for alert_group in alert_groups:
         # convert datetimes to serializers.DateTimeField
         created_at = None
-        if incident.started_at:
-            created_at = incident.started_at.isoformat()
+        if alert_group.started_at:
+            created_at = alert_group.started_at.isoformat()
             created_at = created_at[:-6] + "Z"
 
         resolved_at = None
-        if incident.resolved_at:
-            resolved_at = incident.resolved_at.isoformat()
+        if alert_group.resolved_at:
+            resolved_at = alert_group.resolved_at.isoformat()
             resolved_at = resolved_at[:-6] + "Z"
 
         acknowledged_at = None
-        if incident.acknowledged_at:
-            acknowledged_at = incident.acknowledged_at.isoformat()
+        if alert_group.acknowledged_at:
+            acknowledged_at = alert_group.acknowledged_at.isoformat()
             acknowledged_at = acknowledged_at[:-6] + "Z"
 
         results.append(
             {
-                "id": incident.public_primary_key,
-                "integration_id": incident.channel.public_primary_key,
-                "route_id": incident.channel_filter.public_primary_key,
-                "alerts_count": incident.alerts.count(),
-                "state": incident.state,
+                "id": alert_group.public_primary_key,
+                "integration_id": alert_group.channel.public_primary_key,
+                "route_id": alert_group.channel_filter.public_primary_key,
+                "alerts_count": alert_group.alerts.count(),
+                "state": alert_group.state,
                 "created_at": created_at,
                 "resolved_at": resolved_at,
                 "acknowledged_at": acknowledged_at,
@@ -43,12 +43,12 @@ def construct_expected_response_from_incidents(incidents):
                 "permalinks": {
                     "slack": None,
                     "telegram": None,
-                    "web": incident.web_link,
+                    "web": alert_group.web_link,
                 },
             }
         )
     return {
-        "count": incidents.count(),
+        "count": alert_groups.count(),
         "next": None,
         "previous": None,
         "results": results,
@@ -59,7 +59,7 @@ def construct_expected_response_from_incidents(incidents):
 
 
 @pytest.fixture()
-def incident_public_api_setup(
+def alert_group_public_api_setup(
     make_organization_and_user_with_token,
     make_alert_receive_channel,
     make_channel_filter,
@@ -76,48 +76,52 @@ def incident_public_api_setup(
     grafana_non_default_route = make_channel_filter(grafana, filtering_term="us-east")
     formatted_webhook_default_route = make_channel_filter(formatted_webhook, is_default=True)
 
-    grafana_incident_default_route = make_alert_group(grafana, channel_filter=grafana_default_route)
-    grafana_incident_non_default_route = make_alert_group(grafana, channel_filter=grafana_non_default_route)
-    formatted_webhook_incident = make_alert_group(formatted_webhook, channel_filter=formatted_webhook_default_route)
+    grafana_alert_group_default_route = make_alert_group(grafana, channel_filter=grafana_default_route)
+    grafana_alert_group_non_default_route = make_alert_group(grafana, channel_filter=grafana_non_default_route)
+    formatted_webhook_alert_group = make_alert_group(formatted_webhook, channel_filter=formatted_webhook_default_route)
 
-    make_alert(alert_group=grafana_incident_default_route, raw_request_data=grafana.config.example_payload)
-    make_alert(alert_group=grafana_incident_non_default_route, raw_request_data=grafana.config.example_payload)
-    make_alert(alert_group=formatted_webhook_incident, raw_request_data=grafana.config.example_payload)
+    make_alert(alert_group=grafana_alert_group_default_route, raw_request_data=grafana.config.example_payload)
+    make_alert(alert_group=grafana_alert_group_non_default_route, raw_request_data=grafana.config.example_payload)
+    make_alert(alert_group=formatted_webhook_alert_group, raw_request_data=grafana.config.example_payload)
 
     integrations = grafana, formatted_webhook
-    incidents = grafana_incident_default_route, grafana_incident_non_default_route, formatted_webhook_incident
+    alert_groups = (
+        grafana_alert_group_default_route,
+        grafana_alert_group_non_default_route,
+        formatted_webhook_alert_group,
+    )
     routes = grafana_default_route, grafana_non_default_route, formatted_webhook_default_route
 
-    return token, incidents, integrations, routes
+    return token, alert_groups, integrations, routes
 
 
 @pytest.mark.django_db
-def test_get_incidents(incident_public_api_setup):
-    token, _, _, _ = incident_public_api_setup
-    incidents = AlertGroup.objects.all().order_by("-started_at")
+def test_get_alert_groups(alert_group_public_api_setup):
+    token, _, _, _ = alert_group_public_api_setup
+    alert_groups = AlertGroup.objects.all().order_by("-started_at")
     client = APIClient()
-    expected_response = construct_expected_response_from_incidents(incidents)
+    expected_response = construct_expected_response_from_alert_groups(alert_groups)
 
     url = reverse("api-public:alert_groups-list")
-    response = client.get(url, format="json", HTTP_AUTHORIZATION=f"{token}")
+    response = client.get(url, format="json", HTTP_AUTHORIZATION=token)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == expected_response
 
 
 @pytest.mark.django_db
-def test_get_incidents_filter_by_integration(
-    incident_public_api_setup,
+def test_get_alert_groups_filter_by_integration(
+    alert_group_public_api_setup,
 ):
-    token, incidents, integrations, _ = incident_public_api_setup
+    token, alert_groups, integrations, _ = alert_group_public_api_setup
     formatted_webhook = integrations[1]
-    incidents = AlertGroup.objects.filter(channel=formatted_webhook).order_by("-started_at")
-    expected_response = construct_expected_response_from_incidents(incidents)
+    alert_groups = AlertGroup.objects.filter(channel=formatted_webhook).order_by("-started_at")
+    expected_response = construct_expected_response_from_alert_groups(alert_groups)
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
     response = client.get(
-        url + f"?integration_id={formatted_webhook.public_primary_key}", format="json", HTTP_AUTHORIZATION=f"{token}"
+        url + f"?integration_id={formatted_webhook.public_primary_key}", format="json", HTTP_AUTHORIZATION=token
     )
 
     assert response.status_code == status.HTTP_200_OK
@@ -125,109 +129,109 @@ def test_get_incidents_filter_by_integration(
 
 
 @pytest.mark.django_db
-def test_get_incidents_filter_by_state_new(
-    incident_public_api_setup,
+def test_get_alert_groups_filter_by_state_new(
+    alert_group_public_api_setup,
 ):
-    token, _, _, _ = incident_public_api_setup
-    incidents = AlertGroup.objects.filter(AlertGroup.get_new_state_filter()).order_by("-started_at")
-    expected_response = construct_expected_response_from_incidents(incidents)
+    token, _, _, _ = alert_group_public_api_setup
+    alert_groups = AlertGroup.objects.filter(AlertGroup.get_new_state_filter()).order_by("-started_at")
+    expected_response = construct_expected_response_from_alert_groups(alert_groups)
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
-    response = client.get(url + "?state=new", format="json", HTTP_AUTHORIZATION=f"{token}")
+    response = client.get(url + "?state=new", format="json", HTTP_AUTHORIZATION=token)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == expected_response
 
 
 @pytest.mark.django_db
-def test_get_incidents_filter_by_state_acknowledged(
-    incident_public_api_setup,
+def test_get_alert_groups_filter_by_state_acknowledged(
+    alert_group_public_api_setup,
 ):
-    token, _, _, _ = incident_public_api_setup
-    incidents = AlertGroup.objects.filter(AlertGroup.get_acknowledged_state_filter()).order_by("-started_at")
-    expected_response = construct_expected_response_from_incidents(incidents)
+    token, _, _, _ = alert_group_public_api_setup
+    alert_groups = AlertGroup.objects.filter(AlertGroup.get_acknowledged_state_filter()).order_by("-started_at")
+    expected_response = construct_expected_response_from_alert_groups(alert_groups)
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
-    response = client.get(url + "?state=acknowledged", format="json", HTTP_AUTHORIZATION=f"{token}")
+    response = client.get(url + "?state=acknowledged", format="json", HTTP_AUTHORIZATION=token)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == expected_response
 
 
 @pytest.mark.django_db
-def test_get_incidents_filter_by_state_silenced(
-    incident_public_api_setup,
+def test_get_alert_groups_filter_by_state_silenced(
+    alert_group_public_api_setup,
 ):
-    token, _, _, _ = incident_public_api_setup
-    incidents = AlertGroup.objects.filter(AlertGroup.get_silenced_state_filter()).order_by("-started_at")
-    expected_response = construct_expected_response_from_incidents(incidents)
+    token, _, _, _ = alert_group_public_api_setup
+    alert_groups = AlertGroup.objects.filter(AlertGroup.get_silenced_state_filter()).order_by("-started_at")
+    expected_response = construct_expected_response_from_alert_groups(alert_groups)
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
-    response = client.get(url + "?state=silenced", format="json", HTTP_AUTHORIZATION=f"{token}")
+    response = client.get(url + "?state=silenced", format="json", HTTP_AUTHORIZATION=token)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == expected_response
 
 
 @pytest.mark.django_db
-def test_get_incidents_filter_by_state_resolved(
-    incident_public_api_setup,
+def test_get_alert_groups_filter_by_state_resolved(
+    alert_group_public_api_setup,
 ):
-    token, _, _, _ = incident_public_api_setup
-    incidents = AlertGroup.objects.filter(AlertGroup.get_resolved_state_filter()).order_by("-started_at")
-    expected_response = construct_expected_response_from_incidents(incidents)
+    token, _, _, _ = alert_group_public_api_setup
+    alert_groups = AlertGroup.objects.filter(AlertGroup.get_resolved_state_filter()).order_by("-started_at")
+    expected_response = construct_expected_response_from_alert_groups(alert_groups)
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
-    response = client.get(url + "?state=resolved", format="json", HTTP_AUTHORIZATION=f"{token}")
+    response = client.get(url + "?state=resolved", format="json", HTTP_AUTHORIZATION=token)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == expected_response
 
 
 @pytest.mark.django_db
-def test_get_incidents_filter_by_state_unknown(
-    incident_public_api_setup,
+def test_get_alert_groups_filter_by_state_unknown(
+    alert_group_public_api_setup,
 ):
-    token, _, _, _ = incident_public_api_setup
+    token, _, _, _ = alert_group_public_api_setup
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
-    response = client.get(url + "?state=unknown", format="json", HTTP_AUTHORIZATION=f"{token}")
+    response = client.get(url + "?state=unknown", format="json", HTTP_AUTHORIZATION=token)
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
 @pytest.mark.django_db
-def test_get_incidents_filter_by_integration_no_result(
-    incident_public_api_setup,
+def test_get_alert_groups_filter_by_integration_no_result(
+    alert_group_public_api_setup,
 ):
-    token, _, _, _ = incident_public_api_setup
+    token, _, _, _ = alert_group_public_api_setup
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
-    response = client.get(url + "?integration_id=impossible_integration", format="json", HTTP_AUTHORIZATION=f"{token}")
+    response = client.get(url + "?integration_id=impossible_integration", format="json", HTTP_AUTHORIZATION=token)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["results"] == []
 
 
 @pytest.mark.django_db
-def test_get_incidents_filter_by_route(
-    incident_public_api_setup,
+def test_get_alert_groups_filter_by_route(
+    alert_group_public_api_setup,
 ):
-    token, incidents, integrations, routes = incident_public_api_setup
+    token, alert_groups, integrations, routes = alert_group_public_api_setup
     grafana_non_default_route = routes[1]
-    incidents = AlertGroup.objects.filter(channel_filter=grafana_non_default_route).order_by("-started_at")
-    expected_response = construct_expected_response_from_incidents(incidents)
+    alert_groups = AlertGroup.objects.filter(channel_filter=grafana_non_default_route).order_by("-started_at")
+    expected_response = construct_expected_response_from_alert_groups(alert_groups)
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
     response = client.get(
-        url + f"?route_id={grafana_non_default_route.public_primary_key}", format="json", HTTP_AUTHORIZATION=f"{token}"
+        url + f"?route_id={grafana_non_default_route.public_primary_key}", format="json", HTTP_AUTHORIZATION=token
     )
 
     assert response.status_code == status.HTTP_200_OK
@@ -235,56 +239,58 @@ def test_get_incidents_filter_by_route(
 
 
 @pytest.mark.django_db
-def test_get_incidents_filter_by_route_no_result(
-    incident_public_api_setup,
+def test_get_alert_groups_filter_by_route_no_result(
+    alert_group_public_api_setup,
 ):
-    token, _, _, _ = incident_public_api_setup
+    token, _, _, _ = alert_group_public_api_setup
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
-    response = client.get(url + "?route_id=impossible_route_ir", format="json", HTTP_AUTHORIZATION=f"{token}")
+    response = client.get(url + "?route_id=impossible_route_ir", format="json", HTTP_AUTHORIZATION=token)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["results"] == []
 
 
-@mock.patch("apps.public_api.views.incidents.delete_alert_group", return_value=None)
+@pytest.mark.parametrize(
+    "data,task,status_code",
+    [
+        (None, wipe, status.HTTP_204_NO_CONTENT),
+        ({"mode": "wipe"}, wipe, status.HTTP_204_NO_CONTENT),
+        ({"mode": "delete"}, delete_alert_group, status.HTTP_204_NO_CONTENT),
+        ({"mode": "random"}, None, status.HTTP_400_BAD_REQUEST),
+        ("delete", None, status.HTTP_400_BAD_REQUEST),
+    ],
+)
 @pytest.mark.django_db
-def test_delete_incident_success_response(mocked_task, incident_public_api_setup):
-    token, incidents, _, _ = incident_public_api_setup
-    grafana_first_incident = incidents[0]
+def test_delete_alert_group(alert_group_public_api_setup, data, task, status_code):
+    token, alert_groups, _, _ = alert_group_public_api_setup
+    alert_group = alert_groups[0]
+
     client = APIClient()
+    url = reverse("api-public:alert_groups-detail", kwargs={"pk": alert_group.public_primary_key})
 
-    url = reverse("api-public:alert_groups-detail", kwargs={"pk": grafana_first_incident.public_primary_key})
-    data = {"mode": "delete"}
-    response = client.delete(url, data=data, format="json", HTTP_AUTHORIZATION=f"{token}")
-    assert response.status_code == status.HTTP_204_NO_CONTENT
-    assert mocked_task.apply_async.call_count == 1
+    if task:
+        with patch.object(task, "apply_async") as mock_task:
+            response = client.delete(url, data=data, format="json", HTTP_AUTHORIZATION=token)
+            mock_task.assert_called_once()
+    else:
+        response = client.delete(url, data=data, format="json", HTTP_AUTHORIZATION=token)
 
-
-@pytest.mark.django_db
-def test_delete_incident_invalid_request(incident_public_api_setup):
-    token, incidents, _, _ = incident_public_api_setup
-    grafana_first_incident = incidents[0]
-    client = APIClient()
-
-    url = reverse("api-public:alert_groups-detail", kwargs={"pk": grafana_first_incident.public_primary_key})
-    data = "delete"
-    response = client.delete(url, data=data, format="json", HTTP_AUTHORIZATION=f"{token}")
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.status_code == status_code
 
 
 @pytest.mark.django_db
-def test_pagination(settings, incident_public_api_setup):
+def test_pagination(settings, alert_group_public_api_setup):
     settings.BASE_URL = "https://test.com/test/prefixed/urls"
 
-    token, incidents, _, _ = incident_public_api_setup
+    token, alert_groups, _, _ = alert_group_public_api_setup
     client = APIClient()
 
     url = reverse("api-public:alert_groups-list")
 
     with patch("common.api_helpers.paginators.PathPrefixedPagePagination.get_page_size", return_value=1):
-        response = client.get(url, HTTP_AUTHORIZATION=f"{token}")
+        response = client.get(url, HTTP_AUTHORIZATION=token)
 
     assert response.status_code == status.HTTP_200_OK
     result = response.json()
