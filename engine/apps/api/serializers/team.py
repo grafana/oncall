grafana/oncall

commit 697248dc75dc1a2afb73dbd257613fe453019089

Add responders improvements (#3128)
# What this PR does

https://www.loom.com/share/c5e10b5ec51343d0954c6f41cfd6a5fb

## Summary of backend changes
- Add `AlertReceiveChannel.get_orgs_direct_paging_integrations` method
and `AlertReceiveChannel.is_contactable` property. These are needed to
be able to (optionally) filter down teams, in the `GET /teams` internal
API endpoint
([here](https://github.com/grafana/oncall/pull/3128/files#diff-a4bd76e557f7e11dafb28a52c1034c075028c693b3c12d702d53c07fc6f24c05R55-R63)),
to just teams that have a "contactable" Direct Paging integration
- `engine/apps/alerts/paging.py`
- update these functions to support new UX. In short `direct_paging` no
longer takes a list of `ScheduleNotifications` or an `EscalationChain`
object
  - add `user_is_oncall` helper function
- add `_construct_title` helper function. In short if no `title` is
provided, which is the case for Direct Pages originating from OnCall
(either UI or Slack), then the format is `f"{from_user.username} is
paging <team.name (if team is specified> <comma separated list of
user.usernames> to join escalation"`
- `engine/apps/api/serializers/team.py` - add
`number_of_users_currently_oncall` attribute to response schema
([code](https://github.com/grafana/oncall/pull/3128/files#diff-26af48f796c9e987a76447586dd0f92349783d6ea6a0b6039a2f0f28bd58c2ebR45-R52))
- `engine/apps/api/serializers/user.py` - add `is_currently_oncall`
attribute to response schema
([code](https://github.com/grafana/oncall/pull/3128/files#diff-6744b5544ebb120437af98a996da5ad7d48ee1139a6112c7e3904010ab98f232R157-R162))
- `engine/apps/api/views/team.py` - add support for two new optional
query params `only_include_notifiable_teams` and `include_no_team`
([code](https://github.com/grafana/oncall/pull/3128/files#diff-a4bd76e557f7e11dafb28a52c1034c075028c693b3c12d702d53c07fc6f24c05R55-R70))
- `engine/apps/api/views/user.py`
- in the `GET /users` internal API endpoint, when specifying the

engine/apps/api/serializers/team.py
@@ -1,10 +1,28 @@
+import typing
+
 from rest_framework import serializers
 
+from apps.schedules.ical_utils import SchedulesOnCallUsers
 from apps.user_management.models import Team
 
 
-class TeamSerializer(serializers.ModelSerializer):
+class TeamSerializerContext(typing.TypedDict):
+    schedules_with_oncall_users: SchedulesOnCallUsers
+
+
+class FastTeamSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True, source="public_primary_key")
+
+    class Meta:
+        model = Team
+        fields = ["id", "name", "email", "avatar_url"]
+
+
+class TeamSerializer(serializers.ModelSerializer):
+    context: TeamSerializerContext
+
+    id = serializers.CharField(read_only=True, source="public_primary_key")
+    number_of_users_currently_oncall = serializers.SerializerMethodField()
 
     class Meta:
         model = Team
@@ -14,6 +32,7 @@ class TeamSerializer(serializers.ModelSerializer):
             "email",
             "avatar_url",
             "is_sharing_resources_to_all",
+            "number_of_users_currently_oncall",
         )
 
         read_only_fields = [
@@ -22,3 +41,12 @@ class TeamSerializer(serializers.ModelSerializer):
             "email",
             "avatar_url",
         ]
+
+    def get_number_of_users_currently_oncall(self, obj: Team) -> int:
+        num_of_users_oncall_for_team = 0
+
+        for schedule, users in self.context["schedules_with_oncall_users"].items():
+            if schedule.team == obj:
+                num_of_users_oncall_for_team += len(users)
+
+        return num_of_users_oncall_for_team

engine/apps/api/views/team.py
@@ -1,12 +1,15 @@
+from django.utils.functional import cached_property
 from rest_framework import mixins, viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from apps.alerts.paging import integration_is_notifiable
 from apps.api.permissions import RBACPermission
 from apps.api.serializers.team import TeamSerializer
 from apps.auth_token.auth import PluginAuthentication
 from apps.mobile_app.auth import MobileAppAuthTokenAuthentication
+from apps.schedules.ical_utils import get_oncall_users_for_multiple_schedules
 from apps.user_management.models import Team
 from common.api_helpers.mixins import PublicPrimaryKeyMixin
 
@@ -30,12 +33,41 @@ class TeamViewSet(PublicPrimaryKeyMixin, mixins.ListModelMixin, mixins.UpdateMod
     def get_queryset(self):
         return self.request.user.available_teams
 
+    @cached_property
+    def schedules_with_oncall_users(self):
+        """
+        The result of this method is cached and is reused for the whole lifetime of a request,
+        since self.get_serializer_context() is called multiple times for every instance in the queryset.
+        """
+        team_ids = [t.id for t in self.filter_queryset(self.get_queryset())]
+        team_schedules = self.request.user.organization.oncall_schedules.filter(team__id__in=team_ids)
+        return get_oncall_users_for_multiple_schedules(team_schedules)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"schedules_with_oncall_users": self.schedules_with_oncall_users})
+        return context
+
     def list(self, request, *args, **kwargs):
-        """
-        Adds general team to the queryset in a way that it always shows up first (even when not searched for).
-        """
-        general_team = Team(public_primary_key="null", name="No team", email=None, avatar_url=None)
-        queryset = [general_team] + list(self.filter_queryset(self.get_queryset()))
+        general_team = [Team(public_primary_key="null", name="No team", email=None, avatar_url=None)]
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if self.request.query_params.get("only_include_notifiable_teams", "false") == "true":
+            # filters down to only teams that have a direct paging integration that is "notifiable"
+            orgs_direct_paging_integrations = self.request.user.organization.get_direct_paging_integrations()
+            notifiable_direct_paging_integrations = [
+                i for i in orgs_direct_paging_integrations if integration_is_notifiable(i)
+            ]
+            team_ids = [i.team.pk for i in notifiable_direct_paging_integrations if i.team is not None]
+
+            queryset = queryset.filter(pk__in=team_ids)
+
+        teams = list(queryset)
+        if self.request.query_params.get("include_no_team", "true") != "false":
+            # Adds general team to the queryset in a way that it always shows up first (even when not searched for).
+            queryset = general_team + teams
+        else:
+            queryset = teams
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)

engine/apps/api/views/user.py
@@ -6,6 +6,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db.utils import IntegrityError
 from django.urls import reverse
 from django.utils import timezone
+from django.utils.functional import cached_property
 from django_filters import rest_framework as filters
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
@@ -15,7 +16,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.alerts.paging import check_user_availability
 from apps.api.permissions import (
     ALL_PERMISSION_CHOICES,
     IsOwnerOrHasRBACPermissions,
@@ -29,6 +29,7 @@ from apps.api.serializers.user import (
     CurrentUserSerializer,
     FilterUserSerializer,
     UserHiddenFieldsSerializer,
+    UserLongSerializer,
     UserSerializer,
 )
 from apps.api.throttlers import (
@@ -57,6 +58,7 @@ from apps.phone_notifications.exceptions import (
     ProviderNotSupports,
 )
 from apps.phone_notifications.phone_backend import PhoneBackend
+from apps.schedules.ical_utils import get_oncall_users_for_multiple_schedules
 from apps.schedules.models import OnCallSchedule
 from apps.telegram.client import TelegramClient
 from apps.telegram.models import TelegramVerificationCode
@@ -219,18 +221,39 @@ class UserView(
         "^username",
         "^slack_user_identity__cached_slack_login",
         "^slack_user_identity__cached_name",
+        "^teams__name",
     )
 
     filterset_class = UserFilter
+
+    @cached_property
+    def schedules_with_oncall_users(self):
+        """
+        The result of this method is cached and is reused for the whole lifetime of a request,
+        since self.get_serializer_context() is called multiple times for every instance in the queryset.
+        """
+        return get_oncall_users_for_multiple_schedules(self.request.user.organization.oncall_schedules.all())
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"schedules_with_oncall_users": self.schedules_with_oncall_users})
+        return context
 
     def get_serializer_class(self):
         request = self.request
         user = request.user
         kwargs = self.kwargs
+        query_params = request.query_params
 
-        is_filters_request = request.query_params.get("filters", "false") == "true"
-        if self.action in ["list"] and is_filters_request:
+        is_list_request = self.action in ["list"]
+        is_filters_request = query_params.get("filters", "false") == "true"
+        is_short_request = query_params.get("short", "true") == "false"
+        is_currently_oncall_request = query_params.get("is_currently_oncall", "").lower() in ["true", "false"]
+
+        if is_list_request and is_filters_request:
             return self.get_filter_serializer_class()
+        elif is_list_request and (is_short_request or is_currently_oncall_request):
+            return UserLongSerializer
 
         is_users_own_data = kwargs.get("pk") is not None and kwargs.get("pk") == user.public_primary_key
         has_admin_permission = user_is_authorized(user, [RBACPermission.Permissions.USER_SETTINGS_ADMIN])
@@ -253,9 +276,24 @@ class UserView(
 
     def list(self, request, *args, **kwargs) -> Response:
         queryset = self.filter_queryset(self.get_queryset())
+
+        is_currently_oncall_query_param = request.query_params.get("is_currently_oncall", "").lower()
+
+        def _get_oncall_user_ids():
+            return {user.pk for _, users in self.schedules_with_oncall_users.items() for user in users}
+
+        if is_currently_oncall_query_param == "true":
+            # client explicitly wants to filter out users that are on-call
+            queryset = queryset.filter(pk__in=_get_oncall_user_ids())
+        elif is_currently_oncall_query_param == "false":
+            # user explicitly wants to filter out on-call users
+            queryset = queryset.exclude(pk__in=_get_oncall_user_ids())
+
         page = self.paginate_queryset(queryset)
+
         if page is not None:
-            context = {"request": self.request, "format": self.format_kwarg, "view": self}
+            context = self.get_serializer_context()
+
             if settings.IS_OPEN_SOURCE:
                 if live_settings.GRAFANA_CLOUD_NOTIFICATIONS_ENABLED:
                     from apps.oss_installation.models import CloudConnector, CloudUserIdentity
@@ -274,7 +312,8 @@ class UserView(
         return Response(serializer.data)
 
     def retrieve(self, request, *args, **kwargs) -> Response:
-        context = {"request": self.request, "format": self.format_kwarg, "view": self}
+        context = self.get_serializer_context()
+
         try:
             instance = self.get_object()
         except NotFound:
@@ -647,12 +686,6 @@ class UserView(
                 raise NotFound
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-    @action(detail=True, methods=["get"])
-    def check_availability(self, request, pk) -> Response:
-        user = self.get_object()
-        warnings = check_user_availability(user=user)
-        return Response(data={"warnings": warnings}, status=status.HTTP_200_OK)
 
 
 def handle_phone_notificator_failed(exc: BaseFailed) -> Response:

grafana-plugin/src/containers/AddResponders/parts/AddRespondersPopup/AddRespondersPopup.module.scss
@@ -1,0 +1,48 @@
+.add-responders-dropdown {
+  border: var(--border-medium);
+  position: absolute;
+  right: 16px;
+  top: 55px;
+  background: var(--primary-background);
+  width: 340px;
+  z-index: 10;
+}
+
+.team-direct-paging-info-alert {
+  margin: 8px;
+}
+
+.learn-more-link {
+  display: inline-block;
+}
+
+.responder-item {
+  cursor: pointer;
+  width: 280px;
+  overflow: hidden;
+}
+
+.responders-filters {
+  margin: 8px;
+}
+
+.radio-buttons {
+  margin: 8px;
+}
+
+.table {
+  overflow: auto;
+  padding: 4px 0px;
+
+  & tr:hover {
+    background: var(--background-secondary) !important;
+  }
+
+  & tbody tr:nth-child(odd) {
+    background: unset;
+  }
+}
+
+.user-results-section-header {
+  padding: 10px 8px;
+}

commit d568ad6707faac245ebeb5bed51f36d0a39f5ce7

few add responders patches (#3220)
# Which issue(s) this PR fixes

Closes https://github.com/grafana/support-escalations/issues/8143

Fix a few minor issues introduced in #3128:

- Fix slow `GET /users` internal API endpoint related to [this
change](https://github.com/grafana/oncall/blob/dev/engine/apps/api/views/user.py#L239)
- Fix slow `GET /teams` internal API endpoint. Introduced a `short`
query parameter that only invokes
`apps.schedules.ical_utils.get_oncall_users_for_multiple_schedules` when
`short=false`.
- Order results from `GET /teams` internal API endpoint by name
(ascending)
- Fix search issue when searching for teams in the add responders popup
window (this was strictly a frontend issue)
- CSS changes to add responders dropdown to fix lonnnggg results list:
  **Before**
<img width="377" alt="Screenshot 2023-10-31 at 10 06 20"
src="https://github.com/grafana/oncall/assets/9406895/246c7c3b-7bea-44a1-afec-a38144c2c2d1">
  **After**
<img width="444" alt="Screenshot 2023-10-31 at 10 48 12"
src="https://github.com/grafana/oncall/assets/9406895/b5602a22-c691-4dc7-bd3d-e4d6b76d04a0">



## Still todo

The `apps.schedules.ical_utils.get_oncall_users_for_multiple_schedules`
method is still very slow when an instance has a lot of users (ex.
`ops`). Ideally we should refactor this method to be more efficient
because we still need to call this method under some circumstances. Ex.
to populate this dropdown when Direct Paging a user (note that it didn't
finish loading here on `ops`):
<img width="1037" alt="Screenshot 2023-10-30 at 18 14 59"
src="https://github.com/grafana/oncall/assets/9406895/9d91a57c-5db8-4ff9-862a-cd3755f52690">



## Checklist

- [x] Unit, integration, and e2e (if applicable) tests updated
- [x] Documentation added (or `pr:no public docs` PR label added if not
required)
- [x] `CHANGELOG.md` updated (or `pr:no changelog` PR label added if not
required)

engine/apps/api/serializers/team.py
@@ -19,27 +19,34 @@ class FastTeamSerializer(serializers.ModelSerializer):
 
 
 class TeamSerializer(serializers.ModelSerializer):
-    context: TeamSerializerContext
-
     id = serializers.CharField(read_only=True, source="public_primary_key")
-    number_of_users_currently_oncall = serializers.SerializerMethodField()
 
     class Meta:
         model = Team
-        fields = (
+        fields = [
             "id",
             "name",
             "email",
             "avatar_url",
             "is_sharing_resources_to_all",
-            "number_of_users_currently_oncall",
-        )
+        ]
 
         read_only_fields = [
             "id",
             "name",
             "email",
             "avatar_url",
+        ]
+
+
+class TeamLongSerializer(TeamSerializer):
+    context: TeamSerializerContext
+
+    number_of_users_currently_oncall = serializers.SerializerMethodField()
+
+    class Meta(TeamSerializer.Meta):
+        fields = TeamSerializer.Meta.fields + [
+            "number_of_users_currently_oncall",
         ]
 
     def get_number_of_users_currently_oncall(self, obj: Team) -> int:

engine/apps/api/views/team.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 
 from apps.alerts.paging import integration_is_notifiable
 from apps.api.permissions import RBACPermission
-from apps.api.serializers.team import TeamSerializer
+from apps.api.serializers.team import TeamLongSerializer, TeamSerializer
 from apps.auth_token.auth import PluginAuthentication
 from apps.mobile_app.auth import MobileAppAuthTokenAuthentication
 from apps.schedules.ical_utils import get_oncall_users_for_multiple_schedules
@@ -33,6 +33,9 @@ class TeamViewSet(PublicPrimaryKeyMixin, mixins.ListModelMixin, mixins.UpdateMod
     def get_queryset(self):
         return self.request.user.available_teams
 
+    def _is_long_request(self) -> bool:
+        return self.request.query_params.get("short", "true").lower() == "false"
+
     @cached_property
     def schedules_with_oncall_users(self):
         """
@@ -45,8 +48,13 @@ class TeamViewSet(PublicPrimaryKeyMixin, mixins.ListModelMixin, mixins.UpdateMod
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        context.update({"schedules_with_oncall_users": self.schedules_with_oncall_users})
+        context.update(
+            {"schedules_with_oncall_users": self.schedules_with_oncall_users if self._is_long_request() else {}}
+        )
         return context
+
+    def get_serializer_class(self):
+        return TeamLongSerializer if self._is_long_request() else TeamSerializer
 
     def list(self, request, *args, **kwargs):
         general_team = [Team(public_primary_key="null", name="No team", email=None, avatar_url=None)]
@@ -61,6 +69,8 @@ class TeamViewSet(PublicPrimaryKeyMixin, mixins.ListModelMixin, mixins.UpdateMod
             team_ids = [i.team.pk for i in notifiable_direct_paging_integrations if i.team is not None]
 
             queryset = queryset.filter(pk__in=team_ids)
+
+        queryset = queryset.order_by("name")
 
         teams = list(queryset)
         if self.request.query_params.get("include_no_team", "true") != "false":

engine/apps/api/views/user.py
@@ -234,9 +234,27 @@ class UserView(
         """
         return get_oncall_users_for_multiple_schedules(self.request.user.organization.oncall_schedules.all())
 
+    def _get_is_currently_oncall_query_param(self) -> str:
+        return self.request.query_params.get("is_currently_oncall", "").lower()
+
+    def _is_currently_oncall_request(self) -> bool:
+        return self._get_is_currently_oncall_query_param() in ["true", "false"]
+
+    def _is_long_request(self) -> bool:
+        return self.request.query_params.get("short", "true").lower() == "false"
+
+    def _is_currently_oncall_or_long_request(self) -> bool:
+        return self._is_currently_oncall_request() or self._is_long_request()
+
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        context.update({"schedules_with_oncall_users": self.schedules_with_oncall_users})
+        context.update(
+            {
+                "schedules_with_oncall_users": self.schedules_with_oncall_users
+                if self._is_currently_oncall_or_long_request()
+                else {}
+            }
+        )
         return context
 
     def get_serializer_class(self):
@@ -247,12 +265,10 @@ class UserView(
 
         is_list_request = self.action in ["list"]
         is_filters_request = query_params.get("filters", "false") == "true"
-        is_short_request = query_params.get("short", "true") == "false"
-        is_currently_oncall_request = query_params.get("is_currently_oncall", "").lower() in ["true", "false"]
 
         if is_list_request and is_filters_request:
             return self.get_filter_serializer_class()
-        elif is_list_request and (is_short_request or is_currently_oncall_request):
+        elif is_list_request and self._is_currently_oncall_or_long_request():
             return UserLongSerializer
 
         is_users_own_data = kwargs.get("pk") is not None and kwargs.get("pk") == user.public_primary_key
@@ -277,11 +293,10 @@ class UserView(
     def list(self, request, *args, **kwargs) -> Response:
         queryset = self.filter_queryset(self.get_queryset())
 
-        is_currently_oncall_query_param = request.query_params.get("is_currently_oncall", "").lower()
-
         def _get_oncall_user_ids():
             return {user.pk for _, users in self.schedules_with_oncall_users.items() for user in users}
 
+        is_currently_oncall_query_param = self._get_is_currently_oncall_query_param()
         if is_currently_oncall_query_param == "true":
             # client explicitly wants to filter out users that are on-call
             queryset = queryset.filter(pk__in=_get_oncall_user_ids())

grafana-plugin/src/containers/AddResponders/parts/AddRespondersPopup/AddRespondersPopup.module.scss
@@ -31,6 +31,7 @@
 }
 
 .table {
+  max-height: 150px;
   overflow: auto;
   padding: 4px 0px;
 

grafana-plugin/src/containers/AddResponders/parts/AddRespondersPopup/AddRespondersPopup.tsx
@@ -115,7 +115,7 @@ const AddRespondersPopup = observer(
 
     const handleSearchTermChange = useDebouncedCallback(() => {
       if (isCreateMode && activeOption === TabOptions.Teams) {
-        grafanaTeamStore.updateItems(searchTerm, false, true);
+        grafanaTeamStore.updateItems(searchTerm, false, true, false);
       } else {
         userStore.updateItems({ searchTerm, short: 'false' });
       }

grafana-plugin/src/models/grafana_team/grafana_team.ts
@@ -5,12 +5,14 @@ import { GrafanaTeam } from 'models/grafana_team/grafana_team.types';
 import { makeRequest } from 'network';
 import { RootStore } from 'state';
 
+type TeamItems = { [id: string]: GrafanaTeam };
+
 export class GrafanaTeamStore extends BaseStore {
   @observable
-  searchResult: { [key: string]: Array<GrafanaTeam['id']> } = {};
+  searchResult: Array<GrafanaTeam['id']> = [];
 
   @observable.shallow
-  items: { [id: string]: GrafanaTeam } = {};
+  items: TeamItems = {};
 
   constructor(rootStore: RootStore) {
     super(rootStore);
@@ -29,10 +31,11 @@ export class GrafanaTeamStore extends BaseStore {
   }
 
   @action
-  async updateItems(query = '', includeNoTeam = true, onlyIncludeNotifiableTeams = false) {
-    const result = await makeRequest(`${this.path}`, {
+  async updateItems(query = '', includeNoTeam = true, onlyIncludeNotifiableTeams = false, short = true) {
+    const result = await makeRequest<GrafanaTeam[]>(`${this.path}`, {
       params: {
         search: query,
+        short: short ? 'true' : 'false',
         include_no_team: includeNoTeam ? 'true' : 'false',
         only_include_notifiable_teams: onlyIncludeNotifiableTeams ? 'true' : 'false',
       },
@@ -40,8 +43,8 @@ export class GrafanaTeamStore extends BaseStore {
 
     this.items = {
       ...this.items,
-      ...result.reduce(
-        (acc: { [key: number]: GrafanaTeam }, item: GrafanaTeam) => ({
+      ...result.reduce<TeamItems>(
+        (acc, item) => ({
           ...acc,
           [item.id]: item,
         }),
@@ -49,17 +52,10 @@ export class GrafanaTeamStore extends BaseStore {
       ),
     };
 
-    this.searchResult = {
-      ...this.searchResult,
-      [query]: result.map((item: GrafanaTeam) => item.id),
-    };
+    this.searchResult = result.map((item: GrafanaTeam) => item.id);
   }
 
-  getSearchResult(query = '') {
-    if (!this.searchResult[query]) {
-      return [];
-    }
-
-    return this.searchResult[query].map((teamId: GrafanaTeam['id']) => this.items[teamId]);
+  getSearchResult() {
+    return this.searchResult.map((teamId: GrafanaTeam['id']) => this.items[teamId]);
   }
 }

grafana-plugin/src/models/grafana_team/grafana_team.types.ts
@@ -4,5 +4,5 @@ export interface GrafanaTeam {
   email: string;
   avatar_url: string;
   is_sharing_resources_to_all: boolean;
-  number_of_users_currently_oncall: number;
+  number_of_users_currently_oncall?: number;
 }

grafana-plugin/src/models/user/user.types.ts
@@ -43,6 +43,6 @@ export interface User {
   hidden_fields?: boolean;
   timezone: Timezone;
   working_hours: { [key: string]: [] };
-  is_currently_oncall: boolean;
-  teams: GrafanaTeam[];
+  is_currently_oncall?: boolean;
+  teams?: GrafanaTeam[];
 }

engine/apps/api/tests/test_team.py
@@ -14,15 +14,18 @@ from apps.user_management.models import Team
 GENERAL_TEAM = Team(public_primary_key="null", name="No team", email=None, avatar_url=None)
 
 
-def get_payload_from_team(team):
-    return {
+def get_payload_from_team(team, long=False):
+    payload = {
         "id": team.public_primary_key,
         "name": team.name,
         "email": team.email,
         "avatar_url": team.avatar_url,
         "is_sharing_resources_to_all": team.is_sharing_resources_to_all,
-        "number_of_users_currently_oncall": 0,
     }
+
+    if long:
+        payload.update({"number_of_users_currently_oncall": 0})
+    return payload
 
 
 @pytest.mark.django_db
@@ -40,21 +43,35 @@ def test_list_teams(
     team = make_team(organization)
     team.users.add(user)
 
+    auth_headers = make_user_auth_headers(user, token)
+
     general_team_payload = get_payload_from_team(GENERAL_TEAM)
+    general_team_long_payload = get_payload_from_team(GENERAL_TEAM, long=True)
     team_payload = get_payload_from_team(team)
+    team_long_payload = get_payload_from_team(team, long=True)
 
     client = APIClient()
     url = reverse("api-internal:team-list")
-    response = client.get(url, format="json", **make_user_auth_headers(user, token))
+    response = client.get(url, format="json", **auth_headers)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [general_team_payload, team_payload]
 
+    response = client.get(f"{url}?short=false", format="json", **auth_headers)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [general_team_long_payload, team_long_payload]
+
     url = reverse("api-internal:team-list")
-    response = client.get(f"{url}?include_no_team=false", format="json", **make_user_auth_headers(user, token))
+    response = client.get(f"{url}?include_no_team=false", format="json", **auth_headers)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [team_payload]
+
+    response = client.get(f"{url}?include_no_team=false&short=false", format="json", **auth_headers)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [team_long_payload]
 
 
 @pytest.mark.django_db
@@ -146,7 +163,7 @@ def test_teams_number_of_users_currently_oncall_attribute_works_properly(
     _make_schedule(team=team3, oncall_users=[])
 
     client = APIClient()
-    url = reverse("api-internal:team-list")
+    url = f"{reverse('api-internal:team-list')}?short=false"
 
     response = client.get(url, format="json", **make_user_auth_headers(user1, token))
 