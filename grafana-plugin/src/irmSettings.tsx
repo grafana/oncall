e034ac254e36c41f8a69a119acdfff43c5b776ae
Joey Orlando <joseph.t.orlando@gmail.com>
2023-11-22 15:30:11 -0500

    WIP

---
+++ grafana-plugin/src/irmSettings.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const IRMSettings = () => <p>IRM settings will go here</p>;
+
+export default IRMSettings;
--- grafana-plugin/src/module.ts
+++ grafana-plugin/src/module.ts
@@ -1,6 +1,7 @@
 import { ComponentClass } from 'react';
 
 import { AppPlugin, PluginExtensionPoints } from '@grafana/data';
+import IRMSettings from 'irmSettings';
 
 import MobileAppConnection from 'containers/MobileAppConnection/MobileAppConnection';
 import PluginConfigPage from 'containers/PluginConfigPage/PluginConfigPage';
@@ -20,10 +21,28 @@ if (
   PluginExtensionPoints != null &&
   'ExploreToolbarAction' in PluginExtensionPoints
 ) {
+  const IRMSettingsTabTitle = 'Grafana IRM Settings';
+  const OnCallSettingsTabTitle = 'Grafana OnCall Settings';
+
+  // TODO: change this to use the PluginExtensionPoints enum once we update the @grafana/data package
+  const extensionPointId = 'grafana/user/profile/settings';
+
   plugin.configureExtensionComponent({
-    title: 'OnCall stuff!',
+    title: IRMSettingsTabTitle,
     description: 'hello world',
-    extensionPointId: 'grafana/user/profile/settings',
+    extensionPointId,
+    /**
+     * typing IRMSettings as any until 10.2.0 is released
+     * https://github.com/grafana/grafana/pull/75019#issuecomment-1724997540
+     */
+    component: IRMSettings as any,
+  });
+
+  plugin.configureExtensionComponent({
+    title: OnCallSettingsTabTitle,
+    // TODO: what is the description needed for? I don't think we actually need it but it is a required attribute
+    description: 'hello world',
+    extensionPointId,
     /**
      * typing MobileAppConnection as any until 10.2.0 is released
      * https://github.com/grafana/grafana/pull/75019#issuecomment-1724997540
