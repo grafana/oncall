0d5ef785bfd14061043819e1be7b736e11277108
Ildar Iskhakov <ildar.iskhakov@grafana.com>
2023-11-23 11:27:47 +0800

    Make alert ingestion cache independent

--- engine/apps/integrations/mixins/ratelimit_mixin.py
+++ engine/apps/integrations/mixins/ratelimit_mixin.py
@@ -2,12 +2,14 @@ import logging
 from abc import ABC, abstractmethod
 from functools import wraps
 
+from django.conf import settings
 from django.core.cache import cache
 from django.http import HttpRequest, HttpResponse
 from django.views import View
 from ratelimit import ALL
 from ratelimit.exceptions import Ratelimited
 from ratelimit.utils import is_ratelimited
+from redis.exceptions import ConnectionError as RedisConnectionError
 
 from apps.integrations.tasks import start_notify_about_integration_ratelimit
 
@@ -54,9 +56,16 @@ def ratelimit(group=None, key=None, rate=None, method=ALL, block=False, reason=N
             request.limited = getattr(request, "limited", False)
             was_limited_before = request.limited
 
-            ratelimited = is_ratelimited(
-                request=request, group=group, fn=fn, key=key, rate=rate, method=method, increment=True
-            )
+            # Allow requests when redis cache backend fails and RATELIMIT_FAIL_OPEN setting is true
+            try:
+                ratelimited = is_ratelimited(
+                    request=request, group=group, fn=fn, key=key, rate=rate, method=method, increment=True
+                )
+            except RedisConnectionError as e:
+                if settings.RATELIMIT_FAIL_OPEN:
+                    ratelimited = False
+                else:
+                    raise e
 
             # We need to know if it's the first ratelimited request for notification purposes.
             request.is_first_rate_limited_request = getattr(request, "is_first_rate_limited_request", False)
