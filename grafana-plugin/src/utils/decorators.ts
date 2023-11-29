c0f22db773f41f4868ee487b12eef88b74218ad7
Rares Mardare <rares.mardare@grafana.com>
2023-11-29 10:44:53 +0200

    build fix

--- grafana-plugin/src/utils/decorators.ts
+++ grafana-plugin/src/utils/decorators.ts
@@ -1,28 +1,29 @@
 import { openErrorNotification, openNotification, openWarningNotification } from 'utils';
 import { LoaderStore } from 'models/loader/loader';
+
 export function AutoLoadingState(actionKey: string) {
   return function (_target: object, _key: string, descriptor: PropertyDescriptor) {
-    descriptor.value = async function (...args: any) {
     const originalFunction = descriptor.value;
+    descriptor.value = async function (...args: any) {
       LoaderStore.setLoadingAction(actionKey, true);
       try {
-      } finally {
         await originalFunction.apply(this, args);
+      } finally {
         LoaderStore.setLoadingAction(actionKey, false);
       }
-  };
     };
+  };
 }
 
 export function WrapAutoLoadingState(callback: Function, actionKey: string): (...params: any[]) => Promise<void> {
-
   return async (...params) => {
     LoaderStore.setLoadingAction(actionKey, true);
+
     try {
       await callback(...params);
+    } finally {
       LoaderStore.setLoadingAction(actionKey, false);
     }
-    } finally {
   };
 }
 
@@ -31,28 +32,28 @@ type GlobalNotificationConfig = {
   failure?: string;
   composeFailureMessageFn?: (error: unknown) => string;
   failureType?: 'error' | 'warning';
-
 };
+
 export function WithGlobalNotification({
   success,
   failure,
   composeFailureMessageFn,
-}: GlobalNotificationConfig) {
   failureType = 'error',
+}: GlobalNotificationConfig) {
   return function (_target: object, _key: string, descriptor: PropertyDescriptor) {
     const childFunction = descriptor.value;
-    descriptor.value = async function (...args: any) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    descriptor.value = async function (...args: any) {
       try {
+        const response = await childFunction.apply(this, args);
         openNotification(success);
         return response;
-        const response = await childFunction.apply(this, args);
       } catch (err) {
-        const message = composeFailureMessageFn ? composeFailureMessageFn(err) : failure;
         const open = failureType === 'error' ? openErrorNotification : openWarningNotification;
+        const message = composeFailureMessageFn ? composeFailureMessageFn(err) : failure;
         open(message);
         throw new Error(err);
-    };
       }
+    };
   };
 }
