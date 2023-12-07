c71b9b79b76ac375f541cd74dbf6176fecfa334c
Stanislav Lutsenko <istannum@gmail.com>
2023-12-07 14:49:19 +0300

    Add test_inbound_email.py [amazon_ses]

---
+++ engine/apps/email/tests/test_inbound_email.py
@@ -0,0 +1,18 @@
+import pytest, anymail
+from apps.email.inbound import InboundEmailWebhookView
+from django.test.client import RequestFactory
+
+@pytest.mark.django_db
+def test_amazon_ses_provider_load(
+    settings
+):
+    settings.INBOUND_EMAIL_ESP = "amazon_ses"
+    rf = RequestFactory()
+
+    inbound_view = InboundEmailWebhookView()
+    try:
+        inbound_view.post(rf.post('/fake-mock-location'))
+    except anymail.exceptions.AnymailAPIError:
+        # We don't test anymail, but it's invocation ability
+        result = True
+    assert result
