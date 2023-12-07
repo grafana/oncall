cca8791eb4fa9d3a3e9651af5510c30e3b3154fe
Stanislav Lutsenko <istannum@gmail.com>
2023-12-07 14:54:40 +0300

    fix result True

--- engine/apps/email/tests/test_inbound_email.py
+++ engine/apps/email/tests/test_inbound_email.py
@@ -14,6 +14,7 @@ def test_amazon_ses_provider_load(
     inbound_view = InboundEmailWebhookView()
     try:
         inbound_view.post(rf.post('/fake-mock-location'))
+        result = True
     except anymail.exceptions.AnymailAPIError:
         # We don't test anymail, but it's invocation ability
         result = True
