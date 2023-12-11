f5c636a2e9d4d2c5a7e7f1781dafd45ba258d01f
Joey Orlando <joseph.t.orlando@gmail.com>
2023-12-11 12:33:36 -0500

    fix linting error

--- engine/apps/email/tests/test_inbound_email.py
+++ engine/apps/email/tests/test_inbound_email.py
@@ -1,8 +1,8 @@
 import json
 import pytest
 from django.urls import reverse
-from rest_framework.test import APIClient
 from rest_framework import status
+from rest_framework.test import APIClient
 
 
 @pytest.mark.django_db
