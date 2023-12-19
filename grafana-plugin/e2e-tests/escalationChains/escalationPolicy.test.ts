5f25fb7b883159fb140db17907bce216a98b6c88
Joey Orlando <joseph.t.orlando@gmail.com>
2023-12-19 07:52:19 -0500

    e2e test WIP

--- grafana-plugin/e2e-tests/escalationChains/escalationPolicy.test.ts
+++ grafana-plugin/e2e-tests/escalationChains/escalationPolicy.test.ts
@@ -1,6 +1,6 @@
-import {expect, test} from "../fixtures";
-import {createEscalationChain, EscalationStep, selectEscalationStepValue} from "../utils/escalationChain";
-import {generateRandomValue} from "../utils/forms";
+import { Locator, expect, test } from '../fixtures';
+import { createEscalationChain, EscalationStep, selectEscalationStepValue } from '../utils/escalationChain';
+import { generateRandomValue, selectDropdownValue } from '../utils/forms';
 
 test('escalation policy does not go back to "Default" after adding users to notify', async ({ adminRolePage }) => {
   const { page, userName } = adminRolePage;
@@ -16,4 +16,36 @@ test('escalation policy does not go back to "Default" after adding users to noti
   await page.waitForLoadState('networkidle');
 
   expect(await page.locator('text=Important').isVisible()).toBe(true);
+});
+
+test.only('from_time and to_time for "Continue escalation if current UTC time is in" escalation step type can be properly updated', async ({
+  adminRolePage,
+}) => {
+  const FROM_TIME = '13:31';
+  const TO_TIME = '13:32';
+
+  const { page } = adminRolePage;
+  const escalationChainName = generateRandomValue();
+
+  // create escalation step w/ Continue escalation if current UTC time is in policy step
+  await createEscalationChain(page, escalationChainName, EscalationStep.ContinueEscalationIfCurrentUTCTimeIsIn);
+
+  const _getFromTimeInput = () => page.locator('[data-testid="time-range-from"] > input');
+  const _getToTimeInput = () => page.locator('[data-testid="time-range-to"] > input');
+
+  const clickAndInputValue = async (locator: Locator, value: string) => {
+    await locator.click();
+    await locator.pressSequentially(value);
+  };
+
+  // update from and to time values
+  await clickAndInputValue(_getFromTimeInput(), FROM_TIME);
+  await clickAndInputValue(_getToTimeInput(), TO_TIME);
+
+  // reload and check that these values have been persisted
+  await page.reload();
+  await page.waitForLoadState('networkidle');
+
+  expect(await _getFromTimeInput().textContent()).toBe(FROM_TIME);
+  expect(await _getToTimeInput().textContent()).toBe(FROM_TIME);
 });
