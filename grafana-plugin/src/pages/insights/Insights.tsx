960dcae608f543744e18ad45a0dbfc4d878766c3
Dominik Broj <dominik.broj@grafana.com>
2024-01-05 08:05:31 +0100

    Brojd/implement insights (#3583)
    # What this PR does
    - Use Grafana Scenes to add Insights as a separate page in OnCall
    - Add an option to run Prometheus instance via helm so that Prometheus
    Exporter feature can be used easily without the need of setting up
    Prometheus separately
    
    ## Which issue(s) this PR fixes
    https://github.com/grafana/oncall-private/issues/2382
    
    ## Checklist
    
    - [x] Unit, integration, and e2e (if applicable) tests updated
    - [x] Documentation added (or `pr:no public docs` PR label added if not
    required)
    - [x] `CHANGELOG.md` updated (or `pr:no changelog` PR label added if not
    required)

---
+++ grafana-plugin/src/pages/insights/Insights.tsx
@@ -0,0 +1,170 @@
+import React, { useMemo, useState } from 'react';
+
+import {
+  EmbeddedScene,
+  SceneTimeRange,
+  SceneFlexLayout,
+  SceneControlsSpacer,
+  SceneRefreshPicker,
+  SceneTimePicker,
+  SceneVariableSet,
+  VariableValueSelectors,
+  NestedScene,
+} from '@grafana/scenes';
+import { Alert } from '@grafana/ui';
+import { observer } from 'mobx-react';
+
+import Text from 'components/Text/Text';
+import { useStore } from 'state/useStore';
+import { DOCS_ROOT } from 'utils/consts';
+
+import { getDataSource } from './Insights.helpers';
+import { InsightsConfig } from './Insights.types';
+import getAlertGroupsByIntegrationScene from './scenes/AlertGroupsByIntegration';
+import getAlertGroupsByTeamScene from './scenes/AlertGroupsByTeam';
+import getMTTRScene from './scenes/MTTR';
+import getMTTRByIntegrationScene from './scenes/MTTRByIntegration';
+import getMTTRByTeamScene from './scenes/MTTRByTeam';
+import getMTTRChangedForPeriodStatScene from './scenes/MTTRChangedForPeriodStat';
+import getMTTRChangedForPeriodTimeseriesScene from './scenes/MTTRChangedForPeriodTimeseries';
+import getNewAlertGroupsDuringTimePeriodScene from './scenes/NewAlertGroupsDuringTimePeriod';
+import getNewAlertGroupsForSelectedPeriodScene from './scenes/NewAlertGroupsForSelectedPeriod';
+import getNewAlertGroupsNotificationsDuringTimePeriodScene from './scenes/NewAlertGroupsNotificationsDuringTimePeriod';
+import getNewAlertGroupsNotificationsForPeriodTableScene from './scenes/NewAlertGroupsNotificationsForPeriodTable';
+import getNewAlertGroupsNotificationsInTotalScene from './scenes/NewAlertGroupsNotificationsInTotal';
+import getTotalAlertGroupsScene from './scenes/TotalAlertGroups';
+import getTotalAlertGroupsByStateScene from './scenes/TotalAlertGroupsByState';
+import getVariables from './variables';
+
+const Insights = observer(() => {
+  const { isOpenSource } = useStore();
+  const [alertVisible, setAlertVisible] = useState(true);
+
+  const rootScene = useMemo(
+    () => getRootScene({ isOpenSource, datasource: getDataSource(isOpenSource) }),
+    [isOpenSource]
+  );
+
+  return (
+    <>
+      {isOpenSource && alertVisible && (
+        <Alert onRemove={() => setAlertVisible(false)} severity="info" title="">
+          {
+            <>
+              In order to see insights you need to set up Prometheus, add it to your Grafana instance as a data source,
+              set FEATURE_PROMETHEUS_EXPORTER_ENABLED environment variable to true and then select your Data source in
+              the dropdown below.
+              <br />
+              <br />
+              <>
+                You can find out more in
+                <a
+                  href={`${DOCS_ROOT}/insights-and-metrics/#for-open-source-customers`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Text type="link"> documentation</Text>
+                </a>
+                .
+              </>
+            </>
+          }
+        </Alert>
+      )}
+      <rootScene.Component model={rootScene} />
+    </>
+  );
+});
+
+const getRootScene = (config: InsightsConfig) =>
+  new EmbeddedScene({
+    $timeRange: new SceneTimeRange({ from: 'now-7d', to: 'now' }),
+    $variables: new SceneVariableSet({
+      variables: getVariables(config),
+    }),
+    controls: [
+      new VariableValueSelectors({}),
+      new SceneControlsSpacer(),
+      new SceneTimePicker({}),
+      new SceneRefreshPicker({}),
+    ],
+    body: new SceneFlexLayout({
+      direction: 'column',
+      children: [
+        new NestedScene({
+          title: 'Overview',
+          canCollapse: true,
+          isCollapsed: false,
+          body: new SceneFlexLayout({
+            direction: 'column',
+            children: [
+              new SceneFlexLayout({
+                height: 200,
+                children: [
+                  getTotalAlertGroupsScene(config),
+                  getTotalAlertGroupsByStateScene(config),
+                  getNewAlertGroupsForSelectedPeriodScene(config),
+                  getMTTRScene(config),
+                  getMTTRChangedForPeriodStatScene(config),
+                ],
+              }),
+              new SceneFlexLayout({
+                height: 400,
+                children: [getNewAlertGroupsDuringTimePeriodScene(config)],
+              }),
+              new SceneFlexLayout({
+                height: 400,
+                children: [getMTTRChangedForPeriodTimeseriesScene(config)],
+              }),
+            ],
+          }),
+        }),
+        new NestedScene({
+          title: 'Integrations data',
+          canCollapse: true,
+          isCollapsed: false,
+          body: new SceneFlexLayout({
+            height: 400,
+            children: [getAlertGroupsByIntegrationScene(config), getMTTRByIntegrationScene(config)],
+          }),
+        }),
+        new NestedScene({
+          title: 'Notified alert groups by Users (based on all Integrations)',
+          canCollapse: true,
+          isCollapsed: false,
+          body: new SceneFlexLayout({
+            direction: 'column',
+            children: [
+              new SceneFlexLayout({
+                height: 400,
+                children: [getNewAlertGroupsNotificationsDuringTimePeriodScene(config)],
+              }),
+              new SceneFlexLayout({
+                height: 400,
+                children: [
+                  getNewAlertGroupsNotificationsInTotalScene(config),
+                  getNewAlertGroupsNotificationsForPeriodTableScene(config),
+                ],
+              }),
+            ],
+          }),
+        }),
+        new NestedScene({
+          title: 'Teams data',
+          canCollapse: true,
+          isCollapsed: false,
+          body: new SceneFlexLayout({
+            direction: 'column',
+            children: [
+              new SceneFlexLayout({
+                height: 400,
+                children: [getAlertGroupsByTeamScene(config), getMTTRByTeamScene(config)],
+              }),
+            ],
+          }),
+        }),
+      ],
+    }),
+  });
+
+export default Insights;
---
+++ grafana-plugin/src/pages/insights/scenes/TotalAlertGroupsByState.tsx
@@ -0,0 +1,133 @@
+import { ThresholdsMode } from '@grafana/data';
+import { SceneDataTransformer, SceneFlexItem, SceneQueryRunner, VizPanel } from '@grafana/scenes';
+
+import { InsightsConfig } from 'pages/insights/Insights.types';
+
+export default function getTotalAlertGroupsByStateScene({ datasource }: InsightsConfig) {
+  const query = new SceneQueryRunner({
+    datasource,
+    queries: [
+      {
+        disableTextWrap: false,
+        editorMode: 'code',
+        excludeNullMetadata: false,
+        expr: 'sum by (state) (avg without(pod, instance) ($alert_groups_total{slug=~"$instance", team=~"$team", integration=~"$integration"}))',
+        fullMetaSearch: false,
+        legendFormat: '__auto',
+        range: true,
+        refId: 'A',
+        useBackend: false,
+      },
+    ],
+  });
+
+  const transformedData = new SceneDataTransformer({
+    $data: query,
+    transformations: [
+      {
+        id: 'joinByLabels',
+        options: {
+          value: 'state',
+        },
+      },
+      {
+        id: 'organize',
+        options: {
+          excludeByName: {},
+          indexByName: {
+            acknowledged: 1,
+            firing: 0,
+            resolved: 2,
+            silenced: 3,
+          },
+          renameByName: {},
+        },
+      },
+    ],
+  });
+
+  return new SceneFlexItem({
+    $data: transformedData,
+    body: new VizPanel({
+      title: 'Total alert groups by state',
+      pluginId: 'bargauge',
+      fieldConfig: {
+        defaults: {
+          color: {
+            mode: 'thresholds',
+          },
+          mappings: [],
+          thresholds: {
+            mode: ThresholdsMode.Absolute,
+            steps: [
+              {
+                color: 'green',
+                value: null,
+              },
+            ],
+          },
+        },
+        overrides: [
+          {
+            matcher: {
+              id: 'byName',
+              options: 'firing',
+            },
+            properties: [
+              {
+                id: 'color',
+                value: {
+                  fixedColor: 'red',
+                  mode: 'fixed',
+                },
+              },
+            ],
+          },
+          {
+            matcher: {
+              id: 'byName',
+              options: 'acknowledged',
+            },
+            properties: [
+              {
+                id: 'color',
+                value: {
+                  fixedColor: 'dark-yellow',
+                  mode: 'fixed',
+                },
+              },
+            ],
+          },
+          {
+            matcher: {
+              id: 'byName',
+              options: 'silenced',
+            },
+            properties: [
+              {
+                id: 'color',
+                value: {
+                  mode: 'fixed',
+                },
+              },
+            ],
+          },
+        ],
+      },
+      options: {
+        displayMode: 'gradient',
+        minVizHeight: 10,
+        minVizWidth: 0,
+        orientation: 'vertical',
+        reduceOptions: {
+          calcs: ['lastNotNull'],
+          fields: '',
+          values: false,
+        },
+        showUnfilled: true,
+        valueMode: 'color',
+      },
+      pluginVersion: '9.5.2',
+    }),
+  });
+}
