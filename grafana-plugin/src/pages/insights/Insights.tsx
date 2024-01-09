c87fb43572643b2cf5a8944b30cf92c44303025a
Dominik <dominik.broj@grafana.com>
2024-01-09 16:16:47 +0100

    apply default stack in insights, other improvements

--- grafana-plugin/src/pages/insights/Insights.tsx
+++ grafana-plugin/src/pages/insights/Insights.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import {
   EmbeddedScene,
@@ -39,23 +39,46 @@ import getTotalAlertGroupsScene from './scenes/TotalAlertGroups';
 import getTotalAlertGroupsByStateScene from './scenes/TotalAlertGroupsByState';
 import getVariables from './variables';
 
+const getDefaultStackValue = (isOpenSource: boolean) =>
+  isOpenSource ? 'self_hosted_stack' : location.host.split('.')[0];
+
 const Insights = observer(() => {
   const { isOpenSource, insightsDatasource } = useStore();
+  const [showAllStackInfo, setShowAllStackInfo] = useState(false);
 
-  const datasource = { uid: isOpenSource ? '$datasource' : insightsDatasource };
-  const appScene = useSceneApp(() => getAppScene({ isOpenSource, datasource }));
+  const config = useMemo(
+    () => ({
+      isOpenSource,
+      datasource: { uid: isOpenSource ? '$datasource' : insightsDatasource },
+      stack: getDefaultStackValue(isOpenSource),
+    }),
+    []
+  );
+
+  const variables = useMemo(() => getVariables(config), [config]);
+
+  const getAppScene = useCallback(() => getRootScene(config, variables), [config, variables]);
+
+  const appScene = useSceneApp(getAppScene);
+
+  useEffect(() => {
+    const listener = variables.stack.subscribeToState(({ text }) => {
+      setShowAllStackInfo((text as string[]).includes('All'));
+    });
+    return listener.unsubscribe;
+  }, []);
 
   return (
     <div className={styles.insights}>
-      <InsightsInfoAlert />
+      <InsightsGeneralInfo />
+      {showAllStackInfo && <AllStacksSelectedWarning />}
       <appScene.Component model={appScene} />
     </div>
   );
 });
 
-const InsightsInfoAlert = observer(() => {
+const InsightsGeneralInfo = observer(() => {
   const { isOpenSource } = useStore();
-  const [alertVisible, setAlertVisible] = useState(true);
 
   const docsLink = (
     <a
@@ -69,124 +92,126 @@ const InsightsInfoAlert = observer(() => {
 
   const content = isOpenSource ? (
     <>
-      In order to see insights you need to set up Prometheus, add it to your Grafana instance as a data source, set
+      In order to see insights you need to set up Prometheus, add it to your Grafana stack as a data source, set
       FEATURE_PROMETHEUS_EXPORTER_ENABLED environment variable to true and then select your Data source in the dropdown
-      below.
-      <br />
-      <br />
-      <>You can find out more in our {docsLink}.</>
+      below. You can find out more in our {docsLink}.
     </>
   ) : (
     <>Find out more about OnCall Insights and Metrics in our {docsLink}.</>
   );
 
-  return alertVisible ? (
-    <Alert onRemove={() => setAlertVisible(false)} severity="info" title="">
-      {content}
-    </Alert>
-  ) : null;
+  return <Text type="secondary">{content}</Text>;
 });
 
-const getAppScene = (config: InsightsConfig) =>
+const AllStacksSelectedWarning = () => {
+  const [alertVisible, setAlertVisible] = useState(true);
+
+  return alertVisible ? (
+    <Alert onRemove={() => setAlertVisible(false)} severity="warning" title="" className={styles.alertBox}>
+      Please be aware that retrieving insights from multiple stacks has performance impact and loading data might take
+      significantly more time. We recommend to select only required stacks if possible.
+    </Alert>
+  ) : null;
+};
+
+const getRootScene = (config: InsightsConfig, variables: ReturnType<typeof getVariables>) =>
   new SceneApp({
     pages: [
       new SceneAppPage({
         title: 'OnCall Insights',
         url: '/a/grafana-oncall-app/insights',
-        getScene: () => getRootScene(config),
+        getScene: () =>
+          new EmbeddedScene({
+            $timeRange: new SceneTimeRange({ from: 'now-7d', to: 'now' }),
+            $variables: new SceneVariableSet({
+              variables: Object.values(variables),
+            }),
+            controls: [
+              new VariableValueSelectors({}),
+              new SceneControlsSpacer(),
+              new SceneTimePicker({}),
+              new SceneRefreshPicker({}),
+            ],
+            body: new SceneFlexLayout({
+              direction: 'column',
+              children: [
+                new NestedScene({
+                  title: 'Overview',
+                  canCollapse: true,
+                  isCollapsed: false,
+                  body: new SceneFlexLayout({
+                    direction: 'column',
+                    children: [
+                      new SceneFlexLayout({
+                        height: 200,
+                        children: [
+                          getTotalAlertGroupsScene(config),
+                          getTotalAlertGroupsByStateScene(config),
+                          getNewAlertGroupsForSelectedPeriodScene(config),
+                          getMTTRScene(config),
+                          getMTTRChangedForPeriodStatScene(config),
+                        ],
+                      }),
+                      new SceneFlexLayout({
+                        height: 400,
+                        children: [getNewAlertGroupsDuringTimePeriodScene(config)],
+                      }),
+                      new SceneFlexLayout({
+                        height: 400,
+                        children: [getMTTRChangedForPeriodTimeseriesScene(config)],
+                      }),
+                    ],
+                  }),
+                }),
+                new NestedScene({
+                  title: 'Integrations data',
+                  canCollapse: true,
+                  isCollapsed: false,
+                  body: new SceneFlexLayout({
+                    height: 400,
+                    children: [getAlertGroupsByIntegrationScene(config), getMTTRByIntegrationScene(config)],
+                  }),
+                }),
+                new NestedScene({
+                  title: 'Notified alert groups by Users (based on all Integrations)',
+                  canCollapse: true,
+                  isCollapsed: false,
+                  body: new SceneFlexLayout({
+                    direction: 'column',
+                    children: [
+                      new SceneFlexLayout({
+                        height: 400,
+                        children: [getNewAlertGroupsNotificationsDuringTimePeriodScene(config)],
+                      }),
+                      new SceneFlexLayout({
+                        height: 400,
+                        children: [
+                          getNewAlertGroupsNotificationsInTotalScene(config),
+                          getNewAlertGroupsNotificationsForPeriodTableScene(config),
+                        ],
+                      }),
+                    ],
+                  }),
+                }),
+                new NestedScene({
+                  title: 'Teams data',
+                  canCollapse: true,
+                  isCollapsed: false,
+                  body: new SceneFlexLayout({
+                    direction: 'column',
+                    children: [
+                      new SceneFlexLayout({
+                        height: 400,
+                        children: [getAlertGroupsByTeamScene(config), getMTTRByTeamScene(config)],
+                      }),
+                    ],
+                  }),
+                }),
+              ],
+            }),
+          }),
       }),
     ],
-  });
-
-const getRootScene = (config: InsightsConfig) =>
-  new EmbeddedScene({
-    $timeRange: new SceneTimeRange({ from: 'now-7d', to: 'now' }),
-    $variables: new SceneVariableSet({
-      variables: getVariables(config),
-    }),
-    controls: [
-      new VariableValueSelectors({}),
-      new SceneControlsSpacer(),
-      new SceneTimePicker({}),
-      new SceneRefreshPicker({}),
-    ],
-    body: new SceneFlexLayout({
-      direction: 'column',
-      children: [
-        new NestedScene({
-          title: 'Overview',
-          canCollapse: true,
-          isCollapsed: false,
-          body: new SceneFlexLayout({
-            direction: 'column',
-            children: [
-              new SceneFlexLayout({
-                height: 200,
-                children: [
-                  getTotalAlertGroupsScene(config),
-                  getTotalAlertGroupsByStateScene(config),
-                  getNewAlertGroupsForSelectedPeriodScene(config),
-                  getMTTRScene(config),
-                  getMTTRChangedForPeriodStatScene(config),
-                ],
-              }),
-              new SceneFlexLayout({
-                height: 400,
-                children: [getNewAlertGroupsDuringTimePeriodScene(config)],
-              }),
-              new SceneFlexLayout({
-                height: 400,
-                children: [getMTTRChangedForPeriodTimeseriesScene(config)],
-              }),
-            ],
-          }),
-        }),
-        new NestedScene({
-          title: 'Integrations data',
-          canCollapse: true,
-          isCollapsed: false,
-          body: new SceneFlexLayout({
-            height: 400,
-            children: [getAlertGroupsByIntegrationScene(config), getMTTRByIntegrationScene(config)],
-          }),
-        }),
-        new NestedScene({
-          title: 'Notified alert groups by Users (based on all Integrations)',
-          canCollapse: true,
-          isCollapsed: false,
-          body: new SceneFlexLayout({
-            direction: 'column',
-            children: [
-              new SceneFlexLayout({
-                height: 400,
-                children: [getNewAlertGroupsNotificationsDuringTimePeriodScene(config)],
-              }),
-              new SceneFlexLayout({
-                height: 400,
-                children: [
-                  getNewAlertGroupsNotificationsInTotalScene(config),
-                  getNewAlertGroupsNotificationsForPeriodTableScene(config),
-                ],
-              }),
-            ],
-          }),
-        }),
-        new NestedScene({
-          title: 'Teams data',
-          canCollapse: true,
-          isCollapsed: false,
-          body: new SceneFlexLayout({
-            direction: 'column',
-            children: [
-              new SceneFlexLayout({
-                height: 400,
-                children: [getAlertGroupsByTeamScene(config), getMTTRByTeamScene(config)],
-              }),
-            ],
-          }),
-        }),
-      ],
-    }),
   });
 
 export default Insights;
