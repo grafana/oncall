648f375f5997741d46a9d2682626302b99acd688
Joey Orlando <joseph.t.orlando@gmail.com>
2024-01-12 15:00:44 -0500

    add test

--- engine/common/custom_celery_tasks/tests/test_log_exception_on_failure_task.py
+++ engine/common/custom_celery_tasks/tests/test_log_exception_on_failure_task.py
@@ -11,16 +11,15 @@ from common.custom_celery_tasks.log_exception_on_failure_task import (
 
 EXCEPTION_MSG = "my exception"
 
-STACK_TRACE = f"""
-An exception occured while executing a celery task
-Traceback (most recent call last):
-  File "/usr/local/lib/python3.11/site-packages/celery/app/trace.py", line 477, in trace_task
-    R = retval = fun(*args, **kwargs)
-                 ^^^^^^^^^^^^^^^^^^^^
-  File "/etc/app/common/custom_celery_tasks/tests/test_log_exception_on_failure_task.py", line 28, in my_task
-    raise ValueError(EXCEPTION_MSG)
-Exception: {EXCEPTION_MSG}
-"""
+
+def _generate_stack_trace(line_number, task_name):
+    return (
+        "An exception occured while executing a celery task\nTraceback (most recent call last):\n  "
+        'File "/usr/local/lib/python3.11/site-packages/celery/app/trace.py", line 477, in trace_task\n    '
+        "R = retval = fun(*args, **kwargs)\n                 ^^^^^^^^^^^^^^^^^^^^\n  "
+        f'File "/etc/app/common/custom_celery_tasks/tests/test_log_exception_on_failure_task.py", line {line_number}'
+        f", in {task_name}\n    raise ValueError(EXCEPTION_MSG)\nValueError: {EXCEPTION_MSG}"
+    )
 
 
 def test_log_exception_on_failure_task_logs_stack_trace_on_task_failure(caplog):
@@ -34,10 +33,10 @@ def test_log_exception_on_failure_task_logs_stack_trace_on_task_failure(caplog):
 
     with pytest.raises(ValueError):
         my_task.apply().get()
-        assert STACK_TRACE in caplog.text
+    assert _generate_stack_trace("28", "my_task") in caplog.text
 
     caplog.clear()
 
     with pytest.raises(ValueError):
         my_task_two.apply().get()
-        assert STACK_TRACE in caplog.text
+    assert _generate_stack_trace("32", "my_task_two") in caplog.text
