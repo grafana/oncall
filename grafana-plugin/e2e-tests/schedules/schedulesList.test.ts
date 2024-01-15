d6b7c5d022151ead7fed911f02090dcf095d081a
Dominik <dominik.broj@grafana.com>
2024-01-15 11:11:51 +0100

    add e2e test for schedules

---
+++ grafana-plugin/e2e-tests/schedules/schedulesList.test.ts
@@ -0,0 +1,34 @@
+import { expect, test } from '../fixtures';
+import { generateRandomValue } from '../utils/forms';
+import { goToOnCallPage } from '../utils/navigation';
+import { createOnCallSchedule } from '../utils/schedule';
+
+test('schedule calendar and list of schedules is correctly displayed', async ({ adminRolePage }) => {
+  const { page, userName } = adminRolePage;
+
+  const onCallScheduleName = generateRandomValue();
+  await createOnCallSchedule(page, onCallScheduleName, userName);
+
+  await goToOnCallPage(page, 'schedules');
+
+  // schedule slots are present in calendar
+  const nbOfSlotsInCalendar = await page.getByTestId('schedule-slot').count();
+  await expect(nbOfSlotsInCalendar).toBeGreaterThan(0);
+
+  // filter table to show only created schedule
+  await page
+    .locator('div')
+    .filter({ hasText: /^Search or filter results\.\.\.$/ })
+    .nth(1)
+    .click();
+  await page.keyboard.insertText(onCallScheduleName);
+  await page.keyboard.press('Enter');
+  await page.waitForLoadState('networkidle');
+
+  // schedules table displays created schedule
+  const schedulesTable = page.getByTestId('schedules-table');
+  await expect(schedulesTable.getByRole('cell', { name: onCallScheduleName })).toBeVisible();
+  await expect(schedulesTable.getByRole('cell', { name: 'Web' })).toBeVisible();
+  await expect(schedulesTable.getByRole('cell', { name: userName })).toBeVisible();
+  await expect(schedulesTable.getByRole('cell', { name: 'No team' })).toBeVisible();
+});
