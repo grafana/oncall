e63a7896d9fc6bc7a983947898e6a492060e9ef8
Dominik <dominik.broj@grafana.com>
2024-01-16 13:56:39 +0100

    initial test

---
+++ grafana-plugin/e2e-tests/outgoingWebhooks/createSimpleWebhook.test.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '../fixtures';
+import { clickButton, generateRandomValue } from '../utils/forms';
+import { goToOnCallPage } from '../utils/navigation';
+import { createOnCallSchedule } from '../utils/schedule';
+
+test('create simple webhook and check it is displayed on the list correctly', async ({ adminRolePage: { page } }) => {
+  const webhookName = generateRandomValue();
+  const webhookUrl = 'https://example.com';
+  await goToOnCallPage(page, 'outgoing_webhooks');
+
+  await clickButton({ page, buttonText: 'New Outgoing Webhook' });
+
+  await page.getByText('Simple').first().click();
+
+  await page.waitForTimeout(2000);
+
+  await page.keyboard.insertText(webhookUrl);
+  await page.locator('[name=name]').fill(webhookName);
+  await page.getByLabel('New Outgoing Webhook').getByRole('img').nth(1).click(); // Open team dropdown
+  await page.getByText('No team', { exact: true }).click();
+  await clickButton({ page, buttonText: 'Create Webhook' });
+
+  // const webhookName = generateRandomValue();
+});
