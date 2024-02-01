60d5fe8be04a07aa358c598c5cf76b91f2f88bfb
Joey Orlando <joseph.t.orlando@gmail.com>
2024-02-01 16:27:35 -0500

    remove apps.py

--- engine/apps/metrics_exporter/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class MetricsExporterConfig(AppConfig):
-    name = "apps.metrics_exporter"
-
-    def ready(self):
-        from . import signals  # noqa: F401
