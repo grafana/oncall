3d793d9a4c7e8dedc91162ce50ded3c1a7256f15
Julia <ferril.darkdiver@gmail.com>
2024-02-02 13:08:30 +0100

    Add test

--- engine/apps/telegram/tests/test_tasks.py
+++ engine/apps/telegram/tests/test_tasks.py
@@ -3,9 +3,14 @@ from unittest.mock import patch
 import pytest
 from telegram import error
 
+from apps.base.models import UserNotificationPolicy
 from apps.telegram.client import TelegramClient
-from apps.telegram.models import TelegramMessage
-from apps.telegram.tasks import send_log_and_actions_message
+from apps.telegram.exceptions import AlertGroupTelegramMessageDoesNotExist
+from apps.telegram.models import TelegramMessage, TelegramToUserConnector
+from apps.telegram.tasks import (
+    send_link_to_channel_message_or_fallback_to_full_alert_group,
+    send_log_and_actions_message,
+)
 
 
 @patch.object(TelegramClient, "send_raw_message", side_effect=error.BadRequest("Message to reply not found"))
@@ -45,3 +50,43 @@ def test_send_log_and_actions_replied_message_not_found(
         f"due to 'Message to reply not found'. alert_group {alert_group.pk}"
     )
     assert expected_msg in caplog.text
+
+
+@patch.object(TelegramToUserConnector, "send_link_to_channel_message")
+@pytest.mark.django_db
+def test_send_link_to_channel_message_or_fallback_to_full_alert_group_message_does_not_exist(
+    mock_send_link_to_channel_message,
+    make_organization_and_user,
+    make_telegram_user_connector,
+    make_user_notification_policy,
+    make_alert_receive_channel,
+    make_alert_group,
+):
+    organization, user = make_organization_and_user()
+    user_connector = make_telegram_user_connector(user)
+    user_notification_policy = make_user_notification_policy(
+        user=user,
+        step=UserNotificationPolicy.Step.NOTIFY,
+        notify_by=UserNotificationPolicy.NotificationChannel.TELEGRAM,
+    )
+    alert_receive_channel = make_alert_receive_channel(organization)
+    alert_group = make_alert_group(alert_receive_channel)
+
+    mock_send_link_to_channel_message.side_effect = AlertGroupTelegramMessageDoesNotExist(
+        f"No alert group message found, probably it is not saved to database yet, alert group: {alert_group.id}"
+    )
+
+    with patch.object(send_link_to_channel_message_or_fallback_to_full_alert_group, "apply_async") as mocked_task_call:
+        send_link_to_channel_message_or_fallback_to_full_alert_group(
+            alert_group.id, user_notification_policy.id, user_connector.id
+        )
+    # No exception raised, task restarted with `is_first_message_check=False`
+    mocked_task_call.assert_called_once_with(
+        (alert_group.id, user_notification_policy.id, user_connector.id, False), countdown=3
+    )
+    # if task started with `is_first_message_check=False` and alert group telegram message was not found,
+    # exception will be raised
+    with pytest.raises(AlertGroupTelegramMessageDoesNotExist):
+        send_link_to_channel_message_or_fallback_to_full_alert_group(
+            alert_group.id, user_notification_policy.id, user_connector.id, False
+        )
