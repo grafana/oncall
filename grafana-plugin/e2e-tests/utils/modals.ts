482f764d47d17a2ae6b5cadd2be9c570333f793e
Joey Orlando <joseph.t.orlando@gmail.com>
2024-02-24 07:09:51 +0000

    debugging modal selector

--- grafana-plugin/e2e-tests/utils/modals.ts
+++ grafana-plugin/e2e-tests/utils/modals.ts
@@ -3,6 +3,8 @@ import { Page } from '@playwright/test';
 /**
  * in Grafana v9 the aria-label is "Close dialog"
  * in Grafana v10+ the aria-label is "Close dialogue"
+ *
+ * https://playwright.dev/docs/other-locators#css-elements-matching-one-of-the-conditions
  */
 export const closeModal = async (page: Page): Promise<void> =>
-  (await page.waitForSelector('button[aria-label*="Close dialog"]')).click();
+  (await page.waitForSelector('button[aria-label="Close dialog"], button[aria-label="Close dialogue"]')).click();
