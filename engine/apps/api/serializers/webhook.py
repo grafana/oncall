0453fb81453a8bcee1f85676c020d9ec47ced017
Matias Bordese <mbordese@gmail.com>
2024-03-05 10:04:02 -0300

    Update unset preset values check

--- engine/apps/api/serializers/webhook.py
+++ engine/apps/api/serializers/webhook.py
@@ -189,13 +189,13 @@ class WebhookSerializer(LabelsSerializerMixin, serializers.ModelSerializer):
             for controlled_field in preset_metadata.controlled_fields:
                 if controlled_field in self.initial_data:
                     if self.instance:
-                        if self.initial_data[controlled_field] is not None and self.initial_data[
-                            controlled_field
-                        ] != getattr(self.instance, controlled_field):
+                        if bool(self.initial_data[controlled_field]) and self.initial_data[controlled_field] != getattr(
+                            self.instance, controlled_field
+                        ):
                             raise serializers.ValidationError(
                                 detail=f"{controlled_field} is controlled by preset, cannot update"
                             )
-                    elif self.initial_data[controlled_field] is not None:
+                    elif bool(self.initial_data[controlled_field]):
                         raise serializers.ValidationError(
                             detail=f"{controlled_field} is controlled by preset, cannot create"
                         )
