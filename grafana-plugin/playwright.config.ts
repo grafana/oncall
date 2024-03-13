531cf44d9452a9800cc486fd4b83d59c1d3884d1
Dominik <dominik.broj@grafana.com>
2024-03-13 08:16:46 +0100

    update

--- grafana-plugin/playwright.config.ts
+++ grafana-plugin/playwright.config.ts
@@ -30,7 +30,10 @@ export default defineConfig({
   /* Maximum time all the tests can run for. */
   globalTimeout: 20 * 60 * 1_000, // 20 minutes
 
-  reporter: REPORTER,
+  reporter: [
+    ['html', { open: 'never' }],
+    ['json', { outputFile: 'results.json' }],
+  ],
 
   /* Maximum time one test can run for. */
   timeout: 60_000,
