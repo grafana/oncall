91c13e2a1e235316c73b99d0e11ee280b0dbc10c
Dominik <dominik.broj@grafana.com>
2024-03-19 15:14:58 +0100

    update

--- grafana-plugin/e2e-tests/alerts/sms.test.ts
+++ grafana-plugin/e2e-tests/alerts/sms.test.ts
@@ -2,18 +2,19 @@ import { test, expect } from '../fixtures';
 import { createEscalationChain, EscalationStep } from '../utils/escalationChain';
 import { generateRandomValue } from '../utils/forms';
 import { createIntegrationAndSendDemoAlert } from '../utils/integrations';
-// import { goToOnCallPage } from '../utils/navigation';
+import { goToOnCallPage } from '../utils/navigation';
 import { waitForSms } from '../utils/phone';
 import { configureUserNotificationSettings, verifyUserPhoneNumber } from '../utils/userSettings';
 
 test('we can verify our phone number + receive an SMS alert @expensive', async ({ adminRolePage }) => {
+  test.setTimeout(90_000);
   const { page, userName } = adminRolePage;
   const escalationChainName = generateRandomValue();
   const integrationName = generateRandomValue();
 
-  // await goToOnCallPage(page, 'settings');
-  // await page.getByText('Env Variables').click();
-  // await page.waitForTimeout(5_000);
+  await goToOnCallPage(page, 'settings');
+  await page.getByText('Env Variables').click();
+  await page.waitForTimeout(5_000);
   // await page.getByText('FAAAAAKE').click(); // make it fail to produce test report
 
   await verifyUserPhoneNumber(page);
