339f34add69f6314c90338e8424a1e1a3a6eff86
Dominik <dominik.broj@grafana.com>
2024-03-19 14:08:09 +0100

    update

--- grafana-plugin/e2e-tests/alerts/sms.test.ts
+++ grafana-plugin/e2e-tests/alerts/sms.test.ts
@@ -2,7 +2,6 @@ import { test, expect } from '../fixtures';
 import { createEscalationChain, EscalationStep } from '../utils/escalationChain';
 import { generateRandomValue } from '../utils/forms';
 import { createIntegrationAndSendDemoAlert } from '../utils/integrations';
-import { goToOnCallPage } from '../utils/navigation';
 import { waitForSms } from '../utils/phone';
 import { configureUserNotificationSettings, verifyUserPhoneNumber } from '../utils/userSettings';
 
@@ -11,20 +10,15 @@ test('we can verify our phone number + receive an SMS alert @expensive', async (
   const escalationChainName = generateRandomValue();
   const integrationName = generateRandomValue();
 
-  await goToOnCallPage(page, 'settings');
-  await page.getByText('Env Variables').click();
-  await page.waitForTimeout(5_000);
-  await page.getByText('FAAAAAKE').click(); // make it fail to produce test report
+  await verifyUserPhoneNumber(page);
+  await configureUserNotificationSettings(page, 'SMS');
 
-  // await verifyUserPhoneNumber(page);
-  // await configureUserNotificationSettings(page, 'SMS');
+  await createEscalationChain(page, escalationChainName, EscalationStep.NotifyUsers, userName);
+  await createIntegrationAndSendDemoAlert(page, integrationName, escalationChainName);
 
-  // await createEscalationChain(page, escalationChainName, EscalationStep.NotifyUsers, userName);
-  // await createIntegrationAndSendDemoAlert(page, integrationName, escalationChainName);
+  // wait for the SMS alert notification to arrive
+  const smsAlertNotification = await waitForSms();
 
-  // // wait for the SMS alert notification to arrive
-  // const smsAlertNotification = await waitForSms();
-
-  // expect(smsAlertNotification).toContain('OnCall');
-  // expect(smsAlertNotification).toContain('alert');
+  expect(smsAlertNotification).toContain('OnCall');
+  expect(smsAlertNotification).toContain('alert');
 });
