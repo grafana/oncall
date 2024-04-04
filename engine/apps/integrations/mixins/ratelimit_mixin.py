grafana/oncall

commit d14b1c8e28a65023b8b49a68aa9d30031e35bd36

Improve performance for rate-limited, banned and deleted integrations (#4137)
# What this PR does
- Remove BanAlertConsumptionBasedOnSettingsMiddleware under high traffic
scenarios this causes too much DB load and the same effect can be
achieved by soft-deleting the integration
- Change lookups for integrations so that non-existent and deleted
integrations are also cached to reduce DB load

## Which issue(s) this PR closes

Closes https://github.com/grafana/oncall-private/issues/2608

<!--
*Note*: if you have more than one GitHub issue that this PR closes, be
sure to preface
each issue link with a [closing
keyword](https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/using-keywords-in-issues-and-pull-requests#linking-a-pull-request-to-an-issue).
This ensures that the issue(s) are auto-closed once the PR has been
merged.
-->

## Checklist

- [x] Unit, integration, and e2e (if applicable) tests updated
- [x] Documentation added (or `pr:no public docs` PR label added if not
required)
- [x] Added the relevant release notes label (see labels prefixed w/
`release:`). These labels dictate how your PR will
    show up in the autogenerated release notes.

engine/apps/integrations/mixins/ratelimit_mixin.py
@@ -18,6 +18,9 @@ RATELIMIT_INTEGRATION = "300/5m"
 RATELIMIT_TEAM = "900/5m"
 RATELIMIT_REASON_INTEGRATION = "channel"
 RATELIMIT_REASON_TEAM = "team"
+INTEGRATION_TOKEN_TO_IGNORE_KEY = "integration_tokens_to_ignore_ratelimit"
+RATELIMIT_BYPASS_CACHE_KEY = f"{INTEGRATION_TOKEN_TO_IGNORE_KEY}_cache_key"
+RATELIMIT_BYPASS_CACHE_TIMEOUT = 5
 
 
 def get_rate_limit_per_channel_key(_, request):
@@ -85,17 +88,21 @@ def ratelimit(group=None, key=None, rate=None, method=ALL, block=False, reason=N
 
 
 def is_ratelimit_ignored(alert_receive_channel):
-    from apps.base.models import DynamicSetting
+    integration_tokens_to_ignore_ratelimit = cache.get(RATELIMIT_BYPASS_CACHE_KEY)
+    if not integration_tokens_to_ignore_ratelimit:
+        from apps.base.models import DynamicSetting
 
-    integration_token_to_ignore_ratelimit = DynamicSetting.objects.get_or_create(
-        name="integration_tokens_to_ignore_ratelimit",
-        defaults={
-            "json_value": [
-                "dummytoken_uniq_1213kj1h3",
-            ]
-        },
-    )[0]
-    return alert_receive_channel.token in integration_token_to_ignore_ratelimit.json_value
+        dynamic_setting = DynamicSetting.objects.get_or_create(
+            name=INTEGRATION_TOKEN_TO_IGNORE_KEY,
+            defaults={
+                "json_value": [
+                    "dummytoken_uniq_1213kj1h3",
+                ]
+            },
+        )[0]
+        integration_tokens_to_ignore_ratelimit = dynamic_setting.json_value[0].json_value
+        cache.set(RATELIMIT_BYPASS_CACHE_KEY, integration_tokens_to_ignore_ratelimit, RATELIMIT_BYPASS_CACHE_TIMEOUT)
+    return alert_receive_channel.token in integration_tokens_to_ignore_ratelimit
 
 
 class RateLimitMixin(ABC, View):

engine/apps/integrations/mixins/alert_channel_defining_mixin.py
@@ -8,6 +8,10 @@ from django.db import OperationalError
 
 from apps.user_management.exceptions import OrganizationMovedException
 
+INTEGRATION_PERMISSION_DENIED_MESSAGE = "Integration key was not found. Permission denied."
+
+CHANNEL_DOES_NOT_EXIST_PLACEHOLDER = "DOES_NOT_EXIST"
+
 logger = logging.getLogger(__name__)
 
 
@@ -34,6 +38,10 @@ class AlertChannelDefiningMixin(object):
             # Trying to define from short-term cache
             cache_key_short_term = self.CACHE_KEY_SHORT_TERM + "_" + str(kwargs["alert_channel_key"])
             cached_alert_receive_channel_raw = cache.get(cache_key_short_term)
+            if cached_alert_receive_channel_raw == CHANNEL_DOES_NOT_EXIST_PLACEHOLDER:
+                logger.info(f"Integration {kwargs['alert_channel_key']} already cached as non-existent")
+                raise PermissionDenied(INTEGRATION_PERMISSION_DENIED_MESSAGE)
+
             if cached_alert_receive_channel_raw is not None:
                 try:
                     alert_receive_channel = next(
@@ -45,18 +53,21 @@ class AlertChannelDefiningMixin(object):
 
             if alert_receive_channel is None:
                 # Trying to define channel from DB
-                alert_receive_channel = AlertReceiveChannel.objects.get(token=kwargs["alert_channel_key"])
-                # Update short term cache
-                serialized = serializers.serialize("json", [alert_receive_channel])
-                cache.set(cache_key_short_term, serialized, self.CACHE_SHORT_TERM_TIMEOUT)
+                try:
+                    alert_receive_channel = AlertReceiveChannel.objects_with_deleted.get(
+                        token=kwargs["alert_channel_key"]
+                    )
+                except AlertReceiveChannel.DoesNotExist:
+                    cache.set(cache_key_short_term, CHANNEL_DOES_NOT_EXIST_PLACEHOLDER, self.CACHE_SHORT_TERM_TIMEOUT)
+                else:
+                    # Update short term cache
+                    serialized = serializers.serialize("json", [alert_receive_channel])
+                    cache.set(cache_key_short_term, serialized, self.CACHE_SHORT_TERM_TIMEOUT)
 
-                # Update cached channels
-                if cache.get(self.CACHE_DB_FALLBACK_OBSOLETE_KEY) is None:
-                    cache.set(self.CACHE_DB_FALLBACK_OBSOLETE_KEY, True, self.CACHE_DB_FALLBACK_REFRESH_INTERVAL)
-                    self.update_alert_receive_channel_cache()
-
-        except AlertReceiveChannel.DoesNotExist:
-            raise PermissionDenied("Integration key was not found. Permission denied.")
+                    # Update cached channels
+                    if cache.get(self.CACHE_DB_FALLBACK_OBSOLETE_KEY) is None:
+                        cache.set(self.CACHE_DB_FALLBACK_OBSOLETE_KEY, True, self.CACHE_DB_FALLBACK_REFRESH_INTERVAL)
+                        self.update_alert_receive_channel_cache()
         except OperationalError:
             logger.info("Cannot connect to database, using cache to consume alerts!")
 
@@ -67,18 +78,26 @@ class AlertChannelDefiningMixin(object):
                         alert_receive_channel = obj.object
 
                 if alert_receive_channel is None:
-                    raise PermissionDenied("Integration key was not found in cache. Permission denied.")
+                    logger.info(f"Integration {kwargs['alert_channel_key']} not found in fallback cache")
+                    raise PermissionDenied(INTEGRATION_PERMISSION_DENIED_MESSAGE)
 
             else:
                 logger.info("Cache is empty!")
                 raise
         else:
+            if not alert_receive_channel:
+                logger.info(f"Integration {kwargs['alert_channel_key']} does not exist")
+                raise PermissionDenied(INTEGRATION_PERMISSION_DENIED_MESSAGE)
             if alert_receive_channel.organization.is_moved:
+                logger.info(
+                    f"Channel {kwargs['alert_channel_key']} in organization {alert_receive_channel.organization.public_primary_key} is moved"
+                )
                 raise OrganizationMovedException(alert_receive_channel.organization)
-            if alert_receive_channel.organization.deleted_at:
-                # It's better to raise OrganizarionDeletedException, but in legacy code PermissionDenied is returned when integration key not found.
-                # So, keep it consistent.
-                raise PermissionDenied("Integration key was not found. Permission denied.")
+            if alert_receive_channel.deleted_at or alert_receive_channel.organization.deleted_at:
+                logger.info(
+                    f"Channel {kwargs['alert_channel_key']} or organization {alert_receive_channel.organization.public_primary_key} is deleted"
+                )
+                raise PermissionDenied(INTEGRATION_PERMISSION_DENIED_MESSAGE)
 
         del kwargs["alert_channel_key"]
 

engine/apps/integrations/tests/test_views.py
@@ -12,7 +12,9 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from apps.alerts.models import AlertReceiveChannel
+from apps.alerts.models.alert_receive_channel import random_token_generator
 from apps.integrations.mixins import AlertChannelDefiningMixin
+from apps.integrations.mixins.alert_channel_defining_mixin import CHANNEL_DOES_NOT_EXIST_PLACEHOLDER
 from apps.integrations.views import UniversalAPIView
 
 # https://github.com/pytest-dev/pytest-xdist/issues/432#issuecomment-528510433
@@ -520,3 +522,39 @@ def test_integration_outdated_cached_model(
             "received_at": now.isoformat(),
         },
     )
+
+
+@patch("django.core.cache.cache.get", wraps=cache.get)
+@patch("django.core.cache.cache.set", wraps=cache.set)
+@patch(
+    "apps.alerts.models.AlertReceiveChannel.objects_with_deleted.get",
+    wraps=AlertReceiveChannel.objects_with_deleted.get,
+)
+@pytest.mark.parametrize(
+    "integration_type",
+    [arc_type for arc_type in INTEGRATION_TYPES],
+)
+@pytest.mark.django_db
+def test_non_existent_integration_does_not_repeat_access_db(
+    mock_db_get, mock_cache_set, mock_cache_get, integration_type
+):
+    attempts = 5
+    non_existent_integration_token = random_token_generator()
+    cache_key = AlertChannelDefiningMixin.CACHE_KEY_SHORT_TERM + "_" + non_existent_integration_token
+    client = APIClient()
+    url = reverse(
+        "integrations:universal",
+        kwargs={"integration_type": integration_type, "alert_channel_key": non_existent_integration_token},
+    )
+
+    for _ in range(attempts):
+        data = {"foo": "bar"}
+        response = client.post(url, data, format="json")
+        mock_cache_get.assert_called_with(cache_key)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    assert mock_cache_get.call_count == attempts
+    mock_cache_set.assert_called_once_with(
+        cache_key, CHANNEL_DOES_NOT_EXIST_PLACEHOLDER, AlertChannelDefiningMixin.CACHE_SHORT_TERM_TIMEOUT
+    )
+    mock_db_get.assert_called_once_with(token=non_existent_integration_token)

engine/engine/middlewares.py
@@ -2,8 +2,6 @@ import datetime
 import logging
 
 from django.conf import settings
-from django.core.exceptions import PermissionDenied
-from django.db import OperationalError
 from django.utils.deprecation import MiddlewareMixin
 
 logger = logging.getLogger(__name__)
@@ -53,42 +51,6 @@ class RequestTimeLoggingMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         self.log_message(request, response, "response")
         return response
-
-
-class BanAlertConsumptionBasedOnSettingsMiddleware(MiddlewareMixin):
-    """
-    Banning requests for /integrations/v1
-    Banning is not guaranteed.
-    """
-
-    def is_banned(self, path):
-        try:
-            from apps.base.models import DynamicSetting
-
-            banned_paths = DynamicSetting.objects.get_or_create(
-                name="ban_hammer_list",
-                defaults={
-                    "json_value": [
-                        "full_path_here",
-                    ]
-                },
-            )[0]
-            result = any(p for p in banned_paths.json_value if path.startswith(p))
-            return result
-        except OperationalError:
-            # Fallback to make sure we consume the request even if DB is down.
-            logger.info("Cannot connect to database, assuming the request is not banned by default.")
-            return False
-
-    def process_request(self, request):
-        if request.path.startswith("/integrations/v1") and self.is_banned(request.path):
-            try:
-                # Consume request body since other middleware will be skipped
-                request.body
-            except Exception:
-                pass
-            logging.warning(f"{request.path} has been banned")
-            raise PermissionDenied()
 
 
 class LogRequestHeadersMiddleware:

engine/settings/base.py
@@ -331,7 +331,6 @@ SPECTACULAR_INCLUDED_PATHS = [
 MIDDLEWARE = [
     "log_request_id.middleware.RequestIDMiddleware",
     "engine.middlewares.RequestTimeLoggingMiddleware",
-    "engine.middlewares.BanAlertConsumptionBasedOnSettingsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",

commit bcd1953beb2a3050dfc67969c124d3768bd6d417

Fix bug in ratelimit logic introduced in #4137

engine/apps/integrations/mixins/ratelimit_mixin.py
@@ -100,7 +100,7 @@ def is_ratelimit_ignored(alert_receive_channel):
                 ]
             },
         )[0]
-        integration_tokens_to_ignore_ratelimit = dynamic_setting.json_value[0].json_value
+        integration_tokens_to_ignore_ratelimit = dynamic_setting.json_value
         cache.set(RATELIMIT_BYPASS_CACHE_KEY, integration_tokens_to_ignore_ratelimit, RATELIMIT_BYPASS_CACHE_TIMEOUT)
     return alert_receive_channel.token in integration_tokens_to_ignore_ratelimit
 