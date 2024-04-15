d83c7aec1b8d00dcf4b677fcc74661a2207951ae
Dominik <dominik.broj@grafana.com>
2024-04-15 20:29:13 +0200

    update

--- .github/actions/get-installation-token.js
+++ .github/actions/get-installation-token.js
@@ -12,17 +12,21 @@ const { Octokit } = require("octokit");
   // const repos = await app.octokit.request("Get /user/repos?type=private", {});
   // console.log("REPOS: ", repos);
 
-  // const appOctokit = new Octokit({
-  //   authStrategy: createAppAuth,
-  //   auth: {
-  //     appId: process.env.GH_APP_ID,
-  //     privateKey: process.env.GH_APP_PRIVATE_KEY,
-  //     installationId: process.env.GH_APP_INSTALLATION_ID,
-  //   },
-  // });
+  const appOctokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+      appId: process.env.GH_APP_ID,
+      privateKey: process.env.GH_APP_PRIVATE_KEY,
+      installationId: process.env.GH_APP_INSTALLATION_ID,
+    },
+  });
 
-  // const repos = await appOctokit.request("Get /user/repos?type=private", {});
-  // console.log("REPOS: ", repos);
+  const installationRequests = await appOctokit.request(
+    "GET /app/installation-requests"
+  );
+  const installations = await appOctokit.request("GET /app/installations");
+  console.log("INSTALLATIONS: ", installations);
+  console.log("INSTALLATION REQUESTS: ", installationRequests);
 
   // -----------
   const auth = createAppAuth({
@@ -33,8 +37,8 @@ const { Octokit } = require("octokit");
     type: "installation",
     installationId: process.env.GH_APP_INSTALLATION_ID,
   });
-  // console.log("TOOOKEN: ", token);
-  // console.log(("TOKEN TYPE: ", tokenType));
+  console.log("TOOOKEN: ", token);
+  console.log(("TOKEN TYPE: ", tokenType));
 
   // const tokenWithPrefix =
   //   tokenType === "installation" ? `x-access-token:${token}` : token;
