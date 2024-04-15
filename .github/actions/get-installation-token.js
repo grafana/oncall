3fbde2e09b29e5d852caa27c15eb848f53ccb363
Dominik <dominik.broj@grafana.com>
2024-04-15 17:51:23 +0200

    update

--- .github/actions/get-installation-token.js
+++ .github/actions/get-installation-token.js
@@ -6,17 +6,18 @@ const { createAppAuth } = require("@octokit/auth-app");
   //   privateKey: process.env.GH_APP_PRIVATE_KEY,
   //   installationId: process.env.GH_APP_INSTALLATION_ID,
   // });
-  const auth = createAppAuth({
-    appId: process.env.GH_APP_ID,
-    privateKey: process.env.GH_APP_PRIVATE_KEY,
-  });
+  // const auth = createAppAuth({
+  //   appId: process.env.GH_APP_ID,
+  //   privateKey: process.env.GH_APP_PRIVATE_KEY,
+  // });
 
-  const resp = await auth({
-    type: "installation",
-    installationId: process.env.GH_APP_INSTALLATION_ID,
-  });
+  // const resp = await auth({
+  //   type: "installation",
+  //   installationId: process.env.GH_APP_INSTALLATION_ID,
+  // });
   // console.log("TOOOKEN", resp.token);
-  return resp.token;
+  // return resp.token;
+  return "WOOOOOW";
 })().catch((e) => {
   // console.error("EEEEERRORRR", e);
   process.exit(1);
