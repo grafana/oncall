540a0a65942af7c9198dda839092ee758288d25a
Michael Derynck <michael.derynck@grafana.com>
2024-04-15 15:12:20 -0600

    Fix test_alert

--- engine/apps/alerts/tests/test_alert.py
+++ engine/apps/alerts/tests/test_alert.py
@@ -4,7 +4,6 @@ import pytest
 from django.utils import timezone
 
 from apps.alerts.models import Alert, ChannelFilter, EscalationPolicy
-from apps.alerts.tasks import distribute_alert, escalate_alert_group
 from common.jinja_templater.apply_jinja_template import JinjaTemplateError, JinjaTemplateWarning
 
 
@@ -118,12 +117,13 @@ def test_alert_create_track_received_at_timestamp(make_organization, make_alert_
     assert alert_group.received_at == now
 
 
+@patch("apps.alerts.models.AlertGroup.start_escalation_if_needed", return_value=None)
 @pytest.mark.django_db
 def test_distribute_alert_escalate_alert_group(
+    mock_start_escalation,
     make_organization,
     make_alert_receive_channel,
     make_channel_filter,
-    make_alert_group,
     make_alert,
     make_escalation_chain,
     make_escalation_policy,
@@ -139,35 +139,42 @@ def test_distribute_alert_escalate_alert_group(
     )
     alert_receive_channel = make_alert_receive_channel(organization)
     channel_filter = make_channel_filter(alert_receive_channel, escalation_chain=escalation_chain)
-    alert_group = make_alert_group(alert_receive_channel, channel_filter=channel_filter)
 
     # Check escalate_alert_group is called for the first alert in the group
-    alert_1 = make_alert(
-        alert_group=alert_group,
-        is_the_first_alert_in_group=True,
+    Alert.create(
+        title="the title",
+        message="the message",
+        integration_unique_data={},
+        image_url=None,
+        link_to_upstream_details=None,
         raw_request_data=alert_receive_channel.config.example_payload,
+        alert_receive_channel=alert_receive_channel,
+        channel_filter=channel_filter,
     )
-    with patch.object(escalate_alert_group, "apply_async") as mock_escalate_alert_group_1:
-        distribute_alert(alert_1.pk)
-    mock_escalate_alert_group_1.assert_called_once()
+    mock_start_escalation.assert_called_once()
+    mock_start_escalation.reset_mock()
 
     # Check escalate_alert_group is not called for the second alert in the group
-    alert_2 = make_alert(
-        alert_group=alert_group,
-        is_the_first_alert_in_group=False,
+    Alert.create(
+        title="the title",
+        message="the message",
+        integration_unique_data={},
+        image_url=None,
+        link_to_upstream_details=None,
         raw_request_data=alert_receive_channel.config.example_payload,
+        alert_receive_channel=alert_receive_channel,
+        channel_filter=channel_filter,
     )
-    with patch.object(escalate_alert_group, "apply_async") as mock_escalate_alert_group_2:
-        distribute_alert(alert_2.pk)
-    mock_escalate_alert_group_2.assert_not_called()
+    mock_start_escalation.assert_not_called()
 
 
+@patch("apps.alerts.models.AlertGroup.start_escalation_if_needed", return_value=None)
 @pytest.mark.django_db
 def test_distribute_alert_escalate_alert_group_when_escalation_paused(
+    mock_start_escalation,
     make_organization,
     make_alert_receive_channel,
     make_channel_filter,
-    make_alert_group,
     make_alert,
     make_escalation_chain,
     make_escalation_policy,
@@ -184,31 +191,37 @@ def test_distribute_alert_escalate_alert_group_when_escalation_paused(
     )
     alert_receive_channel = make_alert_receive_channel(organization)
     channel_filter = make_channel_filter(alert_receive_channel, escalation_chain=escalation_chain)
-    alert_group = make_alert_group(alert_receive_channel, channel_filter=channel_filter)
 
     # Check escalate_alert_group is called for the first alert in the group
-    alert_1 = make_alert(
-        alert_group=alert_group,
-        is_the_first_alert_in_group=True,
+    Alert.create(
+        title="the title",
+        message="the message",
+        integration_unique_data={},
+        image_url=None,
+        link_to_upstream_details=None,
         raw_request_data=alert_receive_channel.config.example_payload,
+        alert_receive_channel=alert_receive_channel,
+        channel_filter=channel_filter,
     )
-    with patch.object(escalate_alert_group, "apply_async") as mock_escalate_alert_group_1:
-        distribute_alert(alert_1.pk)
-    mock_escalate_alert_group_1.assert_called_once()
+    mock_start_escalation.assert_called_once()
+    mock_start_escalation.reset_mock()
 
     # Check escalate_alert_group is called for the second alert in the group when escalation is paused
-    alert_2 = make_alert(
-        alert_group=alert_group,
-        is_the_first_alert_in_group=False,
-        raw_request_data=alert_receive_channel.config.example_payload,
-    )
     with patch(
         "apps.alerts.escalation_snapshot.escalation_snapshot_mixin.EscalationSnapshotMixin.pause_escalation",
         new_callable=PropertyMock(return_value=True),
     ):
-        with patch.object(escalate_alert_group, "apply_async") as mock_escalate_alert_group_2:
-            distribute_alert(alert_2.pk)
-    mock_escalate_alert_group_2.assert_called_once()
+        Alert.create(
+            title="the title",
+            message="the message",
+            integration_unique_data={},
+            image_url=None,
+            link_to_upstream_details=None,
+            raw_request_data=alert_receive_channel.config.example_payload,
+            alert_receive_channel=alert_receive_channel,
+            channel_filter=channel_filter,
+        )
+        mock_start_escalation.assert_called_once()
 
 
 @pytest.mark.django_db
