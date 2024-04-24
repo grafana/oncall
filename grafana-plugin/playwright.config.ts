3a8368d6b437e1b181c015f5c380a1dbb66dadbe
Dominik <dominik.broj@grafana.com>
2024-04-24 12:43:56 +0200

    update

--- grafana-plugin/playwright.config.ts
+++ grafana-plugin/playwright.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
    * to flaky tests.. let's allow 1 retry per test
    */
   retries: 1,
-  workers: '50%',
+  workers: '50%', // 50% of logical CPU cores, e.g. for 16 CPU cores it will use 8 workers
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
