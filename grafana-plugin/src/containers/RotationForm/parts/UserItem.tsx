6ed7a1e3b8393b7dc434e4da1dd2f6cdc8f62759
Rares Mardare <rares.mardare@grafana.com>
2024-04-29 11:13:26 +0300

    Tag component tweaks to get rid of hardcoded tag variables (#4280)
    # What this PR does
    
    - Removed the usage of `var(--` within the Tag component to help get rid
    of the vars file once we fully migrate to emotion
    - Added few other display tweaks and migrated a few stylesheets to
    emotion

--- grafana-plugin/src/containers/RotationForm/parts/UserItem.tsx
+++ grafana-plugin/src/containers/RotationForm/parts/UserItem.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
 
+import { css } from '@emotion/css';
+import { useStyles2 } from '@grafana/ui';
 import cn from 'classnames/bind';
 import dayjs from 'dayjs';
+import { COLORS } from 'styles/utils.styles';
 
 import { Text } from 'components/Text/Text';
 import { WorkingHours } from 'components/WorkingHours/WorkingHours';
@@ -23,6 +26,7 @@ const WEEK_IN_SECONDS = 60 * 60 * 24 * 7;
 
 export const UserItem = ({ pk, shiftColor, shiftStart, shiftEnd }: UserItemProps) => {
   const { userStore } = useStore();
+  const styles = useStyles2(getStyles);
 
   useEffect(() => {
     if (!userStore.items[pk]) {
@@ -48,8 +52,16 @@ export const UserItem = ({ pk, shiftColor, shiftStart, shiftEnd }: UserItemProps
         />
       )}
       <div className={cx('user-title')}>
-        <Text strong>{name}</Text> <Text style={{ color: 'var(--always-gray)' }}>({desc})</Text>
+        <Text strong>{name}</Text> <Text className={styles.gray}>({desc})</Text>
       </div>
     </div>
   );
+};
+
+const getStyles = () => {
+  return {
+    gray: css`
+      color: ${COLORS.ALWAYS_GREY};
+    `,
+  };
 };
