39e6b91b8d893460a1bd00222a7a02833ae75470
Michael Derynck <michael.derynck@grafana.com>
2024-04-30 14:32:18 -0600

    Change date range query delimiter to _

--- grafana-plugin/src/utils/datetime.ts
+++ grafana-plugin/src/utils/datetime.ts
@@ -11,11 +11,11 @@ export function convertRelativeToAbsoluteDate(dateRangeString: string) {
     return undefined;
   }
 
-  const [from, to] = dateRangeString?.split('/') || [];
+  const [from, to] = dateRangeString?.split('_') || [];
   if (rangeUtil.isRelativeTimeRange({ from, to })) {
     const { from: startDate, to: endDate } = rangeUtil.convertRawToRange({ from, to });
 
-    return `${startDate.utc().format('YYYY-MM-DDTHH:mm:ss')}/${endDate.utc().format('YYYY-MM-DDTHH:mm:ss')}`;
+    return `${startDate.utc().format('YYYY-MM-DDTHH:mm:ss')}_${endDate.utc().format('YYYY-MM-DDTHH:mm:ss')}`;
   }
   return dateRangeString;
 }
@@ -23,7 +23,7 @@ export function convertRelativeToAbsoluteDate(dateRangeString: string) {
 export const getValueForDateRangeFilterType = (rawInput: string) => {
   let value = { from: undefined, to: undefined, raw: { from: '', to: '' } };
   if (rawInput) {
-    const [fromString, toString] = rawInput.split('/');
+    const [fromString, toString] = rawInput.split('_');
     const isRelative = rangeUtil.isRelativeTimeRange({ from: fromString, to: toString });
 
     const raw = {
@@ -33,7 +33,7 @@ export const getValueForDateRangeFilterType = (rawInput: string) => {
 
     if (isRelative) {
       const absolute = convertRelativeToAbsoluteDate(rawInput);
-      const [absoluteFrom, absoluteTo] = absolute.split('/');
+      const [absoluteFrom, absoluteTo] = absolute.split('_');
       value = {
         from: moment(absoluteFrom + 'Z'),
         to: moment(absoluteTo + 'Z'),
