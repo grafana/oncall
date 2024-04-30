cb8a62126913e9de7c09bd89e9f19a5c939e4d0c
Maxim Mordasov <maxim.mordasov@grafana.com>
2024-04-30 16:16:55 +0300

    Update grafana-plugin/e2e-tests/settings/tabs.test.ts
    Co-authored-by: Dominik Broj <dominik.broj@grafana.com>

--- grafana-plugin/e2e-tests/settings/tabs.test.ts
+++ grafana-plugin/e2e-tests/settings/tabs.test.ts
@@ -5,7 +5,7 @@ test(`tab query param is used to show proper page tab`, async ({ adminRolePage }
   const { page } = adminRolePage;
   goToOnCallPage(page, `settings`, { tab: 'ChatOps' });
 
-  const tab = await page.locator("button[aria-label='Tab Chat Ops']");
+  const tab = page.getByRole('tab', { name: 'Chat Ops' });
   const isSelected = await tab.getAttribute('aria-selected');
 
   expect(isSelected).toBe('true');
