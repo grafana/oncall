3a793dcd0fc35b3432ac8caa62787e96b12159e7
Dominik <dominik.broj@grafana.com>
2024-05-28 16:49:54 +0200

    update

--- grafana-plugin/src/error-types.ts
+++ grafana-plugin/src/error-types.ts
@@ -1,78 +1,34 @@
-/**
- * This one would automatically show notification in top right corner with proper icon and color according to severity.
- * It optionally might have title. Description is required.
- */
-type GlobalNotificationError = {
-  type: 'global-notification';
-  severity: 'error' | 'warning';
-  title?: string;
-  description: string;
-};
+const OnCallErrorCode = {
+  // 1 - 999 reserved for engine errors
+  CANNOT_CREATE_INTEGRATION: 1,
+  CANNOT_CREATE_SCHEDULE: 2,
+  CANNOT_RETRIEVE_USER: 3,
+  SERVICE_ACCOUNT_TOKEN_INVALID: 4,
+  ONCALL_API_TOKEN_INVALID: 5,
+  // ...
 
-/**
- * This one can be used to conditionally show inline banner in any place on UI.
- */
-type InlineBannerError = {
-  type: 'inline-banner';
-  severity: 'error' | 'warning';
-  title?: string;
-  description: string;
-};
+  // 1000 - 1999 reserved for backend plugin proxy errors
+  // ...
+} as const;
+type OnCallErrorCode = (typeof OnCallErrorCode)[keyof typeof OnCallErrorCode];
 
-/**
- * This one can be used to redirect user to a specific URL.
- */
-type RedirectError = {
-  type: 'redirect';
-  path: string;
-};
-
-/**
- * This one is used to show form validation errors. Keys in fields should match the names of fields in form
- * so that they can be easily mapped to react-hook-form errors and properly show below each form field.
- */
-type FormError = {
-  type: 'form';
-  fields: Record<string, string[]>;
-};
-
-/**
- * This one is used just to log error in the console but they wouldn't be shown in UI.
- */
-type ImplicitError = {
-  type: 'implicit';
+type OnCallError = {
+  code: OnCallErrorCode;
   message: string;
+  details?: string;
+  fields?: {
+    [key: string]: string[];
+  };
 };
 
-type OnCallError = GlobalNotificationError | InlineBannerError | RedirectError | FormError | ImplicitError;
+// Unsuccessful HTTP response can be a mix of any of the errors above
 
-/**
- * Unsuccessful HTTP response can be a mix of any of the errors above. For example you might want to
- * show inline form validation errors and at the same time global notification.
- */
 type OnCallUnsuccessfulHttpResponse = OnCallError[];
 
-const GlobalNotificationErrorExample: OnCallUnsuccessfulHttpResponse = [
+const ErrorExample1: OnCallUnsuccessfulHttpResponse = [
   {
-    type: 'global-notification',
-    severity: 'error',
-    title: 'Webhook creation failed',
-    description: 'You already have another webhook with the same name.',
-  },
-];
-
-const InlineBannerErrorExample: OnCallUnsuccessfulHttpResponse = [
-  {
-    type: 'inline-banner',
-    severity: 'warning',
-    title: 'Inline banner title',
-    description: 'Inline banner description',
-  },
-];
-
-const FormErrorExample: OnCallUnsuccessfulHttpResponse = [
-  {
-    type: 'form',
+    code: 1,
+    message: 'Cannot create integration',
     fields: {
       name: ['Ensure this field has no more than 100 characters.', 'Name cannot include special characters.'],
       url: ['This field is required'],
@@ -80,38 +36,21 @@ const FormErrorExample: OnCallUnsuccessfulHttpResponse = [
   },
 ];
 
-const ImplicitErrorExample: OnCallUnsuccessfulHttpResponse = [
+const ErrorExample2: OnCallUnsuccessfulHttpResponse = [
   {
-    type: 'implicit',
-    message: 'Error message',
+    code: 2,
+    message: 'Cannot create schedule',
+    details: 'Schedule with the same name already exists',
   },
 ];
 
-const MixedErrorExample: OnCallUnsuccessfulHttpResponse = [
+const ErrorExample3: OnCallUnsuccessfulHttpResponse = [
   {
-    type: 'global-notification',
-    severity: 'error',
-    title: 'Webook cannot be created',
-    description: 'Your form includes invalid data.',
+    code: 3,
+    message: 'Cannot retrieve user',
   },
   {
-    type: 'form',
-    fields: {
-      name: ['Ensure this field has no more than 100 characters.', 'Name cannot include special characters.'],
-      url: ['This field is required'],
-    },
-  },
-];
-
-const MixedErrorExample2: OnCallUnsuccessfulHttpResponse = [
-  {
-    type: 'redirect',
-    path: '/plugins/grafana-oncall-app/configure',
-  },
-  {
-    type: 'inline-banner',
-    severity: 'warning',
-    title: 'Invalid OnCall API URL',
-    description: 'Make sure your OnCall API URL is correct and retest connection.',
+    code: 4,
+    message: 'OnCall API token invalid',
   },
 ];
