1ed0866c8bbf9ab3d46f6fc808e4b78a0508a500
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2024-06-04 15:29:34 +0800

    Fix slash command tests

--- engine/apps/slack/tests/test_slash_command.py
+++ engine/apps/slack/tests/test_slash_command.py
@@ -2,22 +2,30 @@ from apps.slack.slash_command import SlashCommand
 
 
 def test_parse():
-    text = "/grafana escalate"
-    slash_command = SlashCommand.parse(text)
+    payload = {
+        "command": "/grafana",
+        "text": "escalate",
+        "trigger_id": "trigger_id",
+        "user_id": "user_id",
+        "user_name": "user_name",
+        "api_app_id": "api_app_id",
+    }
+    slash_command = SlashCommand.parse(payload)
     assert slash_command.command == "grafana"
     assert slash_command.args == ["escalate"]
+    assert slash_command.subcommand == "escalate"
 
 
 def test_parse_command_without_subcommand():
-    text = "/escalate"
-    slash_command = SlashCommand.parse(text)
+    payload = {
+        "command": "/escalate",
+        "text": "",
+        "trigger_id": "trigger_id",
+        "user_id": "user_id",
+        "user_name": "user_name",
+        "api_app_id": "api_app_id",
+    }
+    slash_command = SlashCommand.parse(payload)
     assert slash_command.command == "escalate"
     assert slash_command.args == []
     assert slash_command.subcommand is None
-
-
-def test_subcommand():
-    command = "grafana"
-    args = ["escalate"]
-    slash_command = SlashCommand(command, args)
-    assert slash_command.subcommand == "escalate"
