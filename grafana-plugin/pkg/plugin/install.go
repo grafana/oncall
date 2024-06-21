39256776dab593603142dc2fdb04ec19eedf00be
Dominik <dominik.broj@grafana.com>
2024-06-21 10:29:17 +0200

    update

--- grafana-plugin/pkg/plugin/install.go
+++ grafana-plugin/pkg/plugin/install.go
@@ -15,21 +15,6 @@ type OnCallInstall struct {
 	OnCallError `json:"onCallError,omitempty"`
 }
 
-func sendInstallError(w http.ResponseWriter) {
-	w.Header().Add("Content-Type", "application/json")
-		installError := OnCallInstall{
-			OnCallError: OnCallError{
-				Code:    INSTALL_ERROR_CODE,
-				Message: "Install failed check /status for details",
-			},
-		}
-		if err := json.NewEncoder(w).Encode(installError); err != nil {
-			http.Error(w, "Failed to encode response", http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusBadRequest)
-}
-
 // TODO: Lock so that multiple installs do not revoke each others tokens
 func (a *App) handleInstall(w http.ResponseWriter, req *http.Request) {
 	onCallPluginSettings, err := a.OnCallSettingsFromContext(req.Context())
@@ -88,7 +73,18 @@ func (a *App) handleInstall(w http.ResponseWriter, req *http.Request) {
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		sendInstallError(w)
+		w.Header().Add("Content-Type", "application/json")
+		installError := OnCallInstall{
+			OnCallError: OnCallError{
+				Code:    INSTALL_ERROR_CODE,
+				Message: "Install failed check /status for details",
+			},
+		}
+		if err := json.NewEncoder(w).Encode(installError); err != nil {
+			http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
 	} else {
 		provisionBody, err := io.ReadAll(res.Body)
 		if err != nil {
