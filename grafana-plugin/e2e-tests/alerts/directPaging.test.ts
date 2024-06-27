154f287fda8d05f998ea59a78ae4f64eb427cf2e
Dominik <dominik.broj@grafana.com>
2024-06-27 15:43:25 +0200

    update

--- grafana-plugin/e2e-tests/alerts/directPaging.test.ts
+++ grafana-plugin/e2e-tests/alerts/directPaging.test.ts
@@ -1,3 +1,5 @@
+import semver from 'semver';
+
 import { test, expect } from '../fixtures';
 import { clickButton, fillInInput } from '../utils/forms';
 import { goToOnCallPage } from '../utils/navigation';
@@ -20,7 +22,9 @@ test('we can directly page a user', async ({ adminRolePage }) => {
 
   const addRespondersPopup = page.getByTestId('add-responders-popup');
 
-  await addRespondersPopup.getByText('Users').click();
+  await addRespondersPopup[semver.lt(process.env.CURRENT_GRAFANA_VERSION, '10.3.0') ? 'getByText' : 'getByLabel'](
+    'Users'
+  ).click();
   await addRespondersPopup.getByText(adminRolePage.userName).first().click();
 
   // If user is not on call, confirm invitation
