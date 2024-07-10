0276e005888d059708a0d2bd3b288bb413868f73
Ildar Iskhakov <Ildar.iskhakov@grafana.com>
2024-07-10 16:39:49 +0800

    Update consts.ts

--- grafana-plugin/src/utils/consts.ts
+++ grafana-plugin/src/utils/consts.ts
@@ -32,7 +32,7 @@ export const PLUGIN_ROOT = '/a/grafana-oncall-app';
 
 // Environment options list for onCallApiUrl
 export const ONCALL_PROD = 'https://oncall-prod-us-central-0.grafana.net/oncall';
-export const ONCALL_OPS = 'https://oncall-ops-us-east-0.grafana.net/oncall';
+export const ONCALL_OPS = 'https://oncall-ops-eu-south-0.grafana.net/oncall';
 export const ONCALL_DEV = 'https://oncall-dev-us-central-0.grafana.net/oncall';
 
 export const getIsDevelopmentEnv = () => {
