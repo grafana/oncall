fb4d44174df67784f94a4e4df055fea59b80e820
Julia <ferril.darkdiver@gmail.com>
2024-07-11 14:39:08 +0200

    fix migrations

--- engine/apps/phone_notifications/migrations/0003_smsrecord_represents_bundle_uuid.py
+++ engine/apps/phone_notifications/migrations/0003_smsrecord_represents_bundle_uuid.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('phone_notifications', '0001_initial'),
+        ('phone_notifications', '0002_bannedphonenumber'),
     ]
 
     operations = [
