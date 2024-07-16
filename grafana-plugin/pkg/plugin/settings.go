aee2308cb1f8bf9f2cacdcdd3120fac574dac074
Michael Derynck <michael.derynck@grafana.com>
2024-07-16 14:05:24 -0600

    Assume dev-oss == OpenSource license

--- grafana-plugin/pkg/plugin/settings.go
+++ grafana-plugin/pkg/plugin/settings.go
@@ -79,12 +79,10 @@ func (a *App) OnCallSettingsFromContext(ctx context.Context) (*OnCallPluginSetti
 	if settings.License == "" {
 		cloudRe := regexp.MustCompile(CLOUD_VERSION_PATTERN)
 		ossRe := regexp.MustCompile(OSS_VERSION_PATTERN)
-		if pluginContext.PluginVersion == DEV_PATTERN {
-			return &settings, fmt.Errorf("plugin version is dev-oss license must be set in jsonData.license")
+		if pluginContext.PluginVersion == DEV_PATTERN || ossRe.MatchString(pluginContext.PluginVersion) {
+			settings.License = OPEN_SOURCE_LICENSE_NAME
 		} else if cloudRe.MatchString(pluginContext.PluginVersion) {
 			settings.License = CLOUD_LICENSE_NAME
-		} else if ossRe.MatchString(pluginContext.PluginVersion) {
-			settings.License = OPEN_SOURCE_LICENSE_NAME
 		} else {
 			return &settings, fmt.Errorf("jsonData.license is not set and version %s did not match a known pattern", pluginContext.PluginVersion)
 		}
