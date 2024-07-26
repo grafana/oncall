ba0d67eb98e837781644acb03b746b3af52c84b2
Rares Mardare <rares.mardare@grafana.com>
2024-07-26 11:49:29 +0300

    set it to null at the end of the timeout cycle

--- grafana-plugin/src/pages/incidents/Incidents.tsx
+++ grafana-plugin/src/pages/incidents/Incidents.tsx
@@ -1043,6 +1043,7 @@ class _IncidentsPage extends React.Component<IncidentsPageProps, IncidentsPageSt
           return;
         }
 
+        this.pollingIntervalId = null;
         startPolling();
       }, pollingNum);
     };
