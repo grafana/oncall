35a6358068a2b509a528a6f90bb1e255de5cf51a
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2024-07-31 15:56:26 +0800

    Fix tests 2

--- engine/apps/slack/tests/test_slash_command.py
+++ engine/apps/slack/tests/test_slash_command.py
@@ -11,7 +11,7 @@ def test_parse():
         "api_app_id": "api_app_id",
     }
     slash_command = SlashCommand.parse(payload)
-    assert slash_command.command == "grafana"
+    assert slash_command.command == "grafana-irm"
     assert slash_command.args == ["escalate"]
     assert slash_command.subcommand == "escalate"
     assert slash_command.is_grafana_command
