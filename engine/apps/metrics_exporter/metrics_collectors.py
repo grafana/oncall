a46bf51e14e924387527673c22ea4c7a804c4b32
Julia <ferril.darkdiver@gmail.com>
2024-08-06 16:46:42 +0200

    Split application metrics collector into two collectors

--- engine/apps/metrics_exporter/metrics_collectors.py
+++ engine/apps/metrics_exporter/metrics_collectors.py
@@ -1,10 +1,11 @@
 import logging
 import re
 import typing
+from abc import ABC, abstractmethod
 
 from django.core.cache import cache
 from prometheus_client import CollectorRegistry
-from prometheus_client.metrics_core import CounterMetricFamily, GaugeMetricFamily, HistogramMetricFamily
+from prometheus_client.metrics_core import CounterMetricFamily, GaugeMetricFamily, HistogramMetricFamily, Metric
 
 from apps.alerts.constants import AlertGroupState
 from apps.metrics_exporter.constants import (
@@ -26,6 +27,10 @@ from apps.metrics_exporter.helpers import (
     get_organization_ids,
 )
 from apps.metrics_exporter.tasks import start_calculate_and_cache_metrics, start_recalculation_for_new_metric
+from settings.base import (
+    ALERT_GROUPS_AND_NOTIFICATIONS_METRIC_COLLECTOR_ENABLED,
+    RESPONSE_TIME_METRIC_COLLECTOR_ENABLED,
+)
 
 application_metrics_registry = CollectorRegistry()
 
@@ -41,7 +46,7 @@ RE_USER_WAS_NOTIFIED_OF_ALERT_GROUPS = re.compile(_RE_BASE_PATTERN.format(USER_W
 
 
 # https://github.com/prometheus/client_python#custom-collectors
-class ApplicationMetricsCollector:
+class BaseApplicationMetricsCollector(ABC):
     def __init__(self):
         self._buckets = (60, 300, 600, 3600, "+Inf")
         self._stack_labels = [
@@ -60,30 +65,85 @@ class ApplicationMetricsCollector:
         self._integration_labels_with_state = self._integration_labels + ["state"]
         self._user_labels = ["username"] + self._stack_labels
 
+    @abstractmethod
+    def get_metrics_and_missing_orgs(self, org_ids: set) -> typing.Tuple[typing.List[Metric], set]:
+        raise NotImplementedError
+
     def collect(self):
         org_ids = set(get_organization_ids())
 
-        # alert groups total metric: gauge
-        alert_groups_total, missing_org_ids_1 = self._get_alert_groups_total_metric(org_ids)
-        # alert groups response time metrics: histogram
-        alert_groups_response_time_seconds, missing_org_ids_2 = self._get_response_time_metric(org_ids)
-        # user was notified of alert groups metrics: counter
-        user_was_notified, missing_org_ids_3 = self._get_user_was_notified_of_alert_groups_metric(org_ids)
-
-        # This part is used for releasing new metrics to avoid recalculation for every metric.
-        # Uncomment with metric name when needed.
-        # # update new metric gradually
-        # missing_org_ids_3 = self._update_new_metric(USER_WAS_NOTIFIED_OF_ALERT_GROUPS, org_ids, missing_org_ids_3)
-
-        # check for orgs missing any of the metrics or needing a refresh, start recalculation task for missing org ids
-        missing_org_ids = missing_org_ids_1 | missing_org_ids_2 | missing_org_ids_3
+        metrics, missing_org_ids = self.get_metrics_and_missing_orgs(org_ids)
         self.recalculate_cache_for_missing_org_ids(org_ids, missing_org_ids)
 
-        yield alert_groups_total
-        yield alert_groups_response_time_seconds
-        yield user_was_notified
+        for metric in metrics:
+            yield metric
 
-    def _get_alert_groups_total_metric(self, org_ids):
+    def _get_labels_from_integration_data(
+        self, integration_data: AlertGroupsTotalMetricsDict | AlertGroupsResponseTimeMetricsDict
+    ) -> typing.List[str]:
+        # Labels values should have the same order as _integration_labels_with_state
+        labels_values = [
+            integration_data["integration_name"],  # integration
+            integration_data["team_name"],  # team
+            integration_data["org_id"],  # grafana org_id
+            integration_data["slug"],  # grafana instance slug
+            integration_data["id"],  # grafana instance id
+        ]
+        labels_values = list(map(str, labels_values))
+        return labels_values
+
+    def _get_labels_from_user_data(self, user_data: UserWasNotifiedOfAlertGroupsMetricsDict) -> typing.List[str]:
+        # Labels values should have the same order as _user_labels
+        labels_values = [
+            user_data["user_username"],  # username
+            user_data["org_id"],  # grafana org_id
+            user_data["slug"],  # grafana instance slug
+            user_data["id"],  # grafana instance id
+        ]
+        labels_values = list(map(str, labels_values))
+        return labels_values
+
+    def _update_new_metric(self, metric_name: str, org_ids: set, missing_org_ids: set) -> set:
+        """
+        This method is used for new metrics to calculate metrics gradually and avoid force recalculation for all orgs
+        Add to collect() method the following code with metric name when needed:
+        # update new metric gradually
+        missing_org_ids_X = self._update_new_metric(<NEW_METRIC_NAME>, org_ids, missing_org_ids_X)
+        """
+        calculation_started_key = get_metric_calculation_started_key(metric_name)
+        is_calculation_started = cache.get(calculation_started_key)
+        if len(missing_org_ids) == len(org_ids) or is_calculation_started:
+            missing_org_ids = set()
+            if not is_calculation_started:
+                start_recalculation_for_new_metric.apply_async((metric_name,))
+        return missing_org_ids
+
+    def recalculate_cache_for_missing_org_ids(self, org_ids: set, missing_org_ids: set) -> None:
+        cache_timer_for_org_keys = [get_metrics_cache_timer_key(org_id) for org_id in org_ids]
+        cache_timers_for_org = cache.get_many(cache_timer_for_org_keys)
+        recalculate_orgs: typing.List[RecalculateOrgMetricsDict] = []
+        for org_id in org_ids:
+            force_task = org_id in missing_org_ids
+            if force_task or not cache_timers_for_org.get(get_metrics_cache_timer_key(org_id)):
+                recalculate_orgs.append({"organization_id": org_id, "force": force_task})
+        if recalculate_orgs:
+            start_calculate_and_cache_metrics.apply_async((recalculate_orgs,))
+
+
+class AlertGroupsAndNotificationsMetricsCollector(BaseApplicationMetricsCollector):
+    """Collects ALERT_GROUPS_TOTAL and USER_WAS_NOTIFIED_OF_ALERT_GROUPS metrics"""
+
+    def get_metrics_and_missing_orgs(self, org_ids: set) -> typing.Tuple[typing.List[Metric], set]:
+        # alert groups total metric: gauge
+        alert_groups_total, missing_org_ids_1 = self._get_alert_groups_total_metric(org_ids)
+        # user was notified of alert groups metrics: counter
+        user_was_notified, missing_org_ids_2 = self._get_user_was_notified_of_alert_groups_metric(org_ids)
+
+        # check for orgs missing any of the metrics or needing a refresh, start recalculation task for missing org ids
+        missing_org_ids = missing_org_ids_1 | missing_org_ids_2
+        return [alert_groups_total, user_was_notified], missing_org_ids
+
+    def _get_alert_groups_total_metric(self, org_ids: set) -> typing.Tuple[Metric, set]:
         alert_groups_total = GaugeMetricFamily(
             ALERT_GROUPS_TOTAL, "All alert groups", labels=self._integration_labels_with_state
         )
@@ -98,15 +158,7 @@ class ApplicationMetricsCollector:
                     logger.warning(f"Deleting stale metrics cache for {org_key}")
                     cache.delete(org_key)
                     break
-                # Labels values should have the same order as _integration_labels_with_state
-                labels_values = [
-                    integration_data["integration_name"],  # integration
-                    integration_data["team_name"],  # team
-                    integration_data["org_id"],  # grafana org_id
-                    integration_data["slug"],  # grafana instance slug
-                    integration_data["id"],  # grafana instance id
-                ]
-                labels_values = list(map(str, labels_values))
+                labels_values: typing.List[str] = self._get_labels_from_integration_data(integration_data)
                 for service_name in integration_data["services"]:
                     for state in AlertGroupState:
                         alert_groups_total.add_metric(
@@ -118,7 +170,34 @@ class ApplicationMetricsCollector:
         missing_org_ids = org_ids - processed_org_ids
         return alert_groups_total, missing_org_ids
 
-    def _get_response_time_metric(self, org_ids):
+    def _get_user_was_notified_of_alert_groups_metric(self, org_ids: set) -> typing.Tuple[Metric, set]:
+        user_was_notified = CounterMetricFamily(
+            USER_WAS_NOTIFIED_OF_ALERT_GROUPS, "Number of alert groups user was notified of", labels=self._user_labels
+        )
+        processed_org_ids = set()
+        user_was_notified_keys = [get_metric_user_was_notified_of_alert_groups_key(org_id) for org_id in org_ids]
+        org_users: typing.Dict[str, typing.Dict[int, UserWasNotifiedOfAlertGroupsMetricsDict]] = cache.get_many(
+            user_was_notified_keys
+        )
+        for org_key, users in org_users.items():
+            for _, user_data in users.items():
+                labels_values: typing.List[str] = self._get_labels_from_user_data(user_data)
+                user_was_notified.add_metric(labels_values, user_data["counter"])
+            org_id_from_key = RE_USER_WAS_NOTIFIED_OF_ALERT_GROUPS.match(org_key).groups()[0]
+            processed_org_ids.add(int(org_id_from_key))
+        missing_org_ids = org_ids - processed_org_ids
+        return user_was_notified, missing_org_ids
+
+
+class AlertGroupResponseTimeMetricsCollector(BaseApplicationMetricsCollector):
+    """Collects ALERT_GROUPS_RESPONSE_TIME metric"""
+
+    def get_metrics_and_missing_orgs(self, org_ids: set) -> typing.Tuple[typing.List[Metric], set]:
+        # alert groups response time metrics: histogram
+        alert_groups_response_time_seconds, missing_org_ids = self._get_response_time_metric(org_ids)
+        return [alert_groups_response_time_seconds], missing_org_ids
+
+    def _get_response_time_metric(self, org_ids: set) -> typing.Tuple[Metric, set]:
         alert_groups_response_time_seconds = HistogramMetricFamily(
             ALERT_GROUPS_RESPONSE_TIME,
             "Users response time to alert groups in 7 days (seconds)",
@@ -135,21 +214,12 @@ class ApplicationMetricsCollector:
                     logger.warning(f"Deleting stale metrics cache for {org_key}")
                     cache.delete(org_key)
                     break
-                # Labels values should have the same order as _integration_labels
-                labels_values = [
-                    integration_data["integration_name"],  # integration
-                    integration_data["team_name"],  # team
-                    integration_data["org_id"],  # grafana org_id
-                    integration_data["slug"],  # grafana instance slug
-                    integration_data["id"],  # grafana instance id
-                ]
-                labels_values = list(map(str, labels_values))
-
+                labels_values: typing.List[str] = self._get_labels_from_integration_data(integration_data)
                 for service_name, response_time in integration_data["services"].items():
                     if not response_time:
                         continue
-                    buckets, sum_value = self.get_buckets_with_sum(response_time)
-                    buckets = sorted(list(buckets.items()), key=lambda x: float(x[0]))
+                    buckets_values, sum_value = self.get_buckets_with_sum(response_time)
+                    buckets: list = sorted(list(buckets_values.items()), key=lambda x: float(x[0]))
                     alert_groups_response_time_seconds.add_metric(
                         labels_values + [service_name],
                         buckets=buckets,
@@ -160,55 +230,7 @@ class ApplicationMetricsCollector:
         missing_org_ids = org_ids - processed_org_ids
         return alert_groups_response_time_seconds, missing_org_ids
 
-    def _get_user_was_notified_of_alert_groups_metric(self, org_ids):
-        user_was_notified = CounterMetricFamily(
-            USER_WAS_NOTIFIED_OF_ALERT_GROUPS, "Number of alert groups user was notified of", labels=self._user_labels
-        )
-        processed_org_ids = set()
-        user_was_notified_keys = [get_metric_user_was_notified_of_alert_groups_key(org_id) for org_id in org_ids]
-        org_users: typing.Dict[str, typing.Dict[int, UserWasNotifiedOfAlertGroupsMetricsDict]] = cache.get_many(
-            user_was_notified_keys
-        )
-        for org_key, users in org_users.items():
-            for _, user_data in users.items():
-                # Labels values should have the same order as _user_labels
-                labels_values = [
-                    user_data["user_username"],  # username
-                    user_data["org_id"],  # grafana org_id
-                    user_data["slug"],  # grafana instance slug
-                    user_data["id"],  # grafana instance id
-                ]
-                labels_values = list(map(str, labels_values))
-                user_was_notified.add_metric(labels_values, user_data["counter"])
-            org_id_from_key = RE_USER_WAS_NOTIFIED_OF_ALERT_GROUPS.match(org_key).groups()[0]
-            processed_org_ids.add(int(org_id_from_key))
-        missing_org_ids = org_ids - processed_org_ids
-        return user_was_notified, missing_org_ids
-
-    def _update_new_metric(self, metric_name, org_ids, missing_org_ids):
-        """
-        This method is used for new metrics to calculate metrics gradually and avoid force recalculation for all orgs
-        """
-        calculation_started_key = get_metric_calculation_started_key(metric_name)
-        is_calculation_started = cache.get(calculation_started_key)
-        if len(missing_org_ids) == len(org_ids) or is_calculation_started:
-            missing_org_ids = set()
-            if not is_calculation_started:
-                start_recalculation_for_new_metric.apply_async((metric_name,))
-        return missing_org_ids
-
-    def recalculate_cache_for_missing_org_ids(self, org_ids, missing_org_ids):
-        cache_timer_for_org_keys = [get_metrics_cache_timer_key(org_id) for org_id in org_ids]
-        cache_timers_for_org = cache.get_many(cache_timer_for_org_keys)
-        recalculate_orgs: typing.List[RecalculateOrgMetricsDict] = []
-        for org_id in org_ids:
-            force_task = org_id in missing_org_ids
-            if force_task or not cache_timers_for_org.get(get_metrics_cache_timer_key(org_id)):
-                recalculate_orgs.append({"organization_id": org_id, "force": force_task})
-        if recalculate_orgs:
-            start_calculate_and_cache_metrics.apply_async((recalculate_orgs,))
-
-    def get_buckets_with_sum(self, values):
+    def get_buckets_with_sum(self, values: typing.List[int]) -> typing.Tuple[typing.Dict[str, int], typing]:
         """Put values in correct buckets and count values sum"""
         buckets_values = {str(key): 0 for key in self._buckets}
         sum_value = 0
@@ -220,4 +242,7 @@ class ApplicationMetricsCollector:
         return buckets_values, sum_value
 
 
-application_metrics_registry.register(ApplicationMetricsCollector())
+if ALERT_GROUPS_AND_NOTIFICATIONS_METRIC_COLLECTOR_ENABLED:
+    application_metrics_registry.register(AlertGroupsAndNotificationsMetricsCollector())
+if RESPONSE_TIME_METRIC_COLLECTOR_ENABLED:
+    application_metrics_registry.register(AlertGroupResponseTimeMetricsCollector())
