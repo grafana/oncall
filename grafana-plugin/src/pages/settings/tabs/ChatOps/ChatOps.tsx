98b02e9d01819efffd723115e7545b034a7227f1
Ravishankar <ravishankar.gnanaprakasam@gmail.com>
2024-08-06 22:35:52 +0530

    Add mattermost OAuth2 flow
    Settings Frontend flow
    
    Change UI text for mattermost integration
    
    OAuth login flow
    
    OAuth Flow
    
    Fix env status loading
    
    Lint fixes

--- grafana-plugin/src/pages/settings/tabs/ChatOps/ChatOps.tsx
+++ grafana-plugin/src/pages/settings/tabs/ChatOps/ChatOps.tsx
@@ -151,6 +151,14 @@ const Tabs = (props: TabsProps) => {
           </Stack>
         </VerticalTab>
       )}
+      {store.hasFeature(AppFeature.Mattermost) && (
+        <VerticalTab id={ChatOpsTab.Mattermost}>
+          <HorizontalGroup>
+            <Icon name="message" />
+            Mattermost
+          </HorizontalGroup>
+        </VerticalTab>
+      )}
     </VerticalTabsBar>
   );
 };
