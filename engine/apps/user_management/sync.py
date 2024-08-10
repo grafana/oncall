a8513869cf78b6cb2fc57c5374cef43c827f74ff
Michael Derynck <michael.derynck@grafana.com>
2024-08-09 19:33:44 -0600

    Handle empty teams

--- engine/apps/user_management/sync.py
+++ engine/apps/user_management/sync.py
@@ -418,8 +418,9 @@ def _sync_teams_members_data(organization: Organization, team_members: dict[int,
         return
     # set team members
     for team_id, members_ids in team_members.items():
-        team = organization.teams.get(team_id=team_id)
-        team.users.set(organization.users.filter(user_id__in=members_ids))
+        if members_ids:
+            team = organization.teams.get(team_id=team_id)
+            team.users.set(organization.users.filter(user_id__in=members_ids))
 
 
 def apply_sync_data(organization: Organization, sync_data: SyncData):
