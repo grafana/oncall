bc1f3175f12284d945f544abbc965f9a6e8eab71
Joey Orlando <joseph.t.orlando@gmail.com>
2024-08-15 13:05:29 -0400

    typo

--- engine/apps/public_api/serializers/direct_paging.py
+++ engine/apps/public_api/serializers/direct_paging.py
@@ -3,6 +3,6 @@ from apps.api.serializers.direct_paging import BasePagingSerializer
 
 class DirectPagingSerializer(BasePagingSerializer):
     """
-    Very similar to `apps.api.serializers.paging.DirectPagingSerializer` except that
+    Very similar to `apps.api.serializers.direct_paging.DirectPagingSerializer` except that
     there is no `grafana_incident_id` attribute
     """
