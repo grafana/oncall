e9906bad8e6a60ec2ccc7c23b9181d3c509be463
Rares Mardare <rares.mardare@grafana.com>
2024-08-22 11:09:52 +0300

    minor touchup

--- grafana-plugin/src/containers/EscalationChainCard/EscalationChainCard.tsx
+++ grafana-plugin/src/containers/EscalationChainCard/EscalationChainCard.tsx
@@ -35,18 +35,23 @@ export const EscalationChainCard = observer((props: AlertReceiveChannelCardProps
             <Text type="primary" size="medium">
               {escalationChain.name}
             </Text>
-            <Badge
-              text={escalationChain.number_of_integrations}
-              color="green"
-              icon="link"
-              tooltip={
-                escalationChain.number_of_integrations > 0 || escalationChain.number_of_routes > 0
-                  ? `Modifying this escalation chain will affect ${escalationChain.number_of_integrations} integrations and ${escalationChain.number_of_routes} routes.`
-                  : 'This escalation is not connected to any integration route, go to integrations and connect route to this escalation chain'
-              }
-            />
+            <div>
+              <Badge
+                text={escalationChain.number_of_integrations}
+                color="green"
+                icon="link"
+                tooltip={
+                  escalationChain.number_of_integrations > 0 || escalationChain.number_of_routes > 0
+                    ? `Modifying this escalation chain will affect ${escalationChain.number_of_integrations} integrations and ${escalationChain.number_of_routes} routes.`
+                    : 'This escalation is not connected to any integration route, go to integrations and connect route to this escalation chain'
+                }
+              />
+            </div>
           </Stack>
-          <TeamName team={grafanaTeamStore.items[escalationChain.team]} size="small" />
+
+          <div>
+            <TeamName team={grafanaTeamStore.items[escalationChain.team]} size="small" />
+          </div>
         </Stack>
       </Stack>
     </div>
