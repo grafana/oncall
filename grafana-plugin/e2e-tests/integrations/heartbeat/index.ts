bd8d6d296cbe7d5f0e4a89c56e7a5923eaf474fc
Joey Orlando <joseph.t.orlando@gmail.com>
2024-08-27 16:25:13 -0400

    wip

--- grafana-plugin/e2e-tests/integrations/heartbeat/index.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from '../../fixtures';
-
-export const HEARTBEAT_SETTINGS_FORM_TEST_ID = 'heartbeat-settings-form';
-
-export const openHeartbeatSettingsForm = async (page: Page) => {
-  await page.getByTestId('integration-settings-context-menu-wrapper').click();
-  await page.getByTestId('integration-heartbeat-settings').click();
-};
