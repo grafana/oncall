fba5f4d2845fd70b03dbf745fb226a421971a2d0
Ravishankar <ravishankar.gnanaprakasam@gmail.com>
2024-09-17 21:50:09 +0530

    Remove config checks from OrganizationConfigChecksView

--- engine/apps/api/tests/test_organization.py
+++ engine/apps/api/tests/test_organization.py
@@ -279,10 +279,6 @@ def test_get_organization_slack_config_checks(
     expected_result = {
         "is_chatops_connected": False,
         "is_integration_chatops_connected": False,
-        "mattermost": {
-            "env_status": True,
-            "is_integrated": False,
-        },
     }
     response = client.get(url, format="json", **make_user_auth_headers(user, token))
     assert response.status_code == status.HTTP_200_OK
@@ -338,10 +334,6 @@ def test_get_organization_telegram_config_checks(
     expected_result = {
         "is_chatops_connected": False,
         "is_integration_chatops_connected": False,
-        "mattermost": {
-            "env_status": True,
-            "is_integrated": False,
-        },
     }
     response = client.get(url, format="json", **make_user_auth_headers(user, token))
     assert response.status_code == status.HTTP_200_OK
