8b26021ee4cde5eb69a78db91b47bf3ff5deb984
Julia <ferril.darkdiver@gmail.com>
2024-09-24 13:28:29 +0200

    Update log rendering for declare incident step

--- engine/apps/alerts/tasks/declare_incident.py
+++ engine/apps/alerts/tasks/declare_incident.py
@@ -2,6 +2,7 @@ import logging
 
 from django.conf import settings
 
+from apps.alerts.incident_appearance.renderers.constants import DEFAULT_BACKUP_TITLE
 from common.custom_celery_tasks import shared_dedicated_queue_retry_task
 from common.incident_api.client import (
     DEFAULT_INCIDENT_SEVERITY,
@@ -15,7 +16,7 @@ logger = logging.getLogger(__name__)
 MAX_ATTACHED_ALERT_GROUPS_PER_INCIDENT = 5
 
 
-def _attach_alert_group_to_incident(alert_group, incident_id, escalation_policy, attached=False):
+def _attach_alert_group_to_incident(alert_group, incident_id, incident_title, escalation_policy, attached=False):
     from apps.alerts.models import AlertGroupLogRecord, DeclaredIncident, EscalationPolicy
 
     declared_incident, _ = DeclaredIncident.objects.get_or_create(
@@ -32,7 +33,7 @@ def _attach_alert_group_to_incident(alert_group, incident_id, escalation_policy,
         type=AlertGroupLogRecord.TYPE_ESCALATION_TRIGGERED,
         reason=reason,
         alert_group=alert_group,
-        step_specific_info={"incident_id": incident_id},
+        step_specific_info={"incident_id": incident_id, "incident_title": incident_title},
         escalation_policy=escalation_policy,
         escalation_policy_step=EscalationPolicy.STEP_DECLARE_INCIDENT,
     )
@@ -97,6 +98,7 @@ def declare_incident(alert_group_pk, escalation_policy_pk):
             # incident exists, check if it is still active
             if incident_data["status"] == DEFAULT_INCIDENT_STATUS:
                 # attach to incident context
+                incident_title = incident_data["title"]
                 num_attached = AlertGroup.objects.filter(declared_incident=existing_incident).count()
                 if num_attached < MAX_ATTACHED_ALERT_GROUPS_PER_INCIDENT:
                     try:
@@ -104,7 +106,9 @@ def declare_incident(alert_group_pk, escalation_policy_pk):
                     except IncidentAPIException as e:
                         logger.error(f"Error attaching to existing incident: {e.msg}")
                 # setup association between alert group and incident (even if not attached)
-                _attach_alert_group_to_incident(alert_group, incident_id, escalation_policy, attached=True)
+                _attach_alert_group_to_incident(
+                    alert_group, incident_id, incident_title, escalation_policy, attached=True
+                )
             else:
                 existing_incident.is_opened = False
                 existing_incident.save(update_fields=["is_opened"])
@@ -114,7 +118,7 @@ def declare_incident(alert_group_pk, escalation_policy_pk):
         # TODO: get severity from policy? alert group label?
         try:
             incident_data, _ = incident_client.create_incident(
-                alert_group.web_title_cache,
+                alert_group.web_title_cache if alert_group.web_title_cache else DEFAULT_BACKUP_TITLE,
                 severity=DEFAULT_INCIDENT_SEVERITY,
                 attachCaption="OnCall Alert Group",
                 attachURL=alert_group.web_link,
@@ -125,4 +129,6 @@ def declare_incident(alert_group_pk, escalation_policy_pk):
             # raise (and retry)
             raise
         else:
-            _attach_alert_group_to_incident(alert_group, incident_data["incidentID"], escalation_policy)
+            _attach_alert_group_to_incident(
+                alert_group, incident_data["incidentID"], incident_data["title"], escalation_policy
+            )
