c464dcc60658069da37ff513999fed5894225834
Ildar Iskhakov <ildar.iskhakov@grafana.com>
2024-09-25 19:36:29 +0800

    Minor fixes

--- engine/apps/public_api/views/escalation.py
+++ engine/apps/public_api/views/escalation.py
@@ -8,10 +8,9 @@ from apps.auth_token.auth import ApiTokenAuthentication
 from apps.public_api.serializers import AlertGroupSerializer, EscalationSerializer
 from apps.public_api.throttlers import UserThrottle
 from common.api_helpers.exceptions import BadRequest
-from common.api_helpers.mixins import AlertGroupEnrichingMixin
 
 
-class EscalationView(AlertGroupEnrichingMixin, APIView):
+class EscalationView(APIView):
     """
     aka "Direct Paging"
     """
