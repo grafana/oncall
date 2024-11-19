2bcbac8454904ae8e0e8783d41b32cb19ad2d7eb
Matias Bordese <mbordese@gmail.com>
2024-11-19 09:52:23 -0300

    Enable service account token auth for public API (#5254)
    Related to https://github.com/grafana/oncall-private/issues/2826
    
    Continuing work started in https://github.com/grafana/oncall/pull/5211,
    this adds support for Grafana service accounts tokens for API
    authentication (except alert group actions which will still require a
    user behind). Next steps would be updating the go client and the
    terraform provider to allow service account token auth for OnCall
    resources.
    
    Following proposal 1.1 from
    [doc](https://docs.google.com/document/d/1I3nFbsUEkiNPphBXT-kWefIeramTY71qqZ1OA06Kmls/edit?usp=sharing).

---
+++ engine/apps/user_management/models/service_account.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+from typing import List
+
+from django.db import models
+
+from apps.user_management.models import Organization
+
+
+@dataclass
+class ServiceAccountUser:
+    """Authenticated service account in public API requests."""
+
+    service_account: "ServiceAccount"
+    organization: "Organization"  # required for insight logs interface
+    username: str  # required for insight logs interface
+    public_primary_key: str  # required for insight logs interface
+    role: str  # required for permissions check
+    permissions: List[str]  # required for permissions check
+
+    @property
+    def id(self):
+        return self.service_account.id
+
+    @property
+    def pk(self):
+        return self.service_account.id
+
+    @property
+    def organization_id(self):
+        return self.organization.id
+
+    @property
+    def is_authenticated(self):
+        return True
+
+
+class ServiceAccount(models.Model):
+    organization: "Organization"
+
+    grafana_id = models.PositiveIntegerField()
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="service_accounts")
+    login = models.CharField(max_length=300)
+
+    class Meta:
+        unique_together = ("grafana_id", "organization")
+
+    @property
+    def username(self):
+        # required for insight logs interface
+        return self.login
+
+    @property
+    def public_primary_key(self):
+        # required for insight logs interface
+        return f"service-account:{self.grafana_id}"
