b2326ae021721a2b1291a766bd1d195ec2f3f5c3
Ravishankar <ravishankar.gnanaprakasam@gmail.com>
2024-11-20 22:03:50 +0530

    Mattermost incoming event handler
    Remove print
    
    Add migrations

---
+++ engine/apps/mattermost/events/types.py
@@ -0,0 +1,29 @@
+import enum
+import typing
+
+
+class MattermostAlertGroupContext(typing.TypedDict):
+    action: str
+    token: str
+    alert: int
+
+
+class MattermostEvent(typing.TypedDict):
+    user_id: str
+    user_name: str
+    channel_id: str
+    channel_name: str
+    team_id: str
+    team_domain: str
+    post_id: str
+    trigger_id: str
+    type: str
+    data_source: str
+    context: MattermostAlertGroupContext
+
+
+class EventAction(enum.StrEnum):
+    ACKNOWLEDGE = "acknowledge"
+    UNACKNOWLEDGE = "unacknowledge"
+    RESOLVE = "resolve"
+    UNRESOLVE = "unresolve"
