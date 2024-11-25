7ba3cdeb605823bf94a8f1be0ae5e1f2c1aeccf9
Joey Orlando <joseph.t.orlando@gmail.com>
2024-11-25 16:39:24 -0500

    tests + linting

--- engine/apps/schedules/tests/tasks/shift_swaps/test_slack_messages.py
+++ engine/apps/schedules/tests/tasks/shift_swaps/test_slack_messages.py
@@ -42,9 +42,11 @@ def test_create_shift_swap_request_message_post_message_to_channel_called(
     schedule = ssr.schedule
     organization = schedule.organization
 
-    slack_channel = make_slack_channel(slack_team_identity)
-    slack_message = make_slack_message(alert_group=None, channel=slack_channel, organization=organization, slack_id="12345")
     slack_team_identity = make_slack_team_identity()
+    slack_channel = make_slack_channel(slack_team_identity)
+    slack_message = make_slack_message(
+        alert_group=None, channel=slack_channel, organization=organization, slack_id="12345"
+    )
 
     MockBaseShiftSwapRequestStep.return_value.create_message.return_value = slack_message
 
