98f6b1bfc4a9ab9f1ce59ae8056789b10fa80dd0
Joey Orlando <joey.orlando@grafana.com>
2024-11-26 14:04:39 -0500

    fix: patch data migration scripts for non-MySQL databases (#5297)
    # What this PR does
    
    ## Which issue(s) this PR closes
    
    Fixes
    https://github.com/grafana/oncall/issues/5244#issuecomment-2493688544
    
    ## Checklist
    
    - [x] Unit, integration, and e2e (if applicable) tests updated
    - [x] Documentation added (or `pr:no public docs` PR label added if not
    required)
    - [x] Added the relevant release notes label (see labels prefixed w/
    `release:`). These labels dictate how your PR will
        show up in the autogenerated release notes.

--- engine/apps/user_management/migrations/0026_auto_20241017_1919.py
+++ engine/apps/user_management/migrations/0026_auto_20241017_1919.py
@@ -13,21 +13,67 @@ def populate_default_slack_channel(apps, schema_editor):
 
     logger.info("Starting migration to populate default_slack_channel field.")
 
-    sql = f"""
-    UPDATE {Organization._meta.db_table} AS org
-    JOIN {SlackChannel._meta.db_table} AS sc ON sc.slack_id = org.general_log_channel_id
-                         AND sc.slack_team_identity_id = org.slack_team_identity_id
-    SET org.default_slack_channel_id = sc.id
-    WHERE org.general_log_channel_id IS NOT NULL
-      AND org.slack_team_identity_id IS NOT NULL;
-    """
+    # NOTE: the following raw SQL only works on mysql, fall back to the less-efficient (but working) ORM method
+    # for non-mysql databases
+    #
+    # see the following references for more information:
+    # https://github.com/grafana/oncall/issues/5244#issuecomment-2493688544
+    # https://github.com/grafana/oncall/pull/5233/files#diff-e69e0d7ecf51300be2ca5f4239c5f08b4c6e41de9856788f85a522001595a192
+    if schema_editor.connection.vendor == "mysql":
+        sql = f"""
+        UPDATE {Organization._meta.db_table} AS org
+        JOIN {SlackChannel._meta.db_table} AS sc ON sc.slack_id = org.general_log_channel_id
+                            AND sc.slack_team_identity_id = org.slack_team_identity_id
+        SET org.default_slack_channel_id = sc.id
+        WHERE org.general_log_channel_id IS NOT NULL
+        AND org.slack_team_identity_id IS NOT NULL;
+        """
 
-    with schema_editor.connection.cursor() as cursor:
-        cursor.execute(sql)
-        updated_rows = cursor.rowcount  # Number of rows updated
+        with schema_editor.connection.cursor() as cursor:
+            cursor.execute(sql)
+            updated_rows = cursor.rowcount  # Number of rows updated
 
-    logger.info(f"Bulk updated {updated_rows} organizations with their default Slack channel.")
-    logger.info("Finished migration to populate default_slack_channel field.")
+        logger.info(f"Bulk updated {updated_rows} organizations with their default Slack channel.")
+        logger.info("Finished migration to populate default_slack_channel field.")
+    else:
+        queryset = Organization.objects.filter(general_log_channel_id__isnull=False, slack_team_identity__isnull=False)
+        total_orgs = queryset.count()
+        updated_orgs = 0
+        missing_channels = 0
+        organizations_to_update = []
+
+        logger.info(f"Total organizations to process: {total_orgs}")
+
+        for org in queryset:
+            slack_id = org.general_log_channel_id
+            slack_team_identity = org.slack_team_identity
+
+            try:
+                slack_channel = SlackChannel.objects.get(slack_id=slack_id, slack_team_identity=slack_team_identity)
+
+                org.default_slack_channel = slack_channel
+                organizations_to_update.append(org)
+
+                updated_orgs += 1
+                logger.info(
+                    f"Organization {org.id} updated with SlackChannel {slack_channel.id} (slack_id: {slack_id})."
+                )
+            except SlackChannel.DoesNotExist:
+                missing_channels += 1
+                logger.warning(
+                    f"SlackChannel with slack_id {slack_id} and slack_team_identity {slack_team_identity} "
+                    f"does not exist for Organization {org.id}."
+                )
+
+        if organizations_to_update:
+            Organization.objects.bulk_update(organizations_to_update, ["default_slack_channel"])
+            logger.info(f"Bulk updated {len(organizations_to_update)} organizations with their default Slack channel.")
+
+        logger.info(
+            f"Finished migration. Total organizations processed: {total_orgs}. "
+            f"Organizations updated: {updated_orgs}. Missing SlackChannels: {missing_channels}."
+        )
+
 
 class Migration(migrations.Migration):
 
