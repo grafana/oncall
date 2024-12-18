a01c4d378af4858f8c95a8b43a7f816215e177e6
Joey Orlando <joseph.t.orlando@gmail.com>
2024-12-18 18:05:55 -0500

    chore: linting

--- tools/migrators/lib/tests/pagerduty/test_migrate.py
+++ tools/migrators/lib/tests/pagerduty/test_migrate.py
@@ -6,7 +6,9 @@ from lib.pagerduty.migrate import migrate
 @patch("lib.pagerduty.migrate.MIGRATE_USERS", False)
 @patch("lib.pagerduty.migrate.APISession")
 @patch("lib.pagerduty.migrate.OnCallAPIClient")
-def test_users_are_skipped_when_migrate_users_is_false(MockOnCallAPIClient, MockAPISession):
+def test_users_are_skipped_when_migrate_users_is_false(
+    MockOnCallAPIClient, MockAPISession
+):
     mock_session = MockAPISession.return_value
     mock_session.list_all.return_value = []
     mock_oncall_client = MockOnCallAPIClient.return_value
