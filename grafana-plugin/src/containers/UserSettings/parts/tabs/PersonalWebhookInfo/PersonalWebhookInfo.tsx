996e06be793ddbd21e732e16c04dd56c586022d6
Matt Thorning <matt.thorning@grafana.com>
2025-02-06 10:07:59 +0000

    add personal webhook form to user profile settings

--- grafana-plugin/src/containers/UserSettings/parts/tabs/PersonalWebhookInfo/PersonalWebhookInfo.tsx
+++ grafana-plugin/src/containers/UserSettings/parts/tabs/PersonalWebhookInfo/PersonalWebhookInfo.tsx
@@ -1,27 +1,133 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { Stack } from '@grafana/ui';
+import { css } from '@emotion/css';
+import { Button, Select, Stack, Field, TextArea } from '@grafana/ui';
 import { StackSize } from 'helpers/consts';
 import { observer } from 'mobx-react';
+import { Controller, useForm } from 'react-hook-form';
 
-import { Block } from 'components/GBlock/Block';
-import { Text } from 'components/Text/Text';
+import { ActionKey } from 'models/loader/action-keys';
+import { WebhookTriggerType } from 'models/outgoing_webhook/outgoing_webhook.types'
+import { useStore } from 'state/useStore';
+
+interface FormFields {
+  webhook: string;
+  context: string;
+}
 
 export const PersonalWebhookInfo = observer(() => {
+  const { userStore, outgoingWebhookStore, loaderStore } = useStore();
+
+  const {
+    formState: {
+      isDirty,
+      isValid,
+      errors,
+    },
+    watch,
+    control,
+    getValues,
+    setValue,
+    handleSubmit,
+  } = useForm<FormFields>({
+    mode: 'onBlur',
+    defaultValues: { webhook: '', context: '{}' }
+  });
+
+  function onFormSubmit() {
+    const values = getValues();
+    const webhook = values.webhook === '' ? null : values.webhook;
+    const context = JSON.parse(values.context);
+    userStore.addPersonalWebook({ webhook, context });
+  }
+
+  useEffect(() => {
+    (async () => {
+      await userStore.updatePersonalWebhook();
+      await outgoingWebhookStore.updateItems(
+        {
+          trigger_type: WebhookTriggerType.PersonalNotification.key,
+        },
+        true
+      );
+    })();
+  }, []);
+
+  useEffect(() => {
+    const { webhook, context } = userStore.personalWebhook ?? {};
+    setValue('webhook', webhook ?? '');
+    setValue('context', context ? JSON.stringify(context, null, 2) : '{}');
+  }, [userStore.personalWebhook]);
+
+  const options = [
+    { label: 'None', value: '' },
+    ...Object.values(outgoingWebhookStore.items).map((item) => ({
+      label: item.name,
+      value: item.id,
+    }))];
+
+  const webhook = watch('webhook');
   return (
-    <>
-      {/* allow selecting a personal webhook + setting additional metadata as context */}
-      <Stack direction="column" alignItems="flex-start" gap={StackSize.lg}>
-        <Block bordered withBackground>
-          <Stack direction="column" alignItems="center">
-            <Text>Choose a personal notification webhook</Text>
-            <br />
-            <Text>Set your user specific context data</Text>
-            <br />
-            <Text>Display current settings</Text>
-          </Stack>
-        </Block>
-      </Stack>
-    </>
+    <Stack direction="column" alignItems="flex-start" gap={StackSize.lg}>
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <Field label="Webhook" className={styles.field} >
+          <Controller
+            name="webhook"
+            control={control}
+            render={({ field }) =>
+              <Select
+                {...field}
+                menuShouldPortal
+                onChange={({ value }) => field.onChange(value)}
+                isLoading={loaderStore.isLoading(ActionKey.FETCH_WEBHOOKS)}
+                options={options}
+              />
+            }
+          />
+        </Field>
+        {webhook !== '' ? (
+          <Field
+            className={styles.field}
+            label="Context"
+            invalid={!!errors.context}
+            error={errors.context?.message}
+          >
+            <Controller
+              name="context"
+              control={control}
+              rules={{
+                validate(value) {
+                  let context: object;
+                  try {
+                    context = JSON.parse(value);
+                  } catch (_) {
+                    return 'Invalid JSON';
+                  }
+                  if (typeof context !== 'object' || context === null || Array.isArray(context)) {
+                    return 'JSON must be an object';
+                  }
+                  return true;
+                }
+              }}
+              render={({ field }) =>
+                <TextArea
+                  {...field}
+                  rows={8}
+                />
+              }
+            />
+          </Field>
+        ) : null}
+        <Button type="submit" variant={'primary'} disabled={!isDirty || !isValid}>
+          Save
+        </Button>
+      </form>
+    </Stack>
   );
 });
+
+const styles = {
+  field: css`
+    width: 100%;
+  `,
+}
