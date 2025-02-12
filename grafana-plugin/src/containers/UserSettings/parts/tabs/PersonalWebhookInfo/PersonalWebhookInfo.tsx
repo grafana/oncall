2ee6b30a52ceda4424eb6f678eb82ac4ccc487d4
Matt Thorning <matt.thorning@grafana.com>
2025-02-12 11:01:16 +0000

    restyled UI to match Figma

--- grafana-plugin/src/containers/UserSettings/parts/tabs/PersonalWebhookInfo/PersonalWebhookInfo.tsx
+++ grafana-plugin/src/containers/UserSettings/parts/tabs/PersonalWebhookInfo/PersonalWebhookInfo.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useMemo, useState } from 'react';
 
 import { css } from '@emotion/css';
-import { LoadingPlaceholder, Button, Select, Stack, Field, TextArea } from '@grafana/ui';
+import { LoadingPlaceholder, Button, Select, Stack, Field, TextArea, Icon } from '@grafana/ui';
 import { StackSize } from 'helpers/consts';
 import { observer } from 'mobx-react';
 import { Controller, useForm } from 'react-hook-form';
 
 import { PluginLink } from 'components/PluginLink/PluginLink';
+import { PLUGIN_ROOT } from 'helpers/consts';
 import { Text } from 'components/Text/Text';
 import { WithConfirm } from 'components/WithConfirm/WithConfirm';
 import { ActionKey } from 'models/loader/action-keys';
@@ -129,28 +130,38 @@ export const PersonalWebhookInfo = observer(() => {
     )
   }
 
-  if (!isLoadingOptions && !hasConnectedWebhook && webhookOptions.length === 0) {
-    return (
-      <Stack direction="column" alignItems="center" gap={StackSize.lg}>
-        <Text type="secondary">
-          No webhooks found. Make sure you have added at least one webhook with the Trigger Type set to "Personal Notification".
-        </Text>
-        <PluginLink query={{ page: 'outgoing_webhooks' }}>
-          <Button variant="primary">
-            Go to Outgoing Webhooks
-          </Button>
-        </PluginLink>
-      </Stack>
-    )
-  }
-
+  // if (!isLoadingOptions && !hasConnectedWebhook && webhookOptions.length === 0) {
+  //   return (
+  //     <Stack direction="column" alignItems="center" gap={StackSize.lg}>
+  //       <Text type="secondary">
+  //         No webhooks found. Make sure you have added at least one webhook with the Trigger Type set to "Personal Notification".
+  //       </Text>
+  //       <PluginLink query={{ page: 'outgoing_webhooks' }}>
+  //         <Button variant="primary">
+  //           Go to Outgoing Webhooks
+  //         </Button>
+  //       </PluginLink>
+  //     </Stack>
+  //   )
+  // }
   return (
     <Stack direction="column" alignItems="flex-start" gap={StackSize.lg}>
-      <Text.Title level={2}>Connect a personal webhook</Text.Title>
       <form className={styles.form}>
         <Field
-          label="Webhook"
-          description="Select a webhook to trigger as your personal notification channel."
+          label="Add webhook to send personal notifications"
+          description={
+            <p>The list only displays webhooks that have Personal notification as a trigger.
+              <a href={`${PLUGIN_ROOT}/outgoing_webhooks`} target="_blank" rel="noreferrer" className={styles.link}>
+                <Text type="link">
+                  <span>Configure</span>
+                  <Icon
+                    name="external-link-alt"
+                    className={styles.linkIcon}
+                  />
+                </Text>
+              </a>
+            </p>
+          }
         >
           <Controller
             name="webhook"
@@ -159,29 +170,59 @@ export const PersonalWebhookInfo = observer(() => {
               <Select
                 {...field}
                 menuShouldPortal
-                placeholder={(user.messaging_backends?.WEBHOOK?.name as string) ?? 'Select personal webhook to trigger'}
+                placeholder={(user.messaging_backends?.WEBHOOK?.name as string) ?? 'Select webhook'}
                 onChange={({ value }) => field.onChange(value)}
                 options={webhookOptions}
               />
             }
           />
         </Field>
+<<<<<<< HEAD
         {selectedWebhook != null ? (
           <Field
             label="Context"
             description="Optional. Set additional context (as a JSON dict) to make it available in the webhook payload and/or templates."
             invalid={!!errors.context}
             error={errors.context?.message}
+=======
+        {
+          selectedWebhook != null ? (
+            <Field
+              label="Context"
+              description={
+                <p>You can add additional JSON template to use the same webhook for multiple users events.
+                  <a href={"https://grafana.com/docs/oncall/latest/configure/integrations/outgoing-webhooks"} target="_blank" rel="noreferrer" className={styles.link}>
+                    <Text type="link">
+                      <span>Learn more in docs</span>
+                      <Icon
+                        name="external-link-alt"
+                        className={styles.linkIcon}
+                      />
+                    </Text>
+                  </a>
+                </p>
+              }
+              invalid={!!errors.context}
+              error={errors.context?.message}
+            >
+              <Controller
+                name="context"
+                control={control}
+                rules={contextRules}
+                render={({ field }) => <TextArea {...field} rows={8} />}
+              />
+            </Field>
+          ) : null
+        }
+        <Stack direction="row" gap={2}>
+          <Button
+            type="submit"
+            variant="primary" disabled={!isDirty || !isValid || !hasSelectedValidWebhook}
+            onClick={handleSubmit(onFormSubmit)}
+>>>>>>> 7d4667f0 (restyled UI to match Figma)
           >
-            <Controller
-              name="context"
-              control={control}
-              rules={contextRules}
-              render={({ field }) => <TextArea {...field} rows={8} />}
-            />
-          </Field>
-        ) : null}
-        <Stack direction="row" gap={2} justifyContent="flex-end">
+            {hasConnectedWebhook ? 'Save' : 'Connect'}
+          </Button>
           {hasConnectedWebhook ? (
             <WithConfirm
               title={`Are you sure you want to disconnect the webhook named "${user.messaging_backends.WEBHOOK?.name}"?`}
@@ -193,21 +234,37 @@ export const PersonalWebhookInfo = observer(() => {
               >Disconnect</Button>
             </WithConfirm>
           ) : null}
-          <Button
-            type="submit"
-            variant="primary" disabled={!isDirty || !isValid || !hasSelectedValidWebhook}
-            onClick={handleSubmit(onFormSubmit)}
-          >
-            {hasConnectedWebhook ? 'Update' : 'Connect'}
-          </Button>
         </Stack>
-      </form>
-    </Stack>
+      </form >
+    </Stack >
   );
 });
 
 const styles = {
   form: css`
     width: 100%;
+
+    & > div {
+      margin-bottom: 24px;
+    }
+
+    & > div:last-child {
+      margin-bottom: 0;
+    }
+
+    div {
+      max-width: unset;
+    }
+    
+    p {
+      margin-bottom: 8px;
+    }
   `,
+  link: css`
+    margin-left: 8px;
+  `,
+  linkIcon: css`
+    margin-left: 4px;
+    margin-bottom: 2px;
+  `
 }
