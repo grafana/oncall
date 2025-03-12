8c9cf507d69d3bebbb4135e072f735241d45cdcb
Joey Orlando <joseph.t.orlando@gmail.com>
2025-03-11 21:20:29 -0500

    add session tracking header

--- tools/migrators/lib/oncall/api_client.py
+++ tools/migrators/lib/oncall/api_client.py
@@ -1,19 +1,25 @@
 import requests
 
-from lib.base_config import ONCALL_API_TOKEN, ONCALL_API_URL
+from lib.base_config import ONCALL_API_TOKEN, ONCALL_API_URL, MIGRATING_FROM
 from lib.network import api_call as _api_call
+from lib.session import get_or_create_session_id
 
 
 class OnCallAPIClient:
+    _session_id = None
+
     @classmethod
     def api_call(cls, method: str, path: str, **kwargs) -> requests.Response:
-        return _api_call(
-            method,
-            ONCALL_API_URL,
-            path,
-            headers={"Authorization": ONCALL_API_TOKEN},
-            **kwargs
-        )
+        if cls._session_id is None:
+            cls._session_id = get_or_create_session_id()
+
+        kwargs.setdefault("headers", {})
+        kwargs["headers"].update({
+            "Authorization": ONCALL_API_TOKEN,
+            "User-Agent": f"IRM Migrator - {MIGRATING_FROM} - {cls._session_id}",
+        })
+
+        return _api_call(method, ONCALL_API_URL, path, **kwargs)
 
     @classmethod
     def list_all(cls, path: str) -> list[dict]:
